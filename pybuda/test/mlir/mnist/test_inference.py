20fe91387b0a75c1d215c8ae3e2a815f0c1f1bcb
Nikola Vukobrat <nvukobrat@tenstorrent.com>
2024-07-18 09:14:58 +0000

    [Training] MNIST Linear: Inference sample using torch.compile and training...

---
+++ pybuda/test/mlir/mnist/test_inference.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn
+
+from .utils import *
+
+
+def test_mnist_inference():
+    inputs = [torch.rand(1, 784)]
+
+    framework_model = MNISTLinear()
+    fw_out = framework_model(*inputs)
+
+    compiled_model = torch.compile(framework_model.to("tt"), backend="tt")
+    co_out = compiled_model(*[i.to("tt") for i in inputs])
+
+    co_out = [co.to("cpu") for co in co_out]
+    assert [torch.allclose(fo, co) for fo, co in zip(fw_out, co_out)]
