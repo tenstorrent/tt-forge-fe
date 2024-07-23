d98226e1f3527d0cdbf1f7d001593a7328ac2ff5
Milan Topalovic <mtopalovic@tenstorrent.com>
2024-07-23 07:23:39 +0000

    Remove makefile references from ttforge

--- pybuda/module.mk
+++
@@ -1,8 +0,0 @@
-include pybuda/csrc/module.mk
-
-$(PYTHON_ENV_ROOT)/lib/$(PYTHON_VERSION)/site-packages/pybuda.egg-link: $(PYTHON_ENV) $(PYTHON_ENV_ROOT)/lib/$(PYTHON_VERSION)/site-packages/pybuda/_C.so
-	bash -c "source $(PYTHON_ENV_ROOT)/bin/activate; cd pybuda; pip install -e ."
-	touch -r $(PYTHON_ENV_ROOT)/lib/$(PYTHON_VERSION)/site-packages/pybuda/_C.so $@
-
-pybuda: pybuda/csrc $(PYTHON_ENV_ROOT)/lib/$(PYTHON_VERSION)/site-packages/pybuda.egg-link ;
-
