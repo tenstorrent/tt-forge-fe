7e2276c3dd60320da889fb01ef01c9318fdc6cf3
Sterling Taylor <166402033+staylorTT@users.noreply.github.com>
2024-09-04 17:17:26 -0500

    Update ordered_map.hpp
    License cleanup.

--- utils/ordered_associative_containers/ordered_map.hpp
+++ utils/ordered_associative_containers/ordered_map.hpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 //
 // SPDX-License-Identifier: Apache-2.0
+// This file incorporates work covered by the following copyright and permission notice:
 /**
  * MIT License
  *
