fe413b933d7eab730430d0eb86be4d6c89a33e95
Pradeep Chandrasekaran <160489301+chandrasekaranpradeep@users.noreply.github.com>
2024-09-19 10:28:06 +0530

    Update forge embedding op argument order with respect to TTIR (#305)

--- forge/forge/op/eval/forge/embedding.py
+++ forge/forge/op/eval/forge/embedding.py
@@ -12,14 +12,14 @@ def eval(type, attr, ops):
     assert type == "embedding"
     assert len(ops) == 2
     t_ops = to_torch_operands(*ops)
-    return torch.embedding(t_ops[0], t_ops[1].to(torch.int32))
+    return torch.embedding(t_ops[1], t_ops[0].to(torch.int32))
 
 
 def shape(type, attr, ops):
     assert type == "embedding"
     assert len(ops) == 2
-    shape = list(ops[1])
-    shape.append(ops[0][-1])
+    shape = list(ops[0])
+    shape.append(ops[1][-1])
     return shape, []
 
 
@@ -27,18 +27,18 @@ def lower(type, attr, lc, ops, outputs):
     assert type == "embedding"
     assert len(ops) == 2
 
-    lc.set_output_df(ops[1], DataFormat.RawUInt32)
-    lc.set_runtime_tensor_transform(ops[1], RuntimeTensorTransform.EmbeddingIndex(ops[1].shape))
+    lc.set_output_df(ops[0], DataFormat.RawUInt32)
+    lc.set_runtime_tensor_transform(ops[0], RuntimeTensorTransform.EmbeddingIndex(ops[0].shape))
 
-    embedding_dim = ops[0].shape.as_list()
+    embedding_dim = ops[1].shape.as_list()
     while len(embedding_dim) < 4:
         embedding_dim = [1] + embedding_dim
 
     forge_attrs = {
-        "num_indices": ops[1].shape[-1],
+        "num_indices": ops[0].shape[-1],
     }
 
-    lc.op(type, ops, (ops[1].shape[-1],), forge_attrs, "", TILE_DIM, TILE_DIM)
+    lc.op(type, ops, (ops[0].shape[-1],), forge_attrs, "", TILE_DIM, TILE_DIM)
 
 
 def decompose(type, attr, dc, inputs):
