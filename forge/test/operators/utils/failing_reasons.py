8dbb799940aeaee1b462132e8c88d2e066708822
Vladimir Brkic <vbrkic@tenstorrent.com>
2024-09-25 20:54:01 +0000

    Validate xfail reason via raised exceptions
    Issue #239

--- forge/test/operators/utils/failing_reasons.py
+++ forge/test/operators/utils/failing_reasons.py
@@ -12,12 +12,18 @@
 # ...
 
 
+from loguru import logger
+from typing import Type, Optional
+
+
 class FailingReasons:
     NOT_IMPLEMENTED = "Not implemented operator"
 
     BUGGY_SHAPE = "Buggy shape"
 
     MICROBATCHING_UNSUPPORTED = "Higher microbatch size is not supported"
+
+    UNSUPPORTED_DATA_FORMAT = "Data format is not supported"
 
     UNSUPPORTED_DIMENSION = "Unsupported dimension"
 
@@ -49,3 +55,73 @@ class FailingReasons:
 
     UNSUPPORTED_INPUT_SOURCE = "Unsupported input source"
 
+
+class FailingReasonsValidation:
+
+    @classmethod
+    def validate_exception_message(cls, exception_value: Exception, expected_message: str, exception_type: Optional[Type[Exception]]):
+        ''' Validate exception message and type
+
+        Args:
+            exception_value (Exception): Raised exception to validate
+            expected_message (str): Expected exception message
+            exception_type (Optional[Type[Exception]]): Expected exception type
+
+        Returns:
+            bool: True if exception message and type match the expected values, False otherwise
+        '''
+        if exception_type is not None:
+            return isinstance(exception_value, exception_type) and f"{exception_value}" == expected_message
+        else:
+            return f"{exception_value}" == expected_message
+
+    # TODO: Add more checks
+    # Unlisted reasons will not be checked
+    XFAIL_REASON_CHECKS = {
+        FailingReasons.UNSUPPORTED_DATA_FORMAT:
+            [
+                # lambda ex: FailingReasonsValidation.validate_exception_message(ex, RuntimeError, "Unsupported data type"),
+                lambda ex: isinstance(ex, RuntimeError) and f"{ex}" == "Unsupported data type",
+            ],
+        FailingReasons.DATA_MISMATCH:
+            [
+                lambda ex: isinstance(ex, AssertionError) and f"{ex}" == "AssertionError: PCC check failed",
+                lambda ex: isinstance(ex, AssertionError) and f"{ex}" == "PCC check failed",
+            ],
+        FailingReasons.UNSUPPORTED_SPECIAL_CASE:
+            [
+                lambda ex: isinstance(ex, AssertionError) and f"{ex}" == "PCC check failed",
+            ],
+        FailingReasons.NOT_IMPLEMENTED:
+            [
+                lambda ex: isinstance(ex, NotImplementedError) and f"{ex}".startswith("The following operators are not implemented:"),
+                lambda ex: isinstance(ex, RuntimeError) and f"{ex}".startswith("Unsupported operation for lowering from TTForge to TTIR:"),
+                lambda ex: isinstance(ex, AssertionError) and f"{ex}" == "Encountered unsupported op types. Check error logs for more details",
+            ],
+    }
+
+    @classmethod
+    def validate_exception(cls, exception_value: Exception, xfail_reason: str):
+        ''' Validate exception based on xfail reason
+
+        Args:
+            exception_value (Exception): Raised exception to validate
+            xfail_reason (str): Xfail reason
+
+        Returns:
+            bool: True if exception message and type match the expected values, False otherwise, None if no check is defined
+        '''
+        logger.debug(f"Validating xfail reason: '{xfail_reason}' for exception: {type(exception_value)} '{exception_value}'")
+
+        if xfail_reason in cls.XFAIL_REASON_CHECKS:
+            xfail_reason_checks = cls.XFAIL_REASON_CHECKS[xfail_reason]
+            # Checking multiple conditions. If any of the conditions is met, return True
+            for xfail_reason_check in xfail_reason_checks:
+                if xfail_reason_check(exception_value):
+                    logger.trace(f"Correct xfail reason: '{xfail_reason}' for exception: {type(exception_value)} '{exception_value}'")
+                    return True
+            logger.error(f"Wrong xfail reason: '{xfail_reason}' for exception: {type(exception_value)} '{exception_value}'")
+            return False
+        else:
+            logger.warning(f"Test is marked with xfail reason: '{xfail_reason}' but no check performed for exception: {type(exception_value)} '{exception_value}'")
+            return None
