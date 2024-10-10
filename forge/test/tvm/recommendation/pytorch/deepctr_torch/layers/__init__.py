57f5e7e1ac886db06572bcffec7a150dd3c27601
Nikola Vukobrat <124874832+nvukobratTT@users.noreply.github.com>
2024-10-10 09:17:11 +0200

    Clenup of old Buda tests. Settingup test directory structure for Forge (#390)
    Fix #389

--- forge/test/tvm/recommendation/pytorch/deepctr_torch/layers/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
-
-# SPDX-License-Identifier: Apache-2.0
-from .interaction import *
-from .core_modules import *
-from .utils import concat_fun
-from .sequence import *
