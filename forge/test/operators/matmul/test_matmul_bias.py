381f36994dded0d1f75040afbeb183a3b9bbc462
mstojkovicTT <mstojkovic@tenstorrent.com>
2024-10-16 10:03:16 +0000

    parametrize linear features

--- forge/test/operators/matmul/test_matmul_bias.py
+++ forge/test/operators/matmul/test_matmul_bias.py
@@ -11,24 +11,21 @@ from torch import nn
 import forge
 from forge.op.eval.common import compare_with_golden_pcc
 
-INPUT_FEATURES = 784
-OUTPUT_DIM = 10
 
-# if batch is >16 fuse_matmul is applied
 @pytest.mark.parametrize("batch_size", [1, 4, 16, 32, 64])
-def test_matmul_bias(batch_size):
+@pytest.mark.parametrize("linear_features", [(784, 10)])
+def test_matmul_bias(batch_size, linear_features):
+    input_features, output_dim = linear_features
+
     class Linear(nn.Module):
         def __init__(self):
             super().__init__()
-            self.l1 = nn.Linear(INPUT_FEATURES, OUTPUT_DIM, bias=True)
+            self.l1 = nn.Linear(input_features, output_dim, bias=True)
 
         def forward(self, a):
             return self.l1(a)
 
-    # global param for fuse_bias
-    os.environ["FORGE_FUSE_MATMUL_BIAS"] = "1"
-
-    inputs = [torch.rand(batch_size, INPUT_FEATURES)]
+    inputs = [torch.rand(batch_size, input_features)]
 
     framework_model = Linear()
     fw_out = framework_model(*inputs)
