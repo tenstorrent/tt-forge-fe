bd9c7b56b397417df53a6c780477f9aee530fcc8
Mateja Stojkovic <mstojkovic@tenstorrent.com>
2024-10-17 14:01:30 +0200

    Changed default usage of fuse_bias (#414)
    * disabling matmul fuse bias by default
    
    * formatting
    
    * parametrize linear features
    
    * add to test_features

--- forge/test/mlir/test_features.py
+++ forge/test/mlir/test_features.py
@@ -64,3 +64,29 @@ def test_input_order(a_shape, b_shape, c_shape):
     co_out = compiled_model(a, b, c)
 
     assert compare_with_golden_pcc(golden=fw_out, calculated=co_out[0][0], pcc=0.99)
+
+
+@pytest.mark.parametrize("batch_size", [1, 4, 16, 32, 64])
+@pytest.mark.parametrize("linear_features", [(784, 10)])
+def test_matmul_bias(batch_size, linear_features):
+    input_features, output_dim = linear_features
+
+    class Linear(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.l1 = nn.Linear(input_features, output_dim, bias=True)
+
+        def forward(self, a):
+            return self.l1(a)
+
+    inputs = [torch.rand(batch_size, input_features)]
+
+    framework_model = Linear()
+    fw_out = framework_model(*inputs)
+
+    compiled_model = forge.compile(framework_model, sample_inputs=inputs)
+    co_out = compiled_model(*inputs)
+
+    co_out = [co.to("cpu") for co in co_out]
+    fw_out = [fw_out] if isinstance(fw_out, torch.Tensor) else fw_out
+    assert all([compare_with_golden_pcc(golden=fo, calculated=co, pcc=0.99) for fo, co in zip(fw_out, co_out)])
