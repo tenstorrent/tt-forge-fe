0c537b40234e0cf577499323adc961ed2c15fabb
mstojkovicTT <mstojkovic@tenstorrent.com>
2024-10-21 09:36:26 +0000

    test for llama32

--- forge/test/mlir/llama/tests/test_llama_mlp.py
+++ forge/test/mlir/llama/tests/test_llama_mlp.py
@@ -5,19 +5,24 @@ import torch
 import pytest
 
 import forge
-from test.mlir.llama.utils.utils import load_model
+from test.mlir.llama.utils.utils import load_model, load_llama32
 from forge.op.eval.common import compare_with_golden_pcc
 
 
-@pytest.mark.xfail()
-def test_llama_mlp():
-    # Load Llama 3B model and tokenizer
-    framework_model, _ = load_model()
+@pytest.mark.parametrize("llama_ver", ["llama 3B", "llama 3.2 1B"])
+@pytest.mark.xfail(reason="Waiting for the transformers version to be upgraded")
+def test_llama_mlp(llama_ver):
+    # Load Llama model and tokenizer
+    if llama_ver == "llama 3B":
+        framework_model, _ = load_model()
+    elif llama_ver == "llama 3.2 1B":
+        framework_model, _ = load_llama32()
     framework_model = framework_model.model.layers[0].mlp
+    hidden_dim = framework_model.hidden_size
 
     # Input samples
     inputs = [
-        torch.rand((1, 12, 3200)),  # Hidden states
+        torch.rand((1, 12, hidden_dim)),  # Hidden states
     ]
 
     # Sanity run
--- forge/test/mlir/llama/tests/test_llama_rotary_emb.py
+++ forge/test/mlir/llama/tests/test_llama_rotary_emb.py
@@ -5,13 +5,14 @@ import torch
 import pytest
 
 import forge
-from test.mlir.llama.utils.utils import load_model
+from test.mlir.llama.utils.utils import load_model, load_llama32
 from transformers.models.llama.modeling_llama import apply_rotary_pos_emb
 from forge.op.eval.common import compare_with_golden_pcc
 
 
-@pytest.mark.xfail()
-def test_llama_rotary_emb():
+@pytest.mark.parametrize("llama_ver", ["llama 3B", "llama 3.2 1B"])
+@pytest.mark.xfail(reason="Waiting for the transformers version to be upgraded")
+def test_llama_rotary_emb(llama_ver):
     class Llama_Rotary_Embedding(torch.nn.Module):
         def __init__(self, model):
             super().__init__()
@@ -20,24 +21,32 @@ def test_llama_rotary_emb():
             self.seq_length = 12
 
         def forward(self, query_states, key_states):
-            kv_seq_len = key_states.shape[-2]
-            cos, sin = self.rotary_emb(key_states, seq_len=kv_seq_len)
             position_ids = torch.arange(
                 self.past_key_values_length,
                 self.seq_length + self.past_key_values_length,
                 dtype=torch.long,
             )
             position_ids = position_ids.unsqueeze(0)
-            query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+            cos, sin = self.rotary_emb(key_states, position_ids)
+            query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
             return query_states, key_states
 
     # Load the model
-    llama_model, _ = load_model()
+    if llama_ver == "llama 3B":
+        llama_model, _ = load_model()
+    elif llama_ver == "llama 3.2 1B":
+        llama_model, _ = load_llama32()
     framework_model = Llama_Rotary_Embedding(llama_model)
     framework_model.eval()
 
     # Input samples
-    batch_size, q_heads, kv_heads, query_seq_len, kv_seq_len, head_dim = 1, 32, 32, 12, 12, 100
+    config = llama_model.config
+    batch_size = 1
+    q_heads = config.num_attention_heads
+    kv_heads = config.num_key_value_heads
+    query_seq_len = framework_model.seq_length
+    kv_seq_len = framework_model.seq_length
+    head_dim = config.hidden_size // config.num_attention_heads
     inputs = [
         torch.rand((batch_size, q_heads, query_seq_len, head_dim)),  # Query states
         torch.rand((batch_size, kv_heads, kv_seq_len, head_dim)),  # Key states
