cfd52e3af72dcdb2c743a8acb656910d4a2d9c5b
Vladimir Milosevic <157983820+vmilosevic@users.noreply.github.com>
2024-10-24 12:21:05 +0200

    Build docker image as part of a workflow (#502)
    Build docker image as part of a workflow
    Similar to : tenstorrent/tt-mlir#928

---
+++ .github/get-docker-tag.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Calculate hash for docker image tag.
+
+# The hash is based on the MLIR docker tag
+if [ -f third_party/tt-mlir/.github/get-docker-tag.sh ]; then
+    MLIR_DOCKER_TAG=$(cd third_party/tt-mlir && .github/get-docker-tag.sh)
+else
+    MLIR_DOCKER_TAG="default-tag"
+fi
+
+# The hash is based on the environment files
+ENV_HASH=$(find env -type f -name "*.txt" -exec cat {} + | sha256sum | cut -d ' ' -f 1)
+
+# The hash is based on the Dockerfile(s)
+DOCKERFILE_HASH=$(find .github -type f -name "Dockerfile.*" -exec cat {} + | sha256sum | cut -d ' ' -f 1)
+
+# Combine the hashes and calculate the final hash
+DOCKER_TAG=$( (echo $MLIR_DOCKER_TAG; echo $ENV_HASH; echo $DOCKERFILE_HASH) | sha256sum | cut -d ' ' -f 1)
+echo dt-$DOCKER_TAG
