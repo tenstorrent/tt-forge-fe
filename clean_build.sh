118764e6248c143c796cfb33dc7512e59f3534d9
Predrag Ilkic <pilkic@tenstorrent.com>
2024-10-24 12:09:26 +0200

    [build] fixup script for cleaning the build
    The way `tt-mlir` builds the environment and the toolchain has changed,
    so now if you delete the environment directories, everything will need
    to be rebuilt (including LLVM, Flatbuffers, etc.).
    
    Since cleaning everything (including toolchains) is rarely needed,
    update the script to leave `env/build` dirs intact and rename it to
    `clean_build.sh`.
    
    Also, tidying up the build docs...

---
+++ clean_build.sh
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+# Cleans all build artifacts, except for the environment and toolchains.
+# If you want to clean the environment (should be rarely needed), remove the env/build directories.
+# E.g. rm -rf env/build third_party/tt-mlir/env/build
+
+rm -rf build
+rm -rf third_party/tt-mlir/build
+rm -rf third_party/tt-mlir/third_party/tt-metal
+rm -rf third_party/tvm/build
