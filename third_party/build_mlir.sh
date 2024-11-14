a9e25d2369b9c07a77772bc3fe3e3916f340343a
Predrag Ilkic <148892209+pilkicTT@users.noreply.github.com>
2024-11-14 15:38:19 +0100

    [build] option to build tt-mlir runtime debug (#687)
    Using `-DTTMLIR_RUNTIME_DEBUG=ON` you can choose to build `tt-mlir`
    runtime debug tools.
    
    If you need additional logging in `Release` build, this can be handy.
    Since it enables all debug/trace level logs.
    
    If you are already in `Debug` build you can use the environment var
    `TTMLIR_RUNTIME_LOGGER_LEVEL` to enable debug/trace level logs.

--- third_party/build_mlir.sh
+++ third_party/build_mlir.sh
@@ -8,8 +8,9 @@ source env/activate
 build_type=${BUILD_TYPE:-Release}
 c_compiler=${C_COMPILER:-clang}
 cxx_compiler=${CXX_COMPILER:-clang++}
+tt_runtime_debug=${TTMLIR_RUNTIME_DEBUG:-OFF}
 
 source env/activate
-cmake -G Ninja -B build -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_C_COMPILER=$c_compiler -DCMAKE_CXX_COMPILER=$cxx_compiler -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DTTMLIR_ENABLE_RUNTIME=ON
+cmake -G Ninja -B build -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_C_COMPILER=$c_compiler -DCMAKE_CXX_COMPILER=$cxx_compiler -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DTTMLIR_ENABLE_RUNTIME=ON -DTT_RUNTIME_DEBUG=$tt_runtime_debug
 
 cmake --build build
