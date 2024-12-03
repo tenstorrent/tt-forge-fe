5902eece4e484cd6ad519589837110ce183cb9c9
Predrag Ilkic <148892209+pilkicTT@users.noreply.github.com>
2024-12-03 14:13:26 +0100

    uplift tt-mlir @ 0640c7c8 (#824)

--- forge/test/mlir/llama/tests/test_llama_self_attn.py
+++ forge/test/mlir/llama/tests/test_llama_self_attn.py
@@ -10,7 +10,6 @@ from forge.op.eval.common import compare_with_golden_pcc
 
 
 @pytest.mark.parametrize("model_path", ["openlm-research/open_llama_3b", "meta-llama/Llama-3.2-1B"])
-@pytest.mark.xfail()
 @pytest.mark.push
 def test_llama_self_attn(model_path):
     if model_path == "meta-llama/Llama-3.2-1B":
