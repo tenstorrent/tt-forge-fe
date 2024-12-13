75ede04e1632b684e3e20ae8e2ce11bd8e7e9785
Pavle Glusac <pglusac@tenstorrent.com>
2024-12-13 16:38:32 +0100

    Add MSE loss (#868)
    * Add MSE loss
    
    * Add shape validation and reduction handling for loss functions
    
    * Improve shape validation error messages
    
    * Reformat

--- forge/test/mlir/test_loss.py
+++ forge/test/mlir/test_loss.py
@@ -12,21 +12,23 @@ import forge
 @pytest.mark.parametrize(
     "prediction_shape",
     [
+        (33,),
+        (128,),
         (3, 5),
         (32, 32),
         (33, 127),
         (128, 20),
     ],
 )
+@pytest.mark.parametrize("reduction", ["mean", "sum"])
 @pytest.mark.push
-def test_l1_loss(prediction_shape):
-    forge_loss = forge.op.loss.L1Loss("l1_loss")
-    torch_loss = torch.nn.L1Loss()
+def test_l1_loss(prediction_shape, reduction):
+    forge_loss = forge.op.loss.L1Loss("l1_loss", reduction=reduction)
+    torch_loss = torch.nn.L1Loss(reduction=reduction)
 
     prediction = torch.randn(prediction_shape, requires_grad=True)
     prediction_forge = forge.tensor.Tensor.create_from_torch(prediction)
-    target = torch.empty(prediction_shape[0], dtype=torch.long).random_(prediction_shape[1])
-    target = nn.functional.one_hot(target, num_classes=prediction_shape[1]).float()
+    target = torch.randn((prediction_shape))
     target_forge = forge.tensor.Tensor.create_from_torch(target)
 
     forge_loss = forge.compile(forge_loss, sample_inputs=[prediction_forge, target_forge])
@@ -61,3 +63,35 @@ def test_cross_entropy_loss(prediction_shape):
     torch_loss_out = torch_loss(prediction, target)
 
     assert torch.allclose(torch_loss_out, forge_loss_out[0], rtol=11e-3)
+
+
+@pytest.mark.parametrize(
+    "prediction_shape",
+    [
+        (33,),
+        (128,),
+        (2, 2),
+        (3, 5),
+        (32, 32),
+        (33, 127),
+        (128, 20),
+        (128, 128),
+    ],
+)
+@pytest.mark.parametrize("reduction", ["mean", "sum"])
+def test_mse_loss(prediction_shape, reduction):
+    forge_loss = forge.op.loss.MSELoss("mse_loss", reduction=reduction)
+    torch_loss = torch.nn.MSELoss(reduction=reduction)
+
+    prediction = torch.randn(prediction_shape, requires_grad=True)
+    prediction_forge = forge.tensor.Tensor.create_from_torch(prediction)
+    target = torch.randn((prediction_shape))
+    target_forge = forge.tensor.Tensor.create_from_torch(target)
+
+    forge_loss = forge.compile(forge_loss, sample_inputs=[prediction_forge, target_forge])
+    forge_loss_out = forge_loss(prediction, target)
+    torch_loss_out = torch_loss(prediction, target)
+
+    assert torch.allclose(
+        torch_loss_out, forge_loss_out[0], rtol=5e-2, atol=5e-3
+    )  # relative tolerance is 5% and absolute tolerance is 0.005
