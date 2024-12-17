66e71e1f7456b421bd666fbb7088496db7b13f16
Pavle Glusac <pglusac@tenstorrent.com>
2024-12-17 10:45:23 +0000

    Add BCE loss

--- forge/test/mlir/test_loss.py
+++ forge/test/mlir/test_loss.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from forge.verify.compare import compare_with_golden
 import pytest
 import torch
 from torch import nn
@@ -95,3 +96,34 @@ def test_mse_loss(prediction_shape, reduction):
     assert torch.allclose(
         torch_loss_out, forge_loss_out[0], rtol=5e-2, atol=5e-3
     )  # relative tolerance is 5% and absolute tolerance is 0.005
+
+
+@pytest.mark.parametrize(
+    "prediction_shape",
+    [
+        (33,),
+        (128,),
+        (2, 2),
+        (3, 5),
+        (32, 32),
+        (33, 127),
+        (128, 20),
+        (128, 128),
+    ],
+)
+@pytest.mark.parametrize("reduction", ["sum", "mean"])
+def test_bce_loss(prediction_shape, reduction):
+    forge_loss = forge.op.loss.BCELoss("bce_loss", reduction=reduction)
+    torch_loss = torch.nn.BCELoss(reduction=reduction)
+
+    prediction = nn.functional.sigmoid(torch.randn(prediction_shape, requires_grad=True))
+    target = torch.rand(prediction_shape)
+
+    prediction_forge = forge.tensor.Tensor.create_from_torch(prediction)
+    target_forge = forge.tensor.Tensor.create_from_torch(target)
+
+    forge_loss = forge.compile(forge_loss, sample_inputs=[prediction_forge, target_forge])
+    forge_loss_out = forge_loss(prediction, target)
+    torch_loss_out = torch_loss(prediction, target)
+
+    assert torch.allclose(torch_loss_out, forge_loss_out[0], rtol=5e-2, atol=5e-3)
