99678b6b14905e2a66410f22d927a735ed008d49
Pavle Glusac <pglusac@tenstorrent.com>
2024-12-19 10:55:53 +0000

    Add back the MSE loss test

--- forge/test/mlir/test_loss.py
+++ forge/test/mlir/test_loss.py
@@ -79,6 +79,38 @@ def test_cross_entropy_loss(prediction_shape):
     ],
 )
 @pytest.mark.parametrize("reduction", ["mean", "sum"])
+def test_mse_loss(prediction_shape, reduction):
+    forge_loss = forge.op.loss.MSELoss("mse_loss", reduction=reduction)
+    torch_loss = torch.nn.MSELoss(reduction=reduction)
+
+    prediction = torch.randn(prediction_shape, requires_grad=True)
+    prediction_forge = forge.tensor.Tensor.create_from_torch(prediction)
+    target = torch.randn((prediction_shape))
+    target_forge = forge.tensor.Tensor.create_from_torch(target)
+
+    forge_loss = forge.compile(forge_loss, sample_inputs=[prediction_forge, target_forge])
+    forge_loss_out = forge_loss(prediction, target)
+    torch_loss_out = torch_loss(prediction, target)
+
+    assert torch.allclose(
+        torch_loss_out, forge_loss_out[0], rtol=5e-2, atol=5e-3
+    )  # relative tolerance is 5% and absolute tolerance is 0.005
+
+
+@pytest.mark.parametrize(
+    "prediction_shape",
+    [
+        (33,),
+        (128,),
+        (2, 2),
+        (3, 5),
+        (32, 32),
+        (33, 127),
+        (128, 20),
+        (128, 128),
+    ],
+)
+@pytest.mark.parametrize("reduction", ["mean", "sum"])
 def test_nll_loss(prediction_shape, reduction):
     forge_loss = forge.op.loss.NLLLoss("nll_loss", reduction=reduction)
     torch_loss = torch.nn.NLLLoss(reduction=reduction)
