c69c2d9d36f50f753ee17bc4f435a68a46520ec7
Predrag Ilkic <pilkic@tenstorrent.com>
2024-12-20 15:55:46 +0100

    [build & ci] tt-forge-fe wheel
    This change enables creating a `tt-forge-fe` wheel. Additionally, CI
    `build-and-test` workflow is modified to have one job which creates a
    python wheel (build) and then other jobs which are executing the tests
    (using the previously generated wheel).
    
    Logic for creating the wheel is contained in `setup.py`. It simply runs
    cmake to build and install the project. The `setuptools` then picks up
    and creates the wheel.
    
    Installing `tt-mlir` via `cmake --install build --component SharedLib`
    installs all needed dependencies for `tt-mlir` (including `tt-metal`).
    
    `TT_METAL_HOME` environment variable is no longer needed, small piece of
    logic in `forge/forge/__init__.py` tries to deduce it, relative to its
    location. If it can't be deduced, an error message will be shown.
    
    However, if the `TT_METAL_HOME` variable is set the logic will respect
    it as long as it is a valid path, so overriding it in custom scenarios
    is possible.
    
    Closes #200

--- setup.py
+++ setup.py
@@ -2,128 +2,94 @@
 
 # SPDX-License-Identifier: Apache-2.0
 import os
-import re
-import sys
-import sysconfig
-import platform
+import pathlib
 import subprocess
-
-__requires__ = ["pip >= 24.0"]
-
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
-
 
 forge_files = {"test": {"path": "forge/test", "files": ["conftest.py", "__init__.py", "utils.py", "test_user.py"]}}
 
 
 class TTExtension(Extension):
     def __init__(self, name):
-        Extension.__init__(self, name, sources=[])
+        super().__init__(name, sources=[])
 
 
-class MyBuild(build_ext):
+class CMakeBuild(build_ext):
     def run(self):
         for ext in self.extensions:
-            fullname = self.get_ext_fullname(ext.name)
-            filename = self.get_ext_filename(fullname)
-            build_lib = self.build_lib
-            if not os.path.exists(build_lib):
-                continue  # editable install?
-
-            # Build using our make flow, and then copy the file over
-
-            # Pass the required variables for building Wormhole or Grayskull
-            if "BACKEND_ARCH_NAME" not in os.environ:
-                print("Please provide environment variable `BACKEND_ARCH_NAME` to the build process.")
-                sys.exit(1)
-
-            additional_env_variables = {
-                "BACKEND_ARCH_NAME": os.environ.get("BACKEND_ARCH_NAME"),
-            }
-            env = os.environ.copy()
-            env.update(additional_env_variables)
-            nproc = os.cpu_count()
-            subprocess.check_call(
-                ["make", f"-j{nproc}", "forge", r"DEVICE_VERSIM_INSTALL_ROOT=\$$ORIGIN/../.."], env=env
-            )
-
-            src = "build/lib/libforge_csrc.so"
-            self.copy_file(src, os.path.join(build_lib, filename))
-
-            self._copy_forge(build_lib)
-
-    def _copy_forge(self, target_path):
-
-        for t, d in forge_files.items():
-            path = target_path + "/" + d["path"]
-            os.makedirs(path, exist_ok=True)
-
-            src_path = d["path"]
-            if d["files"] == "*":
-                self.copy_tree(src_path, path)
+            if "forge" in ext.name:
+                self.build_forge(ext)
             else:
-                for f in d["files"]:
-                    self.copy_file(src_path + "/" + f, path + "/" + f)
+                raise Exception("Unknown extension")
+
+    def build_forge(self, ext):
+        build_lib = self.build_lib
+        if not os.path.exists(build_lib):
+            # Might be an editable install or something else
+            return
+
+        extension_path = pathlib.Path(self.get_ext_fullpath(ext.name))
+        print(f"Running cmake to install forge at {extension_path}")
+
+        cwd = pathlib.Path().absolute()
+        build_dir = cwd / "build"
+        install_dir = extension_path.parent / "forge"
+
+        cmake_args = [
+            "-G",
+            "Ninja",
+            "-B",
+            str(build_dir),
+            "-DCMAKE_BUILD_TYPE=Release",
+            "-DCMAKE_INSTALL_PREFIX=" + str(install_dir),
+            "-DCMAKE_C_COMPILER=clang",
+            "-DCMAKE_CXX_COMPILER=clang++",
+            "-DTTMLIR_RUNTIME_DEBUG=OFF",
+            "-DCMAKE_C_COMPILER_LAUNCHER=ccache",
+            "-DCMAKE_CXX_COMPILER_LAUNCHER=ccache",
+        ]
+
+        self.spawn(["cmake", *cmake_args])
+        self.spawn(["cmake", "--build", str(build_dir)])
+        self.spawn(["cmake", "--install", str(build_dir)])
 
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
-# Read the requirements from the core list that is shared between
-# dev and test.
-with open("python_env/core_requirements.txt", "r") as f:
-    requirements = f.read().splitlines()
+# Compute requirements
+with open("env/core_requirements.txt", "r") as f:
+    core_requirements = f.read().splitlines()
 
-# Add specific requirements for distribution
-# due to how we use the requirements file we can not use include requirements files
-with open("python_env/dist_requirements.txt", "r") as f:
-    requirements += [r for r in f.read().splitlines() if not r.startswith("-r")]
+with open("env/linux_requirements.txt", "r") as f:
+    linux_requirements = [r for r in f.read().splitlines() if not r.startswith("-r")]
 
-# forge._C
-forge_c = TTExtension("forge._C")
+requirements = core_requirements + linux_requirements
 
-
-ext_modules = [forge_c]
-
-packages = [p for p in find_packages("forge") if not p.startswith("test")]
-
+# Compute a dynamic version from git
 short_hash = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("ascii").strip()
 date = (
     subprocess.check_output(["git", "show", "-s", "--format=%cd", "--date=format:%y%m%d", "HEAD"])
     .decode("ascii")
     .strip()
 )
+version = "0.1." + date + "+dev." + short_hash
 
-arch_codes = {"wormhole_b0": "wh_b0", "grayskull": "gs", "blackhole": "bh"}
-arch_code = arch_codes[os.environ["BACKEND_ARCH_NAME"]]
+forge_c = TTExtension("forge")
 
-version = "0.1." + date + "+dev." + arch_code + "." + short_hash
+# Find packages as before
+packages = [p for p in find_packages("forge") if not p.startswith("test")]
 
 setup(
     name="forge",
     version=version,
-    author="Tenstorrent",
-    url="http://www.tenstorrent.com",
-    author_email="info@tenstorrent.com",
-    description="AI/ML framework for Tenstorrent devices",
-    python_requires=">=3.8",
+    install_requires=requirements,
     packages=packages,
-    package_data={"forge": ["tti/runtime_param_yamls/*.yaml"]},
     package_dir={"forge": "forge/forge"},
+    ext_modules=[forge_c],
+    cmdclass={"build_ext": CMakeBuild},
     long_description=long_description,
     long_description_content_type="text/markdown",
-    ext_modules=ext_modules,
-    cmdclass=dict(build_ext=MyBuild),
     zip_safe=False,
-    install_requires=requirements,
-    license="TBD",
-    keywords="forge machine learning tenstorrent",
-    # PyPI
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    ],
 )
