019e02aaeaeae166d8e3c581ccee9a380749bdd9
Mateja Stojkovic <mstojkovic@tenstorrent.com>
2025-01-08 16:22:06 +0100

    Remove xfail from embedding test (#1019)
    fixes: #688

--- forge/test/mlir/llama/tests/test_llama_embedding.py
+++ forge/test/mlir/llama/tests/test_llama_embedding.py
@@ -12,8 +12,6 @@ from forge.verify.verify import verify
 @pytest.mark.parametrize("model_path", ["openlm-research/open_llama_3b", "meta-llama/Llama-3.2-1B"])
 @pytest.mark.push
 def test_llama_embedding(model_path):
-    if model_path == "meta-llama/Llama-3.2-1B":
-        pytest.xfail("meta-llama/Llama-3.2-1B embedding is not supported yet")
     # Load Llama model and tokenizer
     framework_model, _ = load_model(model_path)
 
