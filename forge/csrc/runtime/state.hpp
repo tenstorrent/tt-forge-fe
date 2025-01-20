b29681a5994d318292d7e7bdda2202d8c4a5dae9
Predrag Ilkic <pilkic@tenstorrent.com>
2025-01-20 10:43:01 +0100

    [runtime] tensors on device aka runtime stitching
    This change implements a mechanism to keep certain tensors on device, so
    that running programs in a loop (or chaining multiple programs) is more
    efficient. We call this - runtime stitching.
    
    Previously, we were sending each input to device each time we ran a
    program - which is problematic, since this includes all of the weights,
    which for example remain the same during multiple invocations of the
    forward program (in inference) and don't need to be sent to the device
    each time.
    
    To keep the tensors on the device we only need to hold on to the
    handles of the tensors provided by the device runtime (`tt-mlir`
    runtime) so that the tensors don't get deallocated; and pass them on
    when needed for executing a program. This change introduces several
    structures for enabling us to that. Tensors which are kept on the device
    are called `persistent`.
    
    Summary of the new structures:
    - `ModelState` which holds info for running the model (module), i.e.
      multiple programs (e.g. tensor pool, all program states)
    - `ProgramState` - holds info needed for running a particular program
      (e.g. persistent input tensors).
    - `TensorPool` simple container for holding named tensors; in the
      context of the `ModelState` it will contain all `persistent` tensors.
    - `Tensor` which represents forge runtime tensor and holds both the
      handle to the runtime tensor (if exists) and the host storage handle -
      the torch tensor from which it was created. This tensor object is
      similar to the torch tensor, in the sense that the object contains
      (in this case `shared_ptr`) pointer to the actual tensor data. Thus,
      copying a tensor only creates a new handle to the underlaying data.
    
    The rest of the change is minimal refactor of existing code to
    incorporate new structures. Additionally, some of the training tests
    were modified so that they remain functional; additional handling is
    needed to ensure proper updating of host/device weight tensors, because
    training tests combine executing some parts of the graphs/modules both
    on the device and cpu.
    
    There is no additional test coverage added and the correctness relies on
    the existing e2e tests. More changes are yet to come with the goal of
    creating a more performant and robust forge runtime. More fine grained
    functional tests of the runtime component will be added.
    
    Closes #1245, Closes #1329

---
+++ forge/csrc/runtime/state.hpp
@@ -0,0 +1,83 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <optional>
+
+#include "runtime/runtime.hpp"
+#include "runtime/tensor.hpp"
+#include "tt/runtime/types.h"
+
+namespace tt
+{
+
+enum class ProgramType : uint32_t
+{
+    Forward = 0,
+    Backward,
+    Optimizer,
+    Count  // Keep this last
+};
+
+std::ostream& operator<<(std::ostream& os, ProgramType program_type);
+
+constexpr size_t PROGRAM_TYPE_COUNT = static_cast<std::underlying_type_t<ProgramType>>(ProgramType::Count);
+
+constexpr auto program_idx(ProgramType program_type)
+{
+    TT_ASSERT(program_type < ProgramType::Count, "Invalid program type");
+    return static_cast<std::underlying_type_t<ProgramType>>(program_type);
+}
+
+// Simple struct to hold the state of a program.
+struct ProgramState
+{
+    ProgramType program_type;
+    std::vector<tt::Tensor> persistent_inputs;
+    std::vector<tt::Tensor> outputs;
+};
+
+// Encapsulates execution context for a model.
+struct ModelState
+{
+    // Handle to the flatbuffer binary which contains the model.
+    runtime::Binary binary;
+
+    // Static array of program states, one for each program type.
+    std::array<std::optional<ProgramState>, PROGRAM_TYPE_COUNT> program_states;
+
+    // Tensor pool containing tensors shared between different programs or program invocations.
+    // E.g. weights, constants, etc.
+    //
+    // These tensors will be pushed to the device the first time they are used in a program (as inputs to the program).
+    // They will be kept on the device until the runtime/user decides to remove them from the device. In that case on
+    // next use they will be pushed to the device again.
+    TensorPool tensor_pool;
+
+    ModelState(runtime::Binary binary) : binary{binary}, program_states{}, tensor_pool{} {}
+
+    // Disallow copy construction and assignment.
+    ModelState(const ModelState&) = delete;
+    ModelState(ModelState&&) = default;
+    ModelState& operator=(const ModelState&) = delete;
+
+    TensorPool& get_tensor_pool() { return tensor_pool; }
+
+    void add_program_state(ProgramState& program_state)
+    {
+        auto program_type = program_state.program_type;
+        auto pidx = program_idx(program_type);
+
+        program_states[pidx] = program_state;
+    }
+
+    void run_program(ProgramType program_type, std::vector<tt::Tensor> act_inputs);
+};
+
+ProgramState create_program_state(
+    ProgramType program_type, const TensorPool& tensor_pool, std::vector<std::string> persistent_input_names);
+
+}  // namespace tt
