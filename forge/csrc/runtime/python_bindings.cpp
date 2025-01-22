c7ae16cc39bd87bb19fec4aeec98f63d0d8c1efd
Predrag Ilkic <pilkic@tenstorrent.com>
2025-01-22 12:33:55 +0100

    working version

--- forge/csrc/runtime/python_bindings.cpp
+++ forge/csrc/runtime/python_bindings.cpp
@@ -5,7 +5,6 @@
 #include "runtime/python_bindings.hpp"
 
 #include "runtime/runtime.hpp"
-#include "runtime/tensor.hpp"
 #include "tt/runtime/types.h"
 
 namespace tt
@@ -17,12 +16,16 @@ void RuntimeModule(py::module& m_runtime)
         .def("get_program_inputs", &runtime::Binary::getProgramInputs)
         .def("get_program_outputs", &runtime::Binary::getProgramOutputs);
     m_runtime.def("run_binary", tt::run_binary);
+    m_runtime.def("run_binary_v2", tt::run_binary_v2);
 
+    py::class_<Tensor>(m_runtime, "Tensor").def(py::init<torch::Tensor&>()).def(py::init<runtime::Tensor&>());
     py::class_<TensorPool>(m_runtime, "TensorPool")
+        .def(py::init<>())
         .def("get_tensor", &TensorPool::get_tensor)
         .def(
             "insert",
-            [](TensorPool& self, const std::string& name, torch::Tensor& tensor) { self.insert(name, tensor); });
+            [](TensorPool& self, const std::string& name, torch::Tensor& tensor) { self.insert(name, tensor); })
+        .def("update_tensor", &TensorPool::update_tensor);
 }
 
 }  // namespace tt
