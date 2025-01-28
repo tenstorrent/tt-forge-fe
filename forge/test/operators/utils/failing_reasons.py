f5c1fa7fca7188768f916d33be4dafe138e13705
Vladica Obojevic <vobojevic@tenstorrent.com>
2025-01-28 08:46:51 +0000

    Reshape op - failing rules

--- forge/test/operators/utils/failing_reasons.py
+++ forge/test/operators/utils/failing_reasons.py
@@ -166,6 +166,9 @@ class FailingReasonsValidation:
             and "Statically allocated circular buffers on core range [(x=0,y=0) - (x=0,y=0)] grow to 2663200 B which is beyond max L1 size of 1499136 B"
             in f"{ex}",
             lambda ex: isinstance(ex, RuntimeError)
+            and "Statically allocated circular buffers on core range [(x=0,y=0) - (x=7,y=7)] grow to 28100144 B which is beyond max L1 size of 1499136 B"
+            in f"{ex}",
+            lambda ex: isinstance(ex, RuntimeError)
             and "Index is out of bounds for the rank, should be between 0 and 0 however is 1" in f"{ex}",
             lambda ex: isinstance(ex, RuntimeError)
             and "293 unique+common runtime args targeting kernel reader_concat_stick_layout_interleaved_start_id on (x=0,y=0) are too large. Max allowable is 256"
