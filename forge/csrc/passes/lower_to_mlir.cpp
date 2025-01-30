f1d70c8bbea1b273d661f53e4c6ab0057e69aefe
Vladimir Canic <vcanic@tenstorrent.com>
2025-01-30 09:58:56 +0000

    Enable printing of debug info in TTIR

--- forge/csrc/passes/lower_to_mlir.cpp
+++ forge/csrc/passes/lower_to_mlir.cpp
@@ -167,7 +167,6 @@ class MLIRGenerator
         // save what's dumped to a file named "{file_name}.mlir"
         reportify::dump_mlir("ttir", graphModule_.getNameAttr().getValue().str(), graphModule_.getOperation());
 
-#ifdef DEBUG
         // Create a string to store the output
         std::string moduleStr;
         llvm::raw_string_ostream rso(moduleStr);
@@ -179,8 +178,7 @@ class MLIRGenerator
 
         rso.flush();
 
-        log_trace(LogMLIRCompiler, "MLIR module after lowering ForgeGraphModule:\n{}", moduleStr);
-#endif
+        log_debug(LogMLIRCompiler, "MLIR module after lowering ForgeGraphModule:\n{}", moduleStr);
 
         return graphModule_;
     }
