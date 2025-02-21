f2230022bdd0257b04d531f7e2e7481fad196b93
Predrag Ilkic <pilkic@tenstorrent.com>
2025-02-21 11:59:23 +0100

    FOR_REMOVAL: quick benchmarking hacks

--- forge/test/mlir/llama/tests/test_llama_prefil.py
+++ forge/test/mlir/llama/tests/test_llama_prefil.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 # SPDX-License-Identifier: Apache-2.0
-
+import time
 import torch
 import pytest
 from transformers import LlamaTokenizer
@@ -56,8 +56,8 @@ def test_llama_prefil_on_device_decode_on_cpu(model_path):
     - The first part is the prefilling of the model on the device.
     - The second part is the decoding of the model on the CPU without KV cache.
     """
-    if model_path == "openlm-research/open_llama_3b":
-        pytest.skip("Insufficient host DRAM to run this model (requires a bit more than 32 GB during compile time)")
+    # if model_path == "openlm-research/open_llama_3b":
+    #     pytest.skip("Insufficient host DRAM to run this model (requires a bit more than 32 GB during compile time)")
 
     # Load Llama model and tokenizer
     model, tokenizer = load_model(model_path, return_dict=True)
@@ -73,33 +73,16 @@ def test_llama_prefil_on_device_decode_on_cpu(model_path):
     compiled_decoder = forge.compile(model_decoder, sample_inputs=input_ids)
 
     # Prefill Phase - Process the initial prompt on device
-    transformer_outputs = compiled_decoder(input_ids)
-    # Get hidden states for all tokens from the last "transformer layer".
-    hidden_states_compiled = transformer_outputs[0]
-    hidden_states_compiled = hidden_states_compiled.to("cpu")
+    first_transformer_outputs = compiled_decoder(input_ids)
+    print(f"Transformer outputs: {first_transformer_outputs}")
+    # forge.verify.verify.verify(input_ids, model_decoder, compiled_decoder)
 
-    # Get hidden states for all tokens from the last "transformer layer" calculated on CPU.
-    hidden_states_framework = prefil_on_cpu(model, input_ids)
+    start_time = time.time()
+    for _ in range(32):
+        transformer_outputs = compiled_decoder(input_ids)
+    end_time = time.time()
+    print(f"Time per batch (prefill): {(end_time - start_time) / 32}")
 
-    # Compare result of prefilling on device with the result of prefilling on CPU.
-    # Calculate the pcc for only the last vector in the hidden states tensor.
-    assert compare_with_golden(hidden_states_framework[:, -1, :], hidden_states_compiled[:, -1, :])
+    last_transformer_outputs = compiled_decoder(input_ids)
 
-    # Decode Phase - Generate new tokens
-    max_new_tokens = 46
-    output_ids_compiled, output_logits_compiled = decode_on_cpu(
-        model, tokenizer, input_ids, hidden_states_compiled, max_new_tokens
-    )
-    _, output_logits_framework = decode_on_cpu(model, tokenizer, input_ids, hidden_states_framework, max_new_tokens)
-
-    # Compare the logits of the generated tokens with the golden values from CPU.
-    assert all(
-        [
-            compare_with_golden(golden=out_logits_fw, calculated=out_logits_tt)
-            for out_logits_fw, out_logits_tt in zip(output_logits_framework, output_logits_compiled)
-        ]
-    )
-
-    # Generated text
-    generated_text_compiled = tokenizer.decode(output_ids_compiled[0], skip_special_tokens=True)
-    print(generated_text_compiled)
+    assert torch.allclose(first_transformer_outputs[0], last_transformer_outputs[0])
