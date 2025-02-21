1268154947f823aca065172ef1b2fdb8529b1bed
vkovinicTT <vkovinic@tenstorrent.com>
2025-02-21 15:56:58 +0000

    moved forge_compile from tvm to forge

---
+++ forge/forge/tvm_passes/__init__.py
@@ -0,0 +1,11 @@
+
+"""
+TVM passes package for forge compilation.
+Contains compilation all the functions required to interact with TVM.
+(What was previously in /contrib/ folder in TVM)
+"""
+
+# Explicitly expose the function
+from .forge_compile import load_tvm_graph
+
+
