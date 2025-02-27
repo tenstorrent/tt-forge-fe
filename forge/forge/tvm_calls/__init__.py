dff2dabbd214ecefbecf680de3c79d5a0e25c07b
Vanja Kovinić <vkovinic@tenstorrent.com>
2025-02-27 10:30:31 +0100

    TVM cleanup (#1267)
    ## Result
    
    Compiler logic moved from `tvm` to `forge-fe`.
    
    ## Comprehensive overview
    
    This is only the first cleaning pass where core compiler logic was
    transfered from tvm to the forge-fe. For comprehensive overview, TVM
    will be cleaned in a few stages.
    
    **Coresponding PR in
    [tvm](https://github.com/tenstorrent/tt-tvm/pull/67).**
    
    ### Moved following files to `forge-fe` repo:
    - `python/tvm/contrib/forge_compile.py`
    - `python/tvm/contrib/forge_utils.py`
    - `python/tvm/relay/op/contrib/forge/__init__.py`
    - `python/tvm/relay/op/contrib/forge/forge.py`
    - `python/tvm/relay/op/contrib/forge/relay_passes.py`
    - `python/tvm/relay/op/contrib/forge/reportify.py`
    - `python/tvm/relay/op/contrib/forge/utils.py`.
    
    Coresponding structure:
    ```bash
    forge/forge
    │
    ├── tvm_calls
    │   ├── forge_utils.py
    │   ├── forge_compile.py
    │   └── relay
    │       ├── op
    │       │   ├── forge_passes.py
    │       │   ├── forge.py
    │       │   ├── relay_passes.py
    │       │   ├── reportify.py
    │       │   └── utils.py
    ```
    
    #### **Aditional notes:
    These files were just moved to our repo so once uplift is done, we
    should probably refactor this code into more sensible modules.

---
+++ forge/forge/tvm_calls/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+TVM passes package for forge compilation.
+Contains compilation all the functions required to interact with TVM.
+(What was previously in /contrib/ folder in TVM)
+"""
