0cde875b11b0f31961d60dde2f1422697ed0b076
Vladimir Milosevic <157983820+vmilosevic@users.noreply.github.com>
2025-02-27 12:18:48 +0100

    Generate and update models ops tests and reimplement the models ops test failure updation script (#1234)
    ### Summary:
    1. Generated models ops tests by extracting the unique ops
    configurations across all the pytorch models present inside the
    forge/test/models directory path.
    2. Skipped avgpool1d, avgpool3d, conv2d, pad and reshape ops test cases
    due to segmentation fault in CI and transpose ops test cases due to
    Insufficient host DRAM (requires 30 GB of memory)
    3. Reimplemented the model ops test failure updation script:
    The `scripts/model_analysis/models_ops_test_failure_update.py` script
    automates the extraction of detailed test **failure information** from
    pytest logs by identifying test cases along with their statuses
    (**FAILED, XFAIL, or SKIP**) and corresponding **error messages**, then
    compiles this data into a comprehensive **Excel report** with columns
    for **test cases**, **marker statuses**, and **reasons**. This report
    enables users to manually refine error messages by updating the "Reason"
    column, and once revised, the script can be rerun using the
    `--use_report` option with the `report file path` to automatically
    update the model ops test files with marker and more descriptive failure
    messages
    4. The adv_index ops test are failing with indexerror out of bound
    error(i.e `IndexError: index 563 is out of bounds for dimension 0 with
    size 448`) in evaluation. The adv_index ops takes two inputs one is
    reference tensor and other is indicies tensor based upon the indicies
    tensor values will pick the reference tensor in respective dim but the
    indicies values exceeds the range of reference tensor. To resolve these
    issue, added pytest param max_int which will be used by the
    create_from_shape method in forge/forge/tensor.py to generate the
    indices tensor values with the range of the reference tensor
    
    ### Process:
    1. Triggered the nightly models ops pipeline for collecting the pytest
    logs
    **Pipeline:**
    https://github.com/tenstorrent/tt-forge-fe/actions/runs/13536357567
    **Collected logs:**
    
    [test-log-n150-1](https://github.com/tenstorrent/tt-forge-fe/actions/runs/13536357567/artifacts/2653622726)
    
    [test-log-n150-2](https://github.com/tenstorrent/tt-forge-fe/actions/runs/13536357567/artifacts/2653622477)
    
    [test-log-n150-3](https://github.com/tenstorrent/tt-forge-fe/actions/runs/13536357567/artifacts/2653619334)
    
    [test-log-n150-4](https://github.com/tenstorrent/tt-forge-fe/actions/runs/13536357567/artifacts/2653707424)
    
    2. Using the collected pytest logs, generated excel report by using
    `scripts/model_analysis/models_ops_test_failure_update.py` script
    **Command:** 
    `python scripts/model_analysis/models_ops_test_failure_update.py
    --log_files ci_logs/pytest_1.log ci_logs/pytest_2.log
    ci_logs/pytest_3.log ci_logs/pytest_4.log`
    **Generated models ops test report:** 
    
    [model_ops_tests_report.xlsx](https://github.com/user-attachments/files/18982115/model_ops_tests_report.xlsx)
    
    
    3. Anlalyzed the excel report and update the models ops tests with xfail
    marker and failure reason by using the
    `scripts/model_analysis/models_ops_test_failure_update.py` script with
    `use_report` option enabled
    **Command:** 
    `python scripts/model_analysis/models_ops_test_failure_update.py
    --report_file_path model_ops_tests_report.xlsx --use_report`
    
    4. Triggered the nightly models ops pipeline and all the test are
    passed.
    **Pipeline:**
    https://github.com/tenstorrent/tt-forge-fe/actions/runs/13540060061
    
    
    ### Note: 
    This models ops test takes around 2hrs to run in push pipeline, so
    created a separate workflow to run this model ops test-
    https://github.com/tenstorrent/tt-forge-fe/pull/1316. So renamed the
    model ops test marker from `push` to `nightly_models_ops`
    
    ---------
    
    Co-authored-by: chandrasekaranpradeep <chandrasekaranpradeep@users.noreply.github.com>
    Co-authored-by: chandrasekaranpradeep <pchandrasekaran@tenstorrent.com>

---
+++ forge/test/models_ops/test_abs.py
@@ -0,0 +1,141 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import forge
+import forge.op
+from forge import ForgeModule
+
+from loguru import logger
+import torch
+
+from forge import Tensor, compile
+from forge.verify.compare import compare_with_golden
+from forge.verify.verify import verify
+from forge.verify.value_checkers import AutomaticValueChecker
+from forge.verify.config import VerifyConfig
+import pytest
+
+
+class Abs0(ForgeModule):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def forward(self, abs_input_0):
+        abs_output_1 = forge.op.Abs("", abs_input_0)
+        return abs_output_1
+
+
+def ids_func(param):
+    forge_module = param[0]
+    shapes_dtypes = param[1]
+    return str(forge_module.__name__) + "-" + str(shapes_dtypes)
+
+
+forge_modules_and_shapes_dtypes_list = [
+    (
+        Abs0,
+        [((2, 1, 1, 13), torch.float32)],
+        {
+            "model_name": [
+                "pt_stereo_facebook_musicgen_large_music_generation_hf",
+                "pt_stereo_facebook_musicgen_medium_music_generation_hf",
+                "pt_stereo_facebook_musicgen_small_music_generation_hf",
+            ],
+            "pcc": 0.99,
+        },
+    ),
+    (
+        Abs0,
+        [((2, 1, 7, 7), torch.float32)],
+        {"model_name": ["pt_clip_openai_clip_vit_base_patch32_text_gen_hf_text"], "pcc": 0.99},
+    ),
+    (
+        Abs0,
+        [((1, 1, 256, 256), torch.float32)],
+        {
+            "model_name": [
+                "pt_bart_facebook_bart_large_mnli_seq_cls_hf",
+                "pt_llama3_meta_llama_llama_3_2_1b_instruct_clm_hf",
+                "pt_llama3_meta_llama_llama_3_2_1b_clm_hf",
+                "pt_opt_facebook_opt_1_3b_clm_hf",
+                "pt_opt_facebook_opt_125m_clm_hf",
+                "pt_opt_facebook_opt_350m_clm_hf",
+                "pt_xglm_facebook_xglm_1_7b_clm_hf",
+                "pt_xglm_facebook_xglm_564m_clm_hf",
+            ],
+            "pcc": 0.99,
+        },
+    ),
+    (
+        Abs0,
+        [((1, 12, 128, 128), torch.float32)],
+        {
+            "model_name": [
+                "pt_distilbert_distilbert_base_multilingual_cased_mlm_hf",
+                "pt_distilbert_distilbert_base_cased_mlm_hf",
+                "pt_distilbert_distilbert_base_uncased_finetuned_sst_2_english_seq_cls_hf",
+                "pt_distilbert_davlan_distilbert_base_multilingual_cased_ner_hrl_token_cls_hf",
+                "pt_distilbert_distilbert_base_uncased_mlm_hf",
+            ],
+            "pcc": 0.99,
+        },
+    ),
+    (
+        Abs0,
+        [((1, 12, 384, 384), torch.float32)],
+        {"model_name": ["pt_distilbert_distilbert_base_cased_distilled_squad_qa_hf"], "pcc": 0.99},
+    ),
+    (
+        Abs0,
+        [((1, 1, 32, 32), torch.float32)],
+        {
+            "model_name": [
+                "pt_opt_facebook_opt_1_3b_seq_cls_hf",
+                "pt_opt_facebook_opt_1_3b_qa_hf",
+                "pt_opt_facebook_opt_350m_qa_hf",
+                "pt_opt_facebook_opt_125m_seq_cls_hf",
+                "pt_opt_facebook_opt_350m_seq_cls_hf",
+                "pt_opt_facebook_opt_125m_qa_hf",
+            ],
+            "pcc": 0.99,
+        },
+    ),
+]
+
+
+@pytest.mark.nightly_models_ops
+@pytest.mark.parametrize("forge_module_and_shapes_dtypes", forge_modules_and_shapes_dtypes_list, ids=ids_func)
+def test_module(forge_module_and_shapes_dtypes, record_forge_property):
+    record_forge_property("op_name", "Abs")
+
+    forge_module, operand_shapes_dtypes, metadata = forge_module_and_shapes_dtypes
+
+    pcc = metadata.pop("pcc")
+
+    for metadata_name, metadata_value in metadata.items():
+        record_forge_property(metadata_name, metadata_value)
+
+    max_int = 1000
+    inputs = [
+        Tensor.create_from_shape(operand_shape, operand_dtype, max_int=max_int)
+        for operand_shape, operand_dtype in operand_shapes_dtypes
+    ]
+
+    framework_model = forge_module(forge_module.__name__)
+    framework_model.process_framework_parameters()
+
+    for name, parameter in framework_model._parameters.items():
+        parameter_tensor = Tensor.create_torch_tensor(
+            shape=parameter.shape.get_pytorch_shape(), dtype=parameter.pt_data_format, max_int=max_int
+        )
+        framework_model.set_parameter(name, parameter_tensor)
+
+    for name, constant in framework_model._constants.items():
+        constant_tensor = Tensor.create_torch_tensor(
+            shape=constant.shape.get_pytorch_shape(), dtype=constant.pt_data_format, max_int=max_int
+        )
+        framework_model.set_constant(name, constant_tensor)
+
+    compiled_model = compile(framework_model, sample_inputs=inputs)
+
+    verify(inputs, framework_model, compiled_model, VerifyConfig(value_checker=AutomaticValueChecker(pcc=pcc)))
---
+++ forge/test/models_ops/test_argmax.py
@@ -0,0 +1,89 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import forge
+import forge.op
+from forge import ForgeModule
+
+from loguru import logger
+import torch
+
+from forge import Tensor, compile
+from forge.verify.compare import compare_with_golden
+from forge.verify.verify import verify
+from forge.verify.value_checkers import AutomaticValueChecker
+from forge.verify.config import VerifyConfig
+import pytest
+
+
+class Argmax0(ForgeModule):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def forward(self, argmax_input_0):
+        argmax_output_1 = forge.op.Argmax("", argmax_input_0, dim=-1)
+        return argmax_output_1
+
+
+def ids_func(param):
+    forge_module = param[0]
+    shapes_dtypes = param[1]
+    return str(forge_module.__name__) + "-" + str(shapes_dtypes)
+
+
+forge_modules_and_shapes_dtypes_list = [
+    pytest.param(
+        (
+            Argmax0,
+            [((1, 32), torch.int32)],
+            {
+                "model_name": [
+                    "pt_opt_facebook_opt_1_3b_seq_cls_hf",
+                    "pt_opt_facebook_opt_125m_seq_cls_hf",
+                    "pt_opt_facebook_opt_350m_seq_cls_hf",
+                ],
+                "pcc": 0.99,
+                "op_params": {"dim": "-1"},
+            },
+        ),
+        marks=[pytest.mark.xfail(reason="RuntimeError: Generated MLIR module failed verification.")],
+    ),
+]
+
+
+@pytest.mark.nightly_models_ops
+@pytest.mark.parametrize("forge_module_and_shapes_dtypes", forge_modules_and_shapes_dtypes_list, ids=ids_func)
+def test_module(forge_module_and_shapes_dtypes, record_forge_property):
+    record_forge_property("op_name", "Argmax")
+
+    forge_module, operand_shapes_dtypes, metadata = forge_module_and_shapes_dtypes
+
+    pcc = metadata.pop("pcc")
+
+    for metadata_name, metadata_value in metadata.items():
+        record_forge_property(metadata_name, metadata_value)
+
+    max_int = 1000
+    inputs = [
+        Tensor.create_from_shape(operand_shape, operand_dtype, max_int=max_int)
+        for operand_shape, operand_dtype in operand_shapes_dtypes
+    ]
+
+    framework_model = forge_module(forge_module.__name__)
+    framework_model.process_framework_parameters()
+
+    for name, parameter in framework_model._parameters.items():
+        parameter_tensor = Tensor.create_torch_tensor(
+            shape=parameter.shape.get_pytorch_shape(), dtype=parameter.pt_data_format, max_int=max_int
+        )
+        framework_model.set_parameter(name, parameter_tensor)
+
+    for name, constant in framework_model._constants.items():
+        constant_tensor = Tensor.create_torch_tensor(
+            shape=constant.shape.get_pytorch_shape(), dtype=constant.pt_data_format, max_int=max_int
+        )
+        framework_model.set_constant(name, constant_tensor)
+
+    compiled_model = compile(framework_model, sample_inputs=inputs)
+
+    verify(inputs, framework_model, compiled_model, VerifyConfig(value_checker=AutomaticValueChecker(pcc=pcc)))
