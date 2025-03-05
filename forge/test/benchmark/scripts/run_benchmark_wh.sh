3c0770d0f541a9f86ce543dcbdd261655daf1447
Vladimir Canic <vcanic@tenstorrent.com>
2025-03-05 22:19:07 +0000

    Add resnet hf model to benchmark models.

--- forge/test/benchmark/scripts/run_benchmark_wh.sh
+++ forge/test/benchmark/scripts/run_benchmark_wh.sh
@@ -9,3 +9,6 @@
 
 # MNIST Linear
 python forge/test/benchmark/benchmark.py -m mnist_linear -bs 1 -lp 32 -o forge-benchmark-e2e-mnist.json
+
+# Resnet HF
+PYTHONPATH=./forge python forge/test/benchmark/benchmark.py -m resnet50_hf -bs 1 -lp 32 -o forge-benchmark-e2e-resnet50_hf.json
