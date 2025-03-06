ff19e264324a33ea966ec61460db7351105d52b8
Meenakshi Ramanathan <mramanathan@tenstorrent.com>
2025-03-06 17:12:17 +0530

    Port tt-torch models to forge (#1337)
    Timm version has been upgraded since it is a dependency for models from
    [tt-torch](https://github.com/tenstorrent/tt-torch/blob/02f2967599ac5a782079dd71b36d8d0ca1983571/tests/models/timm/test_timm_image_classification.py#L14).
    
    xception71.tf_in1k variant is passing end-to-end, so push marker has
    been added for it.
    
    Newly added Effiicientnet_lite timm variants have not been added to
    nightly as it is failing with Segmentation fault.

--- forge/test/models/pytorch/vision/resnet/test_resnet.py
+++ forge/test/models/pytorch/vision/resnet/test_resnet.py
@@ -8,7 +8,6 @@ import timm
 import torch
 from datasets import load_dataset
 from tabulate import tabulate
-from torchvision.models.resnet import resnet50
 from transformers import AutoImageProcessor, ResNetForImageClassification
 
 import forge
@@ -16,6 +15,7 @@ from forge.verify.config import VerifyConfig
 from forge.verify.value_checkers import AutomaticValueChecker
 from forge.verify.verify import verify
 
+from test.models.pytorch.vision.utils.utils import load_vision_model_and_input
 from test.models.utils import Framework, Source, Task, build_module_name
 from test.utils import download_model
 
@@ -113,24 +113,37 @@ def test_resnet_timm(record_forge_property):
     verify(input_sample, framework_model, compiled_model, VerifyConfig(value_checker=AutomaticValueChecker(pcc=0.95)))
 
 
+variants_with_weights = {
+    "resnet18": "ResNet18_Weights",
+    "resnet34": "ResNet34_Weights",
+    "resnet50": "ResNet50_Weights",
+    "resnet101": "ResNet101_Weights",
+    "resnet152": "ResNet152_Weights",
+}
+
+
 @pytest.mark.nightly
-def test_resnet_torchvision(record_forge_property):
-    # Record model details
+@pytest.mark.parametrize("variant", variants_with_weights.keys())
+def test_resnet_torchvision(record_forge_property, variant):
+
+    # Build Module Name
     module_name = build_module_name(
         framework=Framework.PYTORCH,
         model="resnet",
-        source=Source.TORCHVISION,
-        variant="50",
+        variant=variant,
         task=Task.IMAGE_CLASSIFICATION,
+        source=Source.TORCHVISION,
     )
+
+    # Record Forge Property
     record_forge_property("tags.model_name", module_name)
 
-    # Load framework model
-    framework_model = resnet50()
+    # Load model and input
+    weight_name = variants_with_weights[variant]
+    framework_model, inputs = load_vision_model_and_input(variant, "classification", weight_name)
 
-    # Compile model
-    input_sample = [torch.rand(1, 3, 224, 224)]
-    compiled_model = forge.compile(framework_model, input_sample)
+    # Forge compile framework model
+    compiled_model = forge.compile(framework_model, sample_inputs=inputs, module_name=module_name)
 
-    # Verify data on sample input
-    verify(input_sample, framework_model, compiled_model, VerifyConfig(value_checker=AutomaticValueChecker(pcc=0.95)))
+    # Model Verification
+    verify(inputs, framework_model, compiled_model)
