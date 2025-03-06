ddb66aed0fb94de45a7c09056b3362f0a444e80a
dsudhakarTT <dsudhakar@tenstorrent.com>
2025-03-06 07:02:09 +0000

    Add phi1 and phi4 models

---
+++ forge/test/models/pytorch/text/phi4/test_phi4.py
@@ -0,0 +1,107 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+from transformers import AutoTokenizer, PhiForCausalLM, PhiForTokenClassification
+
+import forge
+
+from test.models.utils import Framework, Source, Task, build_module_name
+from test.utils import download_model
+
+variants = ["microsoft/phi-4"]
+
+
+@pytest.mark.nightly
+@pytest.mark.parametrize("variant", variants)
+def test_phi_4_causal_lm(forge_property_recorder, variant):
+
+    # Build Module Name
+    module_name = build_module_name(variant, Source.HUGGINGFACE, Framework.PYTORCH, Task.CAUSAL_LM)
+
+    # Record Forge Property
+    forge_property_recorder.record_group("generality")
+    forge_property_recorder.record_model_name(module_name)
+
+    # Load tokenizer and model from HuggingFace
+    framework_model = download_model(PhiForCausalLM.from_pretrained, variant, return_dict=False, use_cache=False)
+    tokenizer = download_model(AutoTokenizer.from_pretrained, variant)
+    framework_model.eval()
+
+    # input_prompt
+    input_prompt = "Africa is an emerging economy because"
+    inputs = tokenizer(input_prompt, return_tensors="pt")
+    sample_inputs = [inputs["input_ids"], inputs["attention_mask"]]
+
+    # Forge compile framework model
+    compiled_model = forge.compile(
+        framework_model, sample_inputs, module_name, forge_property_handler=forge_property_recorder
+    )
+
+    # Model Verification
+    verify(sample_inputs, framework_model, compiled_model, forge_property_handler=forge_property_recorder)
+
+
+@pytest.mark.nightly
+@pytest.mark.parametrize("variant", variants)
+def test_phi_4_token_classification(forge_property_recorder, variant):
+
+    # Build Module Name
+    module_name = build_module_name(variant, Source.HUGGINGFACE, Framework.PYTORCH, Task.TOKEN_CLASSIFICATION)
+
+    # Record Forge Property
+    forge_property_recorder.record_group("generality")
+    forge_property_recorder.record_model_name(module_name)
+    framework_model = download_model(
+        PhiForTokenClassification.from_pretrained, variant, return_dict=False, use_cache=False
+    )
+    tokenizer = download_model(AutoTokenizer.from_pretrained, variant)
+    framework_model.eval()
+
+    # input_prompt
+    input_prompt = "HuggingFace is a company based in Paris and New York"
+    inputs = tokenizer(input_prompt, return_tensors="pt")
+    inputs = [inputs["input_ids"]]
+
+    # Forge compile framework model
+    compiled_model = forge.compile(framework_model, inputs, module_name, forge_property_handler=forge_property_recorder)
+
+    # Model Verification
+    verify(inputs, framework_model, compiled_model, forge_property_handler=forge_property_recorder)
+
+
+@pytest.mark.nightly
+@pytest.mark.parametrize("variant", variants)
+def test_phi_4_sequence_classification(forge_property_recorder, variant):
+
+    # Build Module Name
+    module_name = build_module_name(variant, Source.HUGGINGFACE, Framework.PYTORCH, Task.SEQUENCE_CLASSIFICATION)
+
+    # Record Forge Property
+    forge_property_recorder.record_group("generality")
+    forge_property_recorder.record_model_name(module_name)
+
+    # Load tokenizer and model from HuggingFace
+    tokenizer = download_model(AutoTokenizer.from_pretrained, variant)
+    tokenizer.pad_token = tokenizer.eos_token
+    framework_model = download_model(
+        PhiForTokenClassification.from_pretrained, variant, return_dict=False, use_cache=False
+    )
+    framework_model.eval()
+
+    # input_prompt
+    input_prompt = "the movie was great!"
+    inputs = tokenizer(
+        input_prompt,
+        return_tensors="pt",
+        max_length=256,
+        pad_to_max_length=True,
+        truncation=True,
+    )
+    inputs = [inputs["input_ids"]]
+
+    # Forge compile framework model
+    compiled_model = forge.compile(framework_model, inputs, module_name, forge_property_handler=forge_property_recorder)
+
+    # Model Verification
+    verify(inputs, framework_model, compiled_model, forge_property_handler=forge_property_recorder)
