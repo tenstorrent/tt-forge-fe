4130ae5f8dae348df363e6a886668e2b577e022c
Vladimir Brkic <vbrkic@tenstorrent.com>
2025-03-07 11:40:47 +0000

    Run garbage collector between tests [ci skip]

--- forge/test/operators/pytorch/test_all.py
+++ forge/test/operators/pytorch/test_all.py
@@ -6,6 +6,8 @@
 
 
 import os
+import gc
+import pytest
 import forge
 import textwrap
 import json
@@ -26,6 +28,20 @@ from test.operators.utils import TestSuite
 from test.operators.utils import TestPlanScanner
 from test.operators.utils import TestPlanUtils
 from test.operators.utils import FailingReasons
+
+
+test_counter = 0
+
+
+@pytest.fixture(autouse=True)
+def run_gc():
+    """Run garbage collection regularly to avoid memory leaks"""
+    yield
+    global test_counter
+    test_counter += 1
+    # Run garbage collection every 10 tests
+    if test_counter % 10 == 0:
+        gc.collect()
 
 
 @dataclass
