b73c7b8d9285d5ffdcac37c26df93eabb5998353
Pradeep Chandrasekaran <160489301+chandrasekaranpradeep@users.noreply.github.com>
2025-03-10 21:16:07 +0530

    Update maxpool2d and stack ops test in models ops (#1420)
    In [latest nightly models ops
    pipeline](https://github.com/tenstorrent/tt-forge-fe/actions/runs/13755366355),
    below issues in maxpool2d test cases was resolved, so remove xfail
    markers for those test
    
    `RuntimeError: TT_FATAL @
    /__w/tt-forge-fe/tt-forge-fe/third_party/tt-mlir/third_party/tt-metal/src/tt-metal/ttnn/cpp/ttnn/tensor/tensor_utils.cpp:50:
    new_volume == old_volume info: Invalid arguments to reshape`
    
    Stack ops test is failing with data mismatch between framework and
    compiled model output, so updating with xfail marker
    
    Models ops test generated report:
    
    [model_ops_tests_report.xlsx](https://github.com/user-attachments/files/19164448/model_ops_tests_report.xlsx)

--- forge/test/models_ops/test_stack.py
+++ forge/test/models_ops/test_stack.py
@@ -40,23 +40,26 @@ def ids_func(param):
 
 
 forge_modules_and_shapes_dtypes_list = [
-    (
-        Stack0,
-        [
-            ((2, 1, 2048), torch.float32),
-            ((2, 1, 2048), torch.float32),
-            ((2, 1, 2048), torch.float32),
-            ((2, 1, 2048), torch.float32),
-        ],
-        {
-            "model_name": [
-                "pt_stereo_facebook_musicgen_large_music_generation_hf",
-                "pt_stereo_facebook_musicgen_medium_music_generation_hf",
-                "pt_stereo_facebook_musicgen_small_music_generation_hf",
+    pytest.param(
+        (
+            Stack0,
+            [
+                ((2, 1, 2048), torch.float32),
+                ((2, 1, 2048), torch.float32),
+                ((2, 1, 2048), torch.float32),
+                ((2, 1, 2048), torch.float32),
             ],
-            "pcc": 0.99,
-            "op_params": {"axis": "-3"},
-        },
+            {
+                "model_name": [
+                    "pt_stereo_facebook_musicgen_large_music_generation_hf",
+                    "pt_stereo_facebook_musicgen_medium_music_generation_hf",
+                    "pt_stereo_facebook_musicgen_small_music_generation_hf",
+                ],
+                "pcc": 0.99,
+                "op_params": {"axis": "-3"},
+            },
+        ),
+        marks=[pytest.mark.xfail(reason="Data mismatch between framework output and compiled model output")],
     ),
     (
         Stack1,
