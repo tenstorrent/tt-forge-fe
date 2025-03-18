c33585487c63573c7e48b320864db9c571f5ba42
saiarthiraguram <ctr-sraguram@ext.tenstorrent.com>
2025-03-18 10:58:20 +0000

    PR Comments addressal

--- forge/test/models/pytorch/vision/yolo/test_yolo_v10.py
+++ forge/test/models/pytorch/vision/yolo/test_yolo_v10.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import subprocess
 
 import numpy as np
 import pytest
@@ -31,10 +30,7 @@ class YoloWrapper(torch.nn.Module):
 
 @pytest.mark.xfail(reason="TypeError: type Tensor doesn't define __round__ method")
 @pytest.mark.nightly
-def test_yolov10(record_forge_property):
-
-    # Upgrading ultralytics to run yolov10 model
-    subprocess.run(["pip", "install", "ultralytics==8.3.91"], check=True)
+def test_yolov10(forge_property_recorder):
 
     # Build Module Name
     module_name = build_module_name(
@@ -46,8 +42,8 @@ def test_yolov10(record_forge_property):
     )
 
     # Record Forge Property
-    record_forge_property("group", "priority_1")
-    record_forge_property("tags.model_name", module_name)
+    forge_property_recorder.record_group("priority_1")
+    forge_property_recorder.record_model_name(module_name)
 
     MODEL_URL = "https://github.com/ultralytics/assets/releases/download/v8.2.0/yolov10n.pt"
 
@@ -57,17 +53,16 @@ def test_yolov10(record_forge_property):
     framework_model = YoloWrapper(framework_model)
 
     # Load input
-    dataset = load_dataset("huggingface/cats-image", split="test")
+    dataset = load_dataset("huggingface/cats-image", split="test[:1]")
     image = dataset[0]["image"]
     image_np = np.array(image)
     image_tensor = torch.tensor(image_np).permute(2, 0, 1).unsqueeze(0).float()
 
     # Forge compile framework model
     inputs = [image_tensor]
-    compiled_model = forge.compile(framework_model, sample_inputs=inputs, module_name=module_name)
+    compiled_model = forge.compile(
+        framework_model, sample_inputs=inputs, module_name=module_name, forge_property_handler=forge_property_recorder
+    )
 
     # Model Verification
-    verify(inputs, framework_model, compiled_model)
-
-    # Reverting to ultralytics version in the core_requirements.txt
-    subprocess.run(["pip", "install", "--no-cache-dir", "ultralytics==8.0.145"], check=True)
+    verify(inputs, framework_model, compiled_model, forge_property_handler=forge_property_recorder)
