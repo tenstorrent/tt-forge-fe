a083cbbafc9eb8753a322a4b675f27ef19f2b89d
Vladica Obojevic <vobojevic@tenstorrent.com>
2025-03-20 18:22:09 +0000

    Concatenate operator test refactor [skip ci]

--- forge/test/operators/pytorch/eltwise_nary/test_concatenate.py
+++ forge/test/operators/pytorch/eltwise_nary/test_concatenate.py
@@ -1,69 +1,16 @@
 # SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 
 # SPDX-License-Identifier: Apache-2.0
-#
-# Tests for testing of concatenate operators
-#
-# In this test we test pytorch concatenate operator
 
-# GENERAL OP SUPPORT TEST PLAN:
-# 1. Operand type - any supported type
-# 2. Operand source(s):
-# (+)  2.1 From another op
-#       - Operator -> input
-# (+)  2.2 From DRAM queue
-#       - Operator is first node in network
-#       - Input_queue flag = false
-# (+)  2.3 Const Inputs (const eval pass)
-#       - Operator where all inputs are constants.
-# (+)  2.4 From host
-#       - Input tensor as input of network
-#       - Operator is first node in network
-#       - Input_queue flag = true
-# 3 Operand shapes type(s):
-# (+)  3.1 Full tensor (i.e. full expected shape)
-#       - 3-4 by default P1 (high prioriy)
-#       - 2, 5, ++ include P2 (lower prioriy)
-# (+)  3.2 Tensor reduce on one or more dims to 1
-#       - Vector
-#       - Only one dim is not equal to 1
-# (+)  3.3 Scalar P2
-#       - Create tensor of dimension equal to 0 (tensor from scalar) or just to use scalar as simple value
-# 4. Operand / output size of dimensions (few examples of each, 10 values total)
-# (+)  4.1 Divisible by 32
-# (+)  4.2 Prime numbers
-# (+)  4.3 Very large (thousands, 10s of thousands)
-#       - 100x100, 100x1000
-#       - maybe nightly only
-# (+)  4.4 Extreme ratios between height/width
-#      4.5 ...probably many more interesting combinations here
-# 5. Data format - all supported formats
-# (/)  5.1 Output DF
-# (/)  5.2 Intermediate DF
-# (/)  5.3 Accumulation DF
-# (+)  5.4 Operand DFs
-#       - Fix HiFi4 for math fidelity value
-# (+) 6. Math fidelity - LoFi, HiFi2a, Hifi2b, Hifi3, Hifi4
-#       - Fix fp16b (default) for data format value
-# (/) 7. Special attributes - if applicable.. like approx_mode for Exp, for example
-# (/) 8. Special cases - if applicable
-# 9. Variable number of operands - if applicable
-# (/) Few representative values
-# (/) Reuse inputs for selected operators
-
-
-import pytest
-
-from typing import List, Dict, Type, Optional, Any
+from typing import List, Dict
 from loguru import logger
 
 import torch
-import forge
-import forge.op
 
+from forge.verify.config import VerifyConfig
+from forge.verify.value_checkers import AllCloseValueChecker, AutomaticValueChecker
 
-from test.operators.utils import InputSourceFlags, VerifyUtils
-from test.operators.utils import ShapeUtils
+from test.operators.utils import VerifyUtils
 from test.operators.utils import InputSource
 from test.operators.utils import TestVector
 from test.operators.utils import TestPlan
@@ -71,19 +18,20 @@ from test.operators.utils import FailingReasons
 from test.operators.utils.compat import TestDevice
 from test.operators.utils import TestCollection
 from test.operators.utils import TestCollectionCommon
+from test.operators.utils import TestCollectionTorch
+from test.operators.utils import ValueRanges
 
 
 class ModelFromAnotherOp(torch.nn.Module):
 
     model_name = "model_op_src_from_another_op"
 
-    def __init__(self, operator, opname, shape, number_of_operands, kwargs):
+    def __init__(self, operator, opname, shape, kwargs):
         super(ModelFromAnotherOp, self).__init__()
         self.testname = "Concatenate_pytorch_operator_" + opname + "_test_op_src_from_another_op"
         self.operator = operator
         self.opname = opname
         self.shape = shape
-        self.number_of_operands = number_of_operands
         self.kwargs = kwargs
 
     def forward(self, *x: torch.Tensor):
@@ -99,13 +47,12 @@ class ModelDirect(torch.nn.Module):
 
     model_name = "model_op_src_from_host"
 
-    def __init__(self, operator, opname, shape, number_of_operands, kwargs):
+    def __init__(self, operator, opname, shape, kwargs):
         super(ModelDirect, self).__init__()
         self.testname = "Concatenate_pytorch_operator_" + opname + "_test_op_src_from_host"
         self.operator = operator
         self.opname = opname
         self.shape = shape
-        self.number_of_operands = number_of_operands
         self.kwargs = kwargs
 
     def forward(self, *x: torch.Tensor):
@@ -117,7 +64,7 @@ class ModelConstEvalPass(torch.nn.Module):
 
     model_name = "model_op_src_const_eval_pass"
 
-    def __init__(self, operator, opname, shape, number_of_operands, kwargs):
+    def __init__(self, operator, opname, shape, kwargs):
         super(ModelConstEvalPass, self).__init__()
         self.testname = "Concatenate_pytorch_operator_" + opname + "_test_op_src_const_eval_pass"
         self.operator = operator
@@ -145,7 +92,6 @@ class TestVerification:
     MODEL_TYPES = {
         InputSource.FROM_ANOTHER_OP: ModelFromAnotherOp,
         InputSource.FROM_HOST: ModelDirect,
-        InputSource.FROM_DRAM_QUEUE: ModelDirect,
         InputSource.CONST_EVAL_PASS: ModelConstEvalPass,
     }
 
@@ -159,10 +105,6 @@ class TestVerification:
     ):
         """Common verification function for all tests"""
 
-        input_source_flag: InputSourceFlags = None
-        if test_vector.input_source in (InputSource.FROM_DRAM_QUEUE,):
-            input_source_flag = InputSourceFlags.FROM_DRAM
-
         operator = getattr(torch, test_vector.operator)
 
         kwargs = test_vector.kwargs if test_vector.kwargs else {}
@@ -172,7 +114,6 @@ class TestVerification:
             operator=operator,
             opname=test_vector.operator,
             shape=test_vector.input_shape,
-            number_of_operands=test_vector.number_of_operands,
             kwargs=kwargs,
         )
 
@@ -188,16 +129,22 @@ class TestVerification:
 
         logger.trace(f"***input_shapes: {input_shapes}")
 
+        # Using AllCloseValueChecker in all cases except for integer data formats:
+        verify_config = VerifyConfig(value_checker=AllCloseValueChecker(atol=1e-2))
+        if test_vector.dev_data_format in TestCollectionTorch.int.dev_data_formats:
+            verify_config = VerifyConfig(value_checker=AutomaticValueChecker())
+
         VerifyUtils.verify(
             model=pytorch_model,
             test_device=test_device,
             input_shapes=input_shapes,
             input_params=input_params,
-            input_source_flag=input_source_flag,
             dev_data_format=test_vector.dev_data_format,
             math_fidelity=test_vector.math_fidelity,
-            pcc=test_vector.pcc,
             warm_reset=warm_reset,
+            value_range=ValueRanges.SMALL,
+            deprecated_verification=False,
+            verify_config=verify_config,
         )
 
 
@@ -206,6 +153,8 @@ class TestParamsData:
     __test__ = False  # Avoid collecting TestParamsData as a pytest test
 
     test_plan: TestPlan = None
+
+    operators = ["concatenate"]
 
     @classmethod
     def generate_kwargs(cls, test_vector: TestVector):
@@ -221,42 +170,9 @@ class TestParamsData:
             7,
             # 15, # consume too much memory
         ]
-        extended_shapes = []
         for shape in shapes:
             for repeat in number_of_operands:
-                extended_shapes.append((shape,) * repeat)
-        return extended_shapes
-
-
-class TestCollectionData:
-
-    __test__ = False  # Avoid collecting TestCollectionData as a pytest test
-
-    all = TestCollection(
-        operators=[
-            "concatenate",  # 00
-        ],
-        input_sources=TestCollectionCommon.all.input_sources,
-        input_shapes=TestCollectionCommon.all.input_shapes,
-        numbers_of_operands=[
-            2,
-            3,
-            7,
-            # 15, # consume too much memory
-        ],
-        dev_data_formats=TestCollectionCommon.all.dev_data_formats,
-        math_fidelities=TestCollectionCommon.all.math_fidelities,
-    )
-
-    single = TestCollection(
-        input_sources=TestCollectionCommon.single.input_sources,
-        input_shapes=TestCollectionCommon.single.input_shapes,
-        numbers_of_operands=[
-            2,
-        ],
-        dev_data_formats=TestCollectionCommon.single.dev_data_formats,
-        math_fidelities=TestCollectionCommon.single.math_fidelities,
-    )
+                yield (shape,) * repeat
 
 
 TestParamsData.test_plan = TestPlan(
@@ -265,64 +181,51 @@ TestParamsData.test_plan = TestPlan(
         test_vector,
     ),
     collections=[
-        # Test plan:
-        # 2. Operand source(s):
-        # 3. Operand shapes type(s):
-        # 4. Operand / output size of dimensions
+        # Test all shapes and input sources collection:
         TestCollection(
-            operators=TestCollectionData.all.operators,
-            input_sources=TestCollectionData.all.input_sources,
+            operators=TestParamsData.operators,
+            input_sources=[  # TODO: use TestCollectionCommon.all.input_sources when becames available
+                InputSource.FROM_ANOTHER_OP,
+                InputSource.FROM_HOST,
+                InputSource.CONST_EVAL_PASS,
+            ],
             input_shapes=TestParamsData.generate_input_shapes(),
             kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
         ),
-        # Test plan:
-        # 5. Data format
+        # Test Data formats collection:
         TestCollection(
-            operators=TestCollectionData.all.operators,
-            input_sources=TestCollectionData.single.input_sources,
-            input_shapes=[
-                ((1, 2, 3, 4), (1, 2, 3, 4)),
-            ],
-            kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
+            operators=TestParamsData.operators,
+            input_sources=TestCollectionCommon.single.input_sources,
+            input_shapes=[(TestCollectionCommon.single.input_shapes[0],) * 2],
+            kwargs=[{"dim": 0}],
             dev_data_formats=[
                 item
-                for item in TestCollectionData.all.dev_data_formats
-                if item not in TestCollectionData.single.dev_data_formats
+                for item in TestCollectionTorch.all.dev_data_formats
+                if item not in TestCollectionTorch.single.dev_data_formats
             ],
-            math_fidelities=TestCollectionData.single.math_fidelities,
+            math_fidelities=TestCollectionCommon.single.math_fidelities,
         ),
-        # Test plan:
-        # 6. Math fidelity
+        # Test math fidelity collection:
         TestCollection(
-            operators=TestCollectionData.all.operators,
-            input_sources=TestCollectionData.single.input_sources,
-            input_shapes=[
-                ((1, 2, 3, 4), (1, 2, 3, 4)),
-            ],
-            kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
-            dev_data_formats=TestCollectionData.single.dev_data_formats,
-            math_fidelities=TestCollectionData.all.math_fidelities,
+            operators=TestParamsData.operators,
+            input_sources=TestCollectionCommon.single.input_sources,
+            input_shapes=[(TestCollectionCommon.single.input_shapes[0],) * 2],
+            kwargs=[{"dim": 0}],
+            dev_data_formats=TestCollectionTorch.single.dev_data_formats,
+            math_fidelities=TestCollectionCommon.all.math_fidelities,
         ),
     ],
     failing_rules=[
-        # Skip all tests with input shapes with 2 dimensions
+        # Unsupported ttnn::DataType... Fatal Python error: Aborted
         TestCollection(
-            criteria=lambda test_vector: len(test_vector.input_shape[0]) == 2,
-            skip_reason=FailingReasons.NOT_IMPLEMENTED,
-        ),
-        TestCollection(
-            criteria=lambda test_vector: test_vector.input_source != InputSource.CONST_EVAL_PASS
-            and len(test_vector.input_shape[0]) == 3
-            and (test_vector.kwargs["dim"] == 1 or test_vector.kwargs["dim"] == 2)
-            and test_vector.input_shape[0][test_vector.kwargs["dim"]] % 32 != 0,
-            failing_reason=FailingReasons.NOT_IMPLEMENTED,
-        ),
-        TestCollection(
-            criteria=lambda test_vector: test_vector.input_source != InputSource.CONST_EVAL_PASS
-            and len(test_vector.input_shape[0]) == 4
-            and (test_vector.kwargs["dim"] == 2 or test_vector.kwargs["dim"] == 3)
-            and test_vector.input_shape[0][test_vector.kwargs["dim"]] % 32 != 0,
-            failing_reason=FailingReasons.NOT_IMPLEMENTED,
+            operators=TestParamsData.operators,
+            input_sources=TestCollectionCommon.single.input_sources,
+            input_shapes=[(TestCollectionCommon.single.input_shapes[0],) * 2],
+            kwargs=[{"dim": 0}],
+            dev_data_formats=[torch.float16],
+            math_fidelities=TestCollectionCommon.single.math_fidelities,
+            failing_reason=FailingReasons.UNSUPPORTED_DATA_FORMAT,
+            skip_reason=FailingReasons.UNSUPPORTED_DATA_FORMAT,
         ),
     ],
 )
