4e4e7cf29ac19648719c79f3eb67e8a81cac1695
mvasiljevicTT <mvasiljevic@tenstorrent.com>
2025-03-21 15:13:22 +0000

    use ErnieTokenizer in ernie tests

--- forge/test/models/paddlepaddle/text/ernie/test_ernie.py
+++ forge/test/models/paddlepaddle/text/ernie/test_ernie.py
@@ -9,13 +9,11 @@ from forge.verify.config import VerifyConfig
 from forge.verify.value_checkers import AutomaticValueChecker
 from forge.verify.verify import verify
 
-from paddlenlp.transformers import ErnieForSequenceClassification, ErnieForMaskedLM, ErnieTokenizer
+from paddlenlp.transformers import ErnieForSequenceClassification, ErnieForMaskedLM, ErnieTokenizer, ErnieConfig
+
 
 def test_multi_head_attention():
-    model = paddle.nn.MultiHeadAttention(
-        embed_dim=128,
-        num_heads=2
-    )
+    model = paddle.nn.MultiHeadAttention(embed_dim=128, num_heads=2)
 
     query = paddle.rand((1, 12, 128))
     key = paddle.rand((1, 12, 128))
@@ -25,17 +23,19 @@ def test_multi_head_attention():
     compiled_model = forge.compile(model, inputs)
     verify(inputs, model, compiled_model)
 
+
 def test_transformer_encoder():
     encoder_layer = paddle.nn.TransformerEncoderLayer(
         d_model=128,
         nhead=2,
         dim_feedforward=512,
-    )    
+    )
     model = paddle.nn.TransformerEncoder(encoder_layer=encoder_layer, num_layers=2)
     input = paddle.rand((1, 12, 128))
     inputs = [input]
     compiled_model = forge.compile(model, inputs)
     verify(inputs, model, compiled_model)
+
 
 def test_ernie_embedding():
     model_name = "ernie-1.0"
@@ -47,18 +47,19 @@ def test_ernie_embedding():
 
     compiled_model = forge.compile(embedding, inputs)
     verify(inputs, embedding, compiled_model)
-    
+
+
 def test_ernie_encoder():
     model_name = "ernie-1.0"
     model = ErnieForSequenceClassification.from_pretrained(model_name, num_classes=2)
-    encoder = model.ernie.encoder
-    
+
     hidden_size = model.config.hidden_size
     input = paddle.rand((1, 12, hidden_size))
     inputs = [input]
-
+    encoder = model.ernie.encoder()
     compiled_model = forge.compile(encoder, inputs)
     verify(inputs, encoder, compiled_model, VerifyConfig(value_checker=AutomaticValueChecker(pcc=0.8)))
+
 
 def test_ernie_pooler():
     model_name = "ernie-1.0"
@@ -72,6 +73,7 @@ def test_ernie_pooler():
     compiled_model = forge.compile(pooler, inputs)
     verify(inputs, pooler, compiled_model)
 
+
 def test_ernie_parts():
     model_name = "ernie-1.0"
     input = paddle.randint(0, 100, (1, 12))
@@ -81,6 +83,7 @@ def test_ernie_parts():
     embedding = model.ernie.embeddings
     encoder = model.ernie.encoder
     pooler = model.ernie.pooler
+
     class Ernie(paddle.nn.Layer):
         def __init__(self, embedding, encoder, pooler):
             super(Ernie, self).__init__()
@@ -93,25 +96,34 @@ def test_ernie_parts():
             encoder_output = self.encoder(embedding_output)
             pooler_output = self.pooler(encoder_output)
             return (encoder_output, pooler_output)
-        
+
     framework_model = Ernie(embedding, encoder, pooler)
     compiled_model = forge.compile(framework_model, inputs)
     verify(inputs, framework_model, compiled_model, VerifyConfig(value_checker=AutomaticValueChecker(pcc=0.8)))
+
 
 def test_ernie_model():
     model_name = "ernie-1.0"
     model = ErnieForSequenceClassification.from_pretrained(model_name, num_classes=2)
     model = model.ernie
-    input = paddle.randint(0, 100, (1, 12))
-    inputs = [input]
+    input_ids = paddle.randint(0, 100, (1, 12), dtype="int32")
+    token_type_ids = paddle.randint(0, 1, (1, 12), dtype="int32")
+    position_ids = paddle.randint(0, 12, (1, 12), dtype="int32")
+    attention_mask = paddle.zeros((1, 1, 1, 12))
+    inputs = [input_ids, token_type_ids, position_ids, attention_mask]
     compiled_model = forge.compile(model, inputs)
     verify(inputs, model, compiled_model, VerifyConfig(value_checker=AutomaticValueChecker(pcc=0.8)))
+
 
 def test_ernie_for_sequence_classification():
     model_name = "ernie-1.0"
     model = ErnieForSequenceClassification.from_pretrained(model_name, num_classes=2)
-    input = paddle.randint(0, 100, (1, 12))
-    inputs = [input]
-    compiled_model = forge.compile(model, inputs)
-    verify(inputs, model, compiled_model, VerifyConfig(value_checker=AutomaticValueChecker(pcc=0.8)))
+    tokenizer = ErnieTokenizer.from_pretrained(model_name)
+    input = ["Hello, my dog is cute"]
+    encoded_input = tokenizer(input, return_token_type_ids=True, return_position_ids=True, return_attention_mask=True)
 
+    # [input_ids, token_type_ids, position_ids, attention_mask]
+    inputs = [paddle.to_tensor(value) for value in encoded_input.values()]
+
+    compiled_model = forge.compile(model, inputs)
+    verify(inputs, model, compiled_model)
