efa1a86ddd0e518d7bc904dad19fea0e83b65402
Kamalraj Kannan <157608228+kamalrajkannan78@users.noreply.github.com>
2025-03-24 14:04:48 +0530

    add test for Mistral-7B-Instruct-v0.3 (#1476)
    ## summary 
    
    - This PR adds test for Mistral-7B-Instruct-v0.3
    
    ## logs
    
    - [mistral.log](https://github.com/user-attachments/files/19336949/mar18_mistral_cc.log)

---
+++ forge/test/models/pytorch/text/mistral/utils/utils.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+def get_current_weather(location: str, format: str):
+    """
+    Get the current weather
+
+    Args:
+        location: The city and state, e.g. San Francisco, CA
+        format: The temperature unit to use. Infer this from the users location. (choices: ["celsius", "fahrenheit"])
+    """
+    pass
