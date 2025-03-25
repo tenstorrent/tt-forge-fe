e54daadbbc4a77c7fa625051f88430240bc2cdb9
mramanathan <mramanathan@tenstorrent.com>
2025-03-25 05:17:35 +0000

    Add onnx test for vovnet

---
+++ forge/test/models/onnx/vision/vovnet/test_vovnet.py
@@ -0,0 +1,187 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+import torch
+import onnx
+from pytorchcv.model_provider import get_model as ptcv_get_model
+
+import forge
+from forge.verify.verify import verify
+
+from test.models.pytorch.vision.vovnet.utils.model_utils import (
+    get_image,
+    preprocess_steps,
+    preprocess_timm_model,
+)
+from test.models.pytorch.vision.vovnet.utils.src_vovnet_stigma import vovnet39, vovnet57
+from test.models.utils import Framework, Source, Task, build_module_name
+from test.utils import download_model
+
+
+def generate_model_vovnet_imgcls_osmr_pytorch(variant):
+    # STEP 2: Create Forge module from PyTorch model
+    model = download_model(ptcv_get_model, variant, pretrained=True)
+    image_tensor = get_image()
+
+    return model, [image_tensor], {}
+
+
+@pytest.mark.nightly
+@pytest.mark.skip(reason="Segmentation Fault")
+@pytest.mark.parametrize("variant", ["vovnet27s"])
+def test_vovnet_osmr_pytorch(forge_property_recorder, variant, tmp_path):
+
+    # Build Module Name
+    module_name = build_module_name(
+        framework=Framework.PYTORCH, model="vovnet", variant=variant, source=Source.OSMR, task=Task.OBJECT_DETECTION
+    )
+
+    # Record Forge Property
+    forge_property_recorder.record_group("priority")
+    forge_property_recorder.record_model_name(module_name)
+
+    # Load model and inputs
+    framework_model, inputs, _ = generate_model_vovnet_imgcls_osmr_pytorch(variant)
+
+    # Export model to ONNX
+    onnx_path = f"{tmp_path}/vovnet_osmr.onnx"
+    torch.onnx.export(
+        framework_model, inputs[0], onnx_path, opset_version=17, input_names=["input"], output_names=["output"]
+    )
+
+    # Load ONNX model
+    onnx_model = onnx.load(onnx_path)
+    onnx.checker.check_model(onnx_model)
+    framework_model = forge.OnnxModule(module_name, onnx_model)
+
+    # Forge compile framework model
+    compiled_model = forge.compile(onnx_model, sample_inputs=inputs, forge_property_handler=forge_property_recorder)
+
+
+def generate_model_vovnet39_imgcls_stigma_pytorch():
+    # STEP 2: Create Forge module from PyTorch model
+    model, image_tensor = download_model(preprocess_steps, vovnet39)
+    return model, [image_tensor], {}
+
+
+@pytest.mark.nightly
+@pytest.mark.skip(reason="Segmentation Fault")
+@pytest.mark.parametrize("variant", ["vovnet39"])
+def test_vovnet_v1_39_stigma_onnx(forge_property_recorder, variant, tmp_path):
+
+    # Build Module Name
+    module_name = build_module_name(
+        framework=Framework.PYTORCH,
+        model="vovnet_v1",
+        variant=variant,
+        source=Source.TORCH_HUB,
+        task=Task.OBJECT_DETECTION,
+    )
+
+    # Record Forge Property
+    forge_property_recorder.record_group("priority")
+    forge_property_recorder.record_model_name(module_name)
+
+    framework_model, inputs, _ = generate_model_vovnet39_imgcls_stigma_pytorch()
+
+    # Export model to ONNX
+    onnx_path = f"{tmp_path}/vovnet_v1_39.onnx"
+    torch.onnx.export(
+        framework_model, inputs[0], onnx_path, opset_version=17, input_names=["input"], output_names=["output"]
+    )
+
+    # Load ONNX model
+    onnx_model = onnx.load(onnx_path)
+    onnx.checker.check_model(onnx_model)
+    framework_model = forge.OnnxModule(module_name, onnx_model)
+
+    # Forge compile framework model
+    compiled_model = forge.compile(onnx_model, sample_inputs=inputs, forge_property_handler=forge_property_recorder)
+
+
+def generate_model_vovnet57_imgcls_stigma_pytorch():
+    # STEP 2: Create Forge module from PyTorch model
+    model, image_tensor = download_model(preprocess_steps, vovnet57)
+
+    return model, [image_tensor], {}
+
+
+@pytest.mark.nightly
+@pytest.mark.skip(reason="Segmentation Fault")
+@pytest.mark.parametrize("variant", ["vovnet_v1_57"])
+def test_vovnet_v1_57_stigma_onnx(forge_property_recorder, variant, tmp_path):
+
+    # Build Module Name
+    module_name = build_module_name(
+        framework=Framework.PYTORCH,
+        model="vovnet",
+        variant=variant,
+        source=Source.TORCH_HUB,
+        task=Task.OBJECT_DETECTION,
+    )
+
+    # Record Forge Property
+    forge_property_recorder.record_group("priority")
+    forge_property_recorder.record_model_name(module_name)
+
+    framework_model, inputs, _ = generate_model_vovnet57_imgcls_stigma_pytorch()
+
+    # Export model to ONNX
+    onnx_path = f"{tmp_path}/vovnet_v1_57.onnx"
+    torch.onnx.export(
+        framework_model, inputs[0], onnx_path, opset_version=17, input_names=["input"], output_names=["output"]
+    )
+
+    # Load ONNX model
+    onnx_model = onnx.load(onnx_path)
+    onnx.checker.check_model(onnx_model)
+    framework_model = forge.OnnxModule(module_name, onnx_model)
+
+    # Forge compile framework model
+    compiled_model = forge.compile(onnx_model, sample_inputs=inputs, forge_property_handler=forge_property_recorder)
+
+
+def generate_model_vovnet_imgcls_timm_pytorch(variant):
+    model, image_tensor = download_model(preprocess_timm_model, variant)
+
+    return model, [image_tensor], {}
+
+
+@pytest.mark.nightly
+@pytest.mark.xfail(
+    reason="AssertionError: Eltwise binary ops must have the same shape in both inputs, or one operand must be 1 wide to broadcast: [1, 1, 7, 7168] vs [1, 1, 7, 7]"
+)
+@pytest.mark.parametrize("variant", ["ese_vovnet19b_dw.ra_in1k"])
+def test_vovnet_timm_pytorch(forge_property_recorder, variant, tmp_path):
+
+    # Build Module Name
+    module_name = build_module_name(
+        framework=Framework.PYTORCH,
+        model="vovnet",
+        variant=variant,
+        source=Source.TORCH_HUB,
+        task=Task.OBJECT_DETECTION,
+    )
+
+    # Record Forge Property
+    forge_property_recorder.record_group("priority")
+    forge_property_recorder.record_model_name(module_name)
+
+    framework_model, inputs, _ = generate_model_vovnet_imgcls_timm_pytorch(
+        variant,
+    )
+
+    # Export model to ONNX
+    onnx_path = f"{tmp_path}/vovnet_timm.onnx"
+    torch.onnx.export(
+        framework_model, inputs[0], onnx_path, opset_version=17, input_names=["input"], output_names=["output"]
+    )
+
+    # Load ONNX model
+    onnx_model = onnx.load(onnx_path)
+    onnx.checker.check_model(onnx_model)
+    framework_model = forge.OnnxModule(module_name, onnx_model)
+
+    # Forge compile framework model
+    compiled_model = forge.compile(onnx_model, sample_inputs=inputs, forge_property_handler=forge_property_recorder)
