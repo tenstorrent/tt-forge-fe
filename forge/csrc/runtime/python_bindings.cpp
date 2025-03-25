27152a6faf51be2343be47c5d4d83f9403af5eb9
Predrag Ilkic <148892209+pilkicTT@users.noreply.github.com>
2025-03-25 20:00:41 +0100

    [runtime] add mechanism to enable program cache (#1525)
    There are still some issues that need to be resolved before we enable
    program cache on by default. For now, adding a mechanism for manually
    enabling it in runtime.
    
    Perf benchmark models are updated to enable program cache before
    executing the benchmarks.
    
    The `tt-forge-fe` APIs for configuring/managing the devices are still
    not properly defined and are work in progress. This is a starting point
    so that we can enable program cache in benchmarks and catch any
    regressions coming from `tt-metal`/`tt-mlir`. Hence, adding these APIs
    to the `experimental` submodule of `runtime` module.
    
    Closes #1493

--- forge/csrc/runtime/python_bindings.cpp
+++ forge/csrc/runtime/python_bindings.cpp
@@ -5,6 +5,7 @@
 #include "runtime/python_bindings.hpp"
 
 #include "runtime/runtime.hpp"
+#include "runtime/tt_device.hpp"
 #include "tt/runtime/types.h"
 
 namespace tt
@@ -12,10 +13,22 @@ namespace tt
 
 void RuntimeModule(py::module &m_runtime)
 {
+    // Main runtime APIs
     py::class_<runtime::Binary>(m_runtime, "Binary")
         .def("get_program_inputs", &runtime::Binary::getProgramInputs)
         .def("get_program_outputs", &runtime::Binary::getProgramOutputs);
     m_runtime.def("run_binary", tt::run_binary);
+
+    // Experimental APIs
+    py::module m_experimental = m_runtime.def_submodule("experimental");
+    py::class_<tt::DeviceSettings>(m_experimental, "DeviceSettings")
+        .def(py::init<>())
+        .def_readwrite("enable_program_cache", &tt::DeviceSettings::enable_program_cache);
+    m_experimental.def(
+        "configure_devices",
+        [](DeviceSettings settings) -> void { tt::TTSystem::get_system().configure_devices(settings); },
+        py::arg("device_settings") = DeviceSettings(),
+        "Configure all devices with the given settings.");
 }
 
 }  // namespace tt
