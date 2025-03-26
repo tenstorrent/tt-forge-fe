43504a3d50945e1960e64cef2af98b5ea9fca1fb
Vladica Obojevic <vobojevic@tenstorrent.com>
2025-03-26 13:47:26 +0000

    Remove dram queue input source from sweeps [skip ci]

--- forge/test/operators/pytorch/tm/test_repeat_interleave.py
+++ forge/test/operators/pytorch/tm/test_repeat_interleave.py
@@ -34,7 +34,6 @@ class TestVerification:
     MODEL_TYPES = {
         InputSource.FROM_ANOTHER_OP: ModelFromAnotherOp,
         InputSource.FROM_HOST: ModelDirect,
-        InputSource.FROM_DRAM_QUEUE: ModelDirect,
         InputSource.CONST_EVAL_PASS: ModelConstEvalPass,
     }
 
@@ -46,10 +45,6 @@ class TestVerification:
         input_params: List[Dict] = [],
         warm_reset: bool = False,
     ):
-
-        input_source_flag: InputSourceFlags = None
-        if test_vector.input_source in (InputSource.FROM_DRAM_QUEUE,):
-            input_source_flag = InputSourceFlags.FROM_DRAM
 
         operator = getattr(torch, test_vector.operator)
         kwargs = test_vector.kwargs if test_vector.kwargs else {}
@@ -75,7 +70,6 @@ class TestVerification:
             test_device=test_device,
             input_shapes=input_shapes,
             input_params=input_params,
-            input_source_flag=input_source_flag,
             dev_data_format=test_vector.dev_data_format,
             math_fidelity=test_vector.math_fidelity,
             warm_reset=warm_reset,
