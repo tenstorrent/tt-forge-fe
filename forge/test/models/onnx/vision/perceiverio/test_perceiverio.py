7d24eb68ab2fff4d186eb882f880766d6b18c096
Nikola Vukobrat <nvukobrat@tenstorrent.com>
2025-03-28 11:40:24 +0000

    Update ONNX model tests

--- forge/test/models/onnx/vision/perceiverio/test_perceiverio.py
+++ forge/test/models/onnx/vision/perceiverio/test_perceiverio.py
@@ -26,56 +26,54 @@ def get_sample_data(model_name):
     return pixel_values
 
 
-# TODO: Temporarily disable this test due to a CI error unrelated to the test itself.
-#   at https://github.com/tenstorrent/tt-forge-fe/pull/1500/checks?check_run_id=39261242125
-# @pytest.mark.skip_model_analysis
-# @pytest.mark.skip(reason="CCM is not public yet.")
-# @pytest.mark.parametrize(
-#    "model_name",
-#    [
-#        "deepmind/vision-perceiver-conv",
-#        "deepmind/vision-perceiver-learned",
-#        "deepmind/vision-perceiver-fourier",
-#    ],
-# )
-# @pytest.mark.nightly
-# def test_perceiver_for_image_classification_onnx(test_device, model_name):
-#
-#    # Set Forge configuration parameters
-#    compiler_cfg = forge.config.CompilerConfig()
-#    compiler_cfg.default_df_override = forge.DataFormat.Float16_b
-#    verify_enabled = True
-#
-#    onnx_model_path = (
-#        "third_party/confidential_customer_models/generated/files/"
-#        + str(model_name).split("/")[-1].replace("-", "_")
-#        + ".onnx"
-#    )
-#
-#    # Sample Image
-#    pixel_values = get_sample_data(model_name)
-#
-#    # Load the onnx model
-#    onnx_model = onnx.load(onnx_model_path)
-#    onnx.checker.check_model(onnx_model)
-#
-#    # Create Forge module from Onnx model
-#    tt_model = forge.OnnxModule(
-#        str(model_name.split("/")[-1].replace("-", "_")) + "_onnx",
-#        onnx_model,
-#    )
-#
-#    # Run inference on Tenstorrent device
-#    verify_module(
-#        tt_model,
-#        input_shapes=(pixel_values.shape,),
-#        inputs=[(pixel_values,)],
-#        verify_cfg=DepricatedVerifyConfig(
-#            arch=test_device.arch,
-#            devtype=test_device.devtype,
-#            devmode=test_device.devmode,
-#            test_kind=TestKind.INFERENCE,
-#            enabled=verify_enabled,  # pcc drops in silicon devicetype
-#            pcc=0.96,
-#        ),
-#    )
+@pytest.mark.skip_model_analysis
+@pytest.mark.skip(reason="CCM is not public yet.")
+@pytest.mark.parametrize(
+    "model_name",
+    [
+        "deepmind/vision-perceiver-conv",
+        "deepmind/vision-perceiver-learned",
+        "deepmind/vision-perceiver-fourier",
+    ],
+)
+@pytest.mark.nightly
+def test_perceiver_for_image_classification_onnx(test_device, model_name):
+
+    # Set Forge configuration parameters
+    compiler_cfg = forge.config.CompilerConfig()
+    compiler_cfg.default_df_override = forge.DataFormat.Float16_b
+    verify_enabled = True
+
+    onnx_model_path = (
+        "third_party/confidential_customer_models/generated/files/"
+        + str(model_name).split("/")[-1].replace("-", "_")
+        + ".onnx"
+    )
+
+    # Sample Image
+    pixel_values = get_sample_data(model_name)
+
+    # Load the onnx model
+    onnx_model = onnx.load(onnx_model_path)
+    onnx.checker.check_model(onnx_model)
+
+    # Create Forge module from Onnx model
+    tt_model = forge.OnnxModule(
+        str(model_name.split("/")[-1].replace("-", "_")) + "_onnx",
+        onnx_model,
+    )
+
+    # Run inference on Tenstorrent device
+    verify_module(
+        tt_model,
+        input_shapes=(pixel_values.shape,),
+        inputs=[(pixel_values,)],
+        verify_cfg=DepricatedVerifyConfig(
+            arch=test_device.arch,
+            devtype=test_device.devtype,
+            devmode=test_device.devmode,
+            test_kind=TestKind.INFERENCE,
+            enabled=verify_enabled,  # pcc drops in silicon devicetype
+            pcc=0.96,
+        ),
+    )
