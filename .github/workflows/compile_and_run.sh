0872c4955e50be6b38a15635e1ea2e00189d45fe
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2025-03-31 16:59:17 +0200

    Run perf tests using ttrt (#1506)
    ### Ticket
    f2f request
    
    ### Problem description
    Need a way to run ttrt perf along benchmark tests
    
    ### What's changed
    Find ttrt wheel and ttmlir-opt/ttmlir-translate in tt-mlir successful
    build action based on commit sha of tt-mlir used in forge-fe. Run
    implemented shell script that does required steps to add performance
    data to perf json files sent to analytics.

---
+++ .github/workflows/compile_and_run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# parameters:
+# <mlir_file> <json_file>
+
+echo "run ttmlir-opt on $1"
+./install/bin/ttmlir-opt --tt-register-device="system-desc-path=ttrt-artifacts/system_desc.ttsys" --ttir-to-ttnn-backend-pipeline $1 -o ttnn.mlir
+if [ $? -ne 0 ]; then
+    echo "Error: TTmlir opt command failed."
+    exit 1
+fi
+echo "run ttmlir-translate"
+./install/bin/ttmlir-translate --ttnn-to-flatbuffer ttnn.mlir -o out.ttnn
+if [ $? -ne 0 ]; then
+    echo "Error: TTmlir translate command failed."
+    exit 1
+fi
+echo "run ttrt-perf"
+ttrt perf out.ttnn
+if [ $? -ne 0 ]; then
+    echo "Error: TTRT perf command failed."
+    exit 1
+fi
+echo "run device_perf.py creating $2"
+python ./forge/test/benchmark/device_perf.py -cdp ttrt-artifacts/out.ttnn/perf/ops_perf_results.csv $2
