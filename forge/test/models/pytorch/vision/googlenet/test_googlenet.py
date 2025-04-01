a976593a8e997084878a4581f46d2ee1151579e3
chandrasekaranpradeep <pchandrasekaran@tenstorrent.com>
2025-04-01 06:56:18 +0000

    Test pytorch and paddle models in models ops generation

--- forge/test/models/pytorch/vision/googlenet/test_googlenet.py
+++ forge/test/models/pytorch/vision/googlenet/test_googlenet.py
@@ -1,17 +1,55 @@
 # SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 
 # SPDX-License-Identifier: Apache-2.0
+import paddle
 import pytest
 import torch
 from loguru import logger
+from paddle.vision.models import googlenet
 from PIL import Image
 from torchvision import models, transforms
 
 import forge
+from forge.verify.config import VerifyConfig
+from forge.verify.value_checkers import AutomaticValueChecker
 from forge.verify.verify import verify
 
 from test.models.utils import Framework, Source, Task, build_module_name
 from test.utils import download_model
+
+
+@pytest.mark.xfail
+@pytest.mark.nightly
+def test_googlenet_paddle(forge_property_recorder):
+    # Record model details
+    module_name = build_module_name(
+        framework=Framework.PADDLE,
+        model="googlenet",
+        source=Source.PADDLE,
+        task=Task.IMAGE_CLASSIFICATION,
+    )
+    forge_property_recorder.record_model_name(module_name)
+
+    # Load framework model
+    framework_model = googlenet(pretrained=True)
+
+    # Compile model
+    input_sample = [paddle.rand([1, 3, 224, 224])]
+    compiled_model = forge.compile(
+        framework_model,
+        sample_inputs=input_sample,
+        module_name=module_name,
+        forge_property_handler=forge_property_recorder,
+    )
+
+    # Verify data on sample input
+    verify(
+        input_sample,
+        framework_model,
+        compiled_model,
+        VerifyConfig(value_checker=AutomaticValueChecker(pcc=0.95)),
+        forge_property_handler=forge_property_recorder,
+    )
 
 
 @pytest.mark.nightly
