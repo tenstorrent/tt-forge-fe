ff3bc5e349dd2f8cf06fb0fc98a2ea1f14e2cb9b
Ashok Kumar Kannan <160501980+ashokkumarkannan1@users.noreply.github.com>
2025-04-03 18:41:07 +0530

    Update utils.py

--- forge/test/models/onnx/vision/efficientnet/utils/utils.py
+++ forge/test/models/onnx/vision/efficientnet/utils/utils.py
@@ -24,6 +24,5 @@ def load_inputs(img):
 
     # Apply the transformation
     img_tensor = transforms(img).unsqueeze(0)  # Add batch dimension
-    input_tensors.append(img_tensor)
 
-    return input_tensors
+    return img_tensor
