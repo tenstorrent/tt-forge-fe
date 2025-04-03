4d2a8f4600d0ffeda4b109730a673ba2b54e3df1
Predrag Ilkic <pilkic@tenstorrent.com>
2025-04-03 18:03:00 +0200

    add missing test files

---
+++ forge/csrc/test/verif/test_verif.cpp
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+#include <gtest/gtest.h>
+#include <pybind11/embed.h>
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    pybind11::scoped_interpreter guard{};
+    return RUN_ALL_TESTS();
+}
