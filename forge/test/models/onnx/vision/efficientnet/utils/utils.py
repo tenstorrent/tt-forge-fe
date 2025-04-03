f583390392aa8308e85134518f76d7238d48a955
Ashok Kumar Kannan <160501980+ashokkumarkannan1@users.noreply.github.com>
2025-04-03 18:06:21 +0530

    Update utils.py

--- forge/test/models/onnx/vision/efficientnet/utils/utils.py
+++ forge/test/models/onnx/vision/efficientnet/utils/utils.py
@@ -14,7 +14,7 @@ def load_inputs(img, model):
     data_config = timm.data.resolve_model_data_config(model)
     transforms = timm.data.create_transform(**data_config, is_training=False)
     # Apply the transformation
-    img_tensor = transform(img).unsqueeze(0)  # Add batch dimension
+    img_tensor = transforms(img).unsqueeze(0)  # Add batch dimension
     input_tensors.append(img_tensor)
 
     return input_tensors
