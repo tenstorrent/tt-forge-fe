45b80aa7b4ed59d382d79e3eca046f1b492ce673
Ashok Kumar Kannan <160501980+ashokkumarkannan1@users.noreply.github.com>
2025-04-03 18:42:06 +0530

    Update utils.py

--- forge/test/models/onnx/vision/mobilenetv2/utils/utils.py
+++ forge/test/models/onnx/vision/mobilenetv2/utils/utils.py
@@ -15,6 +15,5 @@ def load_inputs(img, model):
     transforms = timm.data.create_transform(**data_config, is_training=False)
     # Apply the transformation
     img_tensor = transforms(img).unsqueeze(0)  # Add batch dimension
-    input_tensors.append(img_tensor)
 
-    return input_tensors
+    return [img_tensor]
