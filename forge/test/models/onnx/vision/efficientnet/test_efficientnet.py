78ad18e965a79916ed155a05024f61d82756dd6b
Ashok Kumar Kannan <160501980+ashokkumarkannan1@users.noreply.github.com>
2025-04-10 12:51:33 +0000

    Updated efficientnet bringup

--- forge/test/models/onnx/vision/efficientnet/test_efficientnet.py
+++ forge/test/models/onnx/vision/efficientnet/test_efficientnet.py
@@ -2,36 +2,37 @@
 
 # SPDX-License-Identifier: Apache-2.0
 import pytest
-from datasets import load_dataset
+import timm
 import forge
 import onnx
 import torch
 from forge.verify.verify import verify
-import shutil
+from datasets import load_dataset
+from forge.verify.config import VerifyConfig, AutomaticValueChecker
 from utils import load_inputs
+from urllib.request import urlopen
+from PIL import Image
+from test.models.models_utils import print_cls_results
 from forge.forge_property_utils import Framework, Source, Task
-from test.models.model_utils import print_cls_results
-import timm
 
-variants = [
-    "efficientnet_b0",
-    "efficientnet_b1",
-    "efficientnet_b2",
-    "efficientnet_b2a",
-    "efficientnet_b3",
-    "efficientnet_b3a",
-    "efficientnet_b4",
-    "efficientnet_b5",
-    "efficientnet_lite0",
+params = [
+    pytest.param("efficientnet_b0"),
+    pytest.param("efficientnet_b1", marks=[pytest.mark.push]),
+    pytest.param("efficientnet_b2"),
+    pytest.param("efficientnet_b2a"),
+    pytest.param("efficientnet_b3"),
+    pytest.param("efficientnet_b3a"),
+    pytest.param("efficientnet_b4"),
+    pytest.param("efficientnet_b5"),
+    pytest.param("efficientnet_lite0"),
 ]
 
 
-@pytest.mark.push
-@pytest.mark.parametrize("variant", variants, ids=variants)
+@pytest.mark.parametrize("variant", params)
 @pytest.mark.nightly
 def test_efficientnet_onnx(variant, forge_property_recorder, tmp_path):
 
-    # Build Module Name
+    # Record Forge Property
     module_name = forge_property_recorder.record_model_properties(
         framework=Framework.ONNX,
         model="efficientnet",
@@ -43,19 +44,18 @@ def test_efficientnet_onnx(variant, forge_property_recorder, tmp_path):
     # Record Forge Property
     if variant == "efficientnet_b0":
         forge_property_recorder.record_group("red")
-        forge_property_recorder.record_priority("p1")
     else:
         forge_property_recorder.record_group("generality")
 
-    forge_property_recorder.record_model_name(module_name)
-
-    # Load the inputs
-    dataset = load_dataset("huggingface/cats-image")
-    img = dataset["test"]["image"][0]
-    inputs = load_inputs(img)
-
     # Load efficientnet model
     model = timm.create_model(variant, pretrained=True)
+
+    # Load the inputs
+    img = Image.open(
+        urlopen("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/beignets-task-guide.png")
+    )
+
+    inputs = load_inputs(img, model)
     onnx_path = f"{tmp_path}/efficientnet.onnx"
     torch.onnx.export(model, inputs[0], onnx_path)
 
@@ -67,7 +67,6 @@ def test_efficientnet_onnx(variant, forge_property_recorder, tmp_path):
     # Compile model
     compiled_model = forge.compile(onnx_model, inputs, forge_property_handler=forge_property_recorder)
 
-    # Verify data on sample input
     fw_out, co_out = verify(
         inputs,
         framework_model,
