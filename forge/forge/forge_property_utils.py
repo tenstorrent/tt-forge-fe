4b5eeb83ee21b190c5d66637af8d680a314dd4a2
Kohei Yamaguchi <kyamaguchi@tenstorrent.com>
2025-04-14 16:52:36 +0900

    Support Jax/Flax (#1699)
    ### Ticket
    N/A
    
    ### Problem description
    The JAX support implementation is currently inactive (commented out),
    and it is outdated and contains known bugs.
    
    ### What's changed
    This patch adds support for flax.linen.Module as the JAX frontend, along
    with transformers.FlaxPreTrainedModel. Most basic operations now pass,
    but a few tests are currently skipped due to ttnn runtime errors.
    Additionally, the ResNet-50 test from transformers has not yet fully
    passed due to unresolved issues. I’ve added TODO comments and documented
    the reasons for each skipped test. I’m actively working on resolving
    these issues.
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes

--- forge/forge/forge_property_utils.py
+++ forge/forge/forge_property_utils.py
@@ -35,6 +35,7 @@ class Framework(BaseEnum):
     TENSORFLOW = ("tf", "TensorFlow")
     ONNX = ("onnx", "ONNX")
     PADDLE = ("pd", "PaddlePaddle")
+    JAX = ("jax", "JAX")
 
 
 class Task(BaseEnum):
