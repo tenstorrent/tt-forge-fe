4fa79e6f92e8e2cc9cec2e735bfd7c2d69aa61b4
dgolubovicTT <dgolubovic@tenstorrent.com>
2025-04-16 23:20:12 +0000

    - Set user-defined data format override in post-initial graph pass.   Insert cast nodes on all input edges to ensure that constants, buffers, and parameters are in the lowered dtype. - Move TVM module generation inside the block where the traced model is in float32, since that is the format supported by TVM (due to its NumPy base). - Extend ConvertEmulatedDtypes to convert model buffers (e.g., BatchNorm runing mean and variance) to the desired dtype.   Buffers are not considered parameters but are inputs to the computational graph and must be aligned in data format with the rest of the model.

--- forge/test/benchmark/scripts/run_benchmark_wh.sh
+++ forge/test/benchmark/scripts/run_benchmark_wh.sh
@@ -11,7 +11,7 @@
 python forge/test/benchmark/benchmark.py -m mnist_linear -bs 32 -lp 32 -o forge-benchmark-e2e-mnist.json
 
 # Resnet HF
-python forge/test/benchmark/benchmark.py -m resnet50_hf -bs 3 -lp 32 -o forge-benchmark-e2e-resnet50_hf.json
+python forge/test/benchmark/benchmark.py -m resnet50_hf -bs 8 -lp 32 -o forge-benchmark-e2e-resnet50_hf.json
 
 # Llama
 python forge/test/benchmark/benchmark.py -m llama -bs 1 -lp 32 -o forge-benchmark-e2e-llama.json
