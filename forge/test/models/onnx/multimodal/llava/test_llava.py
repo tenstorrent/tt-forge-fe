81dd7f0ad70420735ed9ea9a267dda9472e3e4e5
mramanathan <mramanathan@tenstorrent.com>
2025-04-24 08:40:41 +0000

    Add llava onnx test

---
+++ forge/test/models/onnx/multimodal/llava/test_llava.py
@@ -0,0 +1,85 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+import pytest
+import torch
+from transformers import AutoProcessor, LlavaForConditionalGeneration
+
+import torch
+import onnx
+import forge
+from forge.forge_property_utils import Framework, Source, Task
+from forge.verify.verify import verify
+
+from loguru import logger
+from test.models.pytorch.multimodal.llava.utils import load_inputs
+
+
+class Wrapper(torch.nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, input_ids, attention_mask, pixel_values):
+        inputs = {"input_ids": input_ids, "attention_mask": attention_mask, "pixel_values": pixel_values}
+        output = self.model(**inputs)
+        return output.logits
+
+
+def load_model(variant):
+    processor = AutoProcessor.from_pretrained(variant)
+    model = LlavaForConditionalGeneration.from_pretrained(variant)
+    model = Wrapper(model)
+    return model, processor
+
+
+variants = ["llava-hf/llava-1.5-7b-hf"]
+
+
+@pytest.mark.nightly
+@pytest.mark.parametrize("variant", variants, ids=variants)
+def test_llava(forge_property_recorder, variant):
+
+    # Record Forge Property
+    module_name = forge_property_recorder.record_model_properties(
+        framework=Framework.PYTORCH,
+        model="llava",
+        variant=variant,
+        task=Task.CONDITIONAL_GENERATION,
+        source=Source.HUGGINGFACE,
+    )
+
+    # Record Forge Property
+    forge_property_recorder.record_group("red")
+
+    framework_model, processor = load_model(variant)
+    image = "https://www.ilankelman.org/stopsigns/australia.jpg"
+    text = "What’s shown in this image?"
+
+    # Input sample
+    input_ids, attn_mask, pixel_values = load_inputs(image, text, processor)
+    inputs = [input_ids, attn_mask, pixel_values]
+
+    onnx_path = "./llava.onnx"
+    logger.info("Export starts")
+    torch.onnx.export(
+        framework_model,
+        args=(input_ids, attn_mask, pixel_values),
+        f=onnx_path,
+        opset_version=17,
+    )
+    logger.info("Export finished successfully")
+    # Load ONNX model
+    onnx_model = onnx.load(onnx_path)
+    onnx.checker.check_model(onnx_model)
+    framework_model = forge.OnnxModule(module_name, onnx_model)
+
+    # Forge compile framework model
+    compiled_model = forge.compile(
+        onnx_model, sample_inputs=inputs, module_name=module_name, forge_property_handler=forge_property_recorder
+    )
+
+    # Model Verification
+    verify(inputs, framework_model, compiled_model, forge_property_handler=forge_property_recorder)
