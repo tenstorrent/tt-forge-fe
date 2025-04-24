4abe445293db3ebdf50b9d1e28075191e3538064
mramanathan <mramanathan@tenstorrent.com>
2025-04-24 09:12:23 +0000

    Add Phi3.5 vision onnx test

---
+++ forge/test/models/onnx/text/phi3/test_phi3_5_vision.py
@@ -0,0 +1,87 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+import onnx
+from transformers import AutoModelForCausalLM, AutoProcessor
+
+import forge
+from forge.forge_property_utils import Framework, Source, Task
+from forge.verify.verify import verify
+from test.models.models_utils import build_optimum_cli_command
+
+from test.models.pytorch.multimodal.phi3.utils.utils import load_input
+from test.utils import download_model
+
+
+class Wrapper(torch.nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, input_ids, attention_mask, pixel_values, image_sizes):
+        return self.model(input_ids, attention_mask, None, None, None, pixel_values, image_sizes)
+
+
+variants = ["microsoft/Phi-3.5-vision-instruct"]
+
+
+@pytest.mark.nightly
+@pytest.mark.parametrize("variant", variants)
+def test_phi3_5_vision(forge_property_recorder, variant, tmp_path):
+
+    # Record Forge Property
+    module_name = forge_property_recorder.record_model_properties(
+        framework=Framework.PYTORCH,
+        model="phi3_5_vision",
+        variant=variant,
+        task=Task.MULTIMODAL_TEXT_GENERATION,
+        source=Source.HUGGINGFACE,
+    )
+
+    # Record Forge Property
+    forge_property_recorder.record_group("red")
+
+    # Load model and processor
+    model = download_model(
+        AutoModelForCausalLM.from_pretrained,
+        variant,
+        return_dict=False,
+        trust_remote_code=True,
+        use_cache=False,
+        _attn_implementation="eager",
+    )
+    model.eval()
+    framework_model = Wrapper(model)
+    processor = download_model(AutoProcessor.from_pretrained, variant, trust_remote_code=True, num_crops=4)
+
+    # prepare input
+    inputs = load_input(processor)
+
+    # Export model to ONNX
+    onnx_path = f"{tmp_path}/model.onnx"
+    input_ids, attention_mask, pixel_values, image_sizes = inputs
+    torch.onnx.export(
+        framework_model, (input_ids, attention_mask, pixel_values, image_sizes), onnx_path, opset_version=17
+    )
+
+    # Load framework model
+    onnx_model = onnx.load(onnx_path)
+
+    onnx.checker.check_model(onnx_path)
+    framework_model = forge.OnnxModule(module_name, onnx_model)
+
+    # Compile model
+    compiled_model = forge.compile(
+        onnx_model, inputs, forge_property_handler=forge_property_recorder, module_name=module_name
+    )
+
+    # Model Verification
+    verify(
+        inputs,
+        framework_model,
+        compiled_model,
+        forge_property_handler=forge_property_recorder,
+    )
