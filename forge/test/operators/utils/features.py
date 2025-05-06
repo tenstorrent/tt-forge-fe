0b630757ed486f09b5639df1e35e4ff9767df475
Vladimir Brkic <vbrkic@tenstorrent.com>
2025-05-06 13:39:52 +0000

    TestSweepsFeatures
    (cherry picked from commit 3c91fc4dfaa9e3feb358ea387049a764ac704ad1)

--- forge/test/operators/utils/features.py
+++ forge/test/operators/utils/features.py
@@ -4,14 +4,48 @@
 
 import os
 
+from loguru import logger
+from dataclasses import dataclass
 
-class TestFeaturesConfiguration:
-    """Store test features configuration"""
+
+@dataclass
+class TestSweepsFeaturesParams:
+    """Test features configuration parameters"""
 
     __test__ = False  # Disable pytest collection
+
+    skip_forge_verification: bool
+    dry_run: bool
 
     @staticmethod
     def get_env_property(env_var: str, default_value: str):
         return os.getenv(env_var, default_value)
 
-    SKIP_FORGE_VERIFICATION = get_env_property("SKIP_FORGE_VERIFICATION", "false").lower() == "true"
+    @staticmethod
+    def get_env_property_bool(env_var: str, default_value: bool):
+        return os.getenv(env_var, f"{default_value}".lower()).lower() == "true"
+
+    @classmethod
+    def from_env(cls):
+        """Create a TestSweepsFeaturesParams object from environment variables"""
+
+        skip_forge_verification = cls.get_env_property_bool("SKIP_FORGE_VERIFICATION", False)
+        dry_run = cls.get_env_property_bool("DRY_RUN", False)
+
+        # Construct feature parameters
+        feature_params = cls(
+            skip_forge_verification=skip_forge_verification,
+            dry_run=dry_run,
+        )
+
+        logger.info(f"Features parameters: {feature_params}")
+
+        return feature_params
+
+
+class TestSweepsFeatures:
+    """Store test features configuration parameters"""
+
+    __test__ = False  # Disable pytest collection
+
+    params = TestSweepsFeaturesParams.from_env()
