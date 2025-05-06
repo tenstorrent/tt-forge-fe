ffe51e96a6a0dd4d469b547c8e8d405e6fc2b5f0
Vladimir Brkic <vbrkic@tenstorrent.com>
2025-05-06 13:39:52 +0000

    New failing reasons definition
    Central failing reasons register

---
+++ forge/test/operators/pytorch/ids/failing_reasons_register.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Register of all detected failing reasons by operator
+
+from test.operators.utils import FailingReasons
+
+
+class FailingReasonsRegister:
+
+    all = [
+    ]
