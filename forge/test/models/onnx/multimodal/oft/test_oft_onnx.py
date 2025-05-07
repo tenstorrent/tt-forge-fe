6f1e50d29198e8c8846865cf7297ce6ed2f13e88
sai_arthi_raguram <ctr-sraguram@ext.tenstorrent.com>
2025-05-07 05:18:23 +0000

    Fixes CI failures

--- forge/test/models/onnx/multimodal/oft/test_oft_onnx.py
+++ forge/test/models/onnx/multimodal/oft/test_oft_onnx.py
@@ -7,7 +7,7 @@ import forge
 from forge.verify.verify import verify
 
 from test.models.onnx.multimodal.oft.utils.oft_utils import get_inputs, get_models
-from test.models.utils import Framework, Source, Task
+from forge.forge_property_utils import Framework, Source, Task
 
 
 @pytest.mark.skip(reason="Segmentation Fault")
