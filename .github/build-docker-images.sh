fd6ed876f66a6bec34cc65c8d6adc975535f65c4
jmcgrathTT <jmcgrath@tenstorrent.com>
2025-05-07 00:33:34 +0000

    test

--- .github/build-docker-images.sh
+++ .github/build-docker-images.sh
@@ -14,10 +14,14 @@ IRD_IMAGE_NAME=ghcr.io/$REPO/tt-forge-fe-ird-ubuntu-22-04
 DOCKER_TAG=$(./.github/get-docker-tag.sh)
 echo "Docker tag: $DOCKER_TAG"
 
+# Are we on main branch
+ON_MAIN=$(git branch --show-current | grep -q main && echo "true" || echo "false")
+
 build_and_push() {
     local image_name=$1
     local dockerfile=$2
-    local from_image=$3
+    local on_main=$3
+    local from_image=$4
 
     if docker manifest inspect $image_name:$DOCKER_TAG > /dev/null; then
         echo "Image $image_name:$DOCKER_TAG already exists"
@@ -33,11 +37,17 @@ build_and_push() {
         echo "Pushing image $image_name:$DOCKER_TAG"
         docker push $image_name:$DOCKER_TAG
     fi
+
+    # If we are on main branch also push the latest tag
+    if [ "$on_main" = "true" ]; then
+        printf "\nPushing latest tag for $image_name"
+        # Used to push both tags in one command on the original sha256 sum layer remotely (docker create manifest create a new sha256 sum only with only with new tag)
+        docker buildx imagetools create $image_name:$DOCKER_TAG --tag $image_name:latest --tag $image_name:$DOCKER_TAG
+    fi
 }
 
-build_and_push $BASE_IRD_IMAGE_NAME .github/Dockerfile.ird base
-build_and_push $CI_IMAGE_NAME .github/Dockerfile.ci
-build_and_push $IRD_IMAGE_NAME .github/Dockerfile.ird ci
+build_and_push $CI_IMAGE_NAME .github/Dockerfile.ci $ON_MAIN
+build_and_push $IRD_IMAGE_NAME .github/Dockerfile.ird $ON_MAIN
 
 echo "All images built and pushed successfully"
 echo "CI_IMAGE_NAME:"
