fe428782e6948589fa06fb1257421b56199f1412
sai_arthi_raguram <ctr-sraguram@ext.tenstorrent.com>
2025-05-08 07:26:41 +0000

    Adds eye op test for Hippyn model

---
+++ forge/test/mlir/operators/tm/test_eye.py
@@ -0,0 +1,42 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+import torch
+import forge
+from forge.verify.verify import verify
+
+
+@pytest.mark.parametrize(
+    "n, m, dtype",
+    [
+        (4, 4, torch.float32),
+        (6, 7, torch.int32),
+        (8, 8, torch.bool),
+    ],
+)
+@pytest.mark.push
+def test_eye(forge_property_recorder, n, m, dtype):
+    class EyeModel(torch.nn.Module):
+        def __init__(self, n, m, dtype):
+            super().__init__()
+            self.n = n
+            self.m = m
+            self.dtype = dtype
+
+        def forward(self, dummy):
+            x = dummy + 1
+            if self.m is None:
+                out = torch.eye(self.n, dtype=self.dtype)
+            else:
+                out = torch.eye(self.n, self.m, dtype=self.dtype)
+            return out + x * 0
+
+    dummy_input = torch.randn(1)
+    inputs = [dummy_input]
+
+    model = EyeModel(n, m, dtype)
+    model.eval()
+
+    compiled_model = forge.compile(model, sample_inputs=inputs, forge_property_handler=forge_property_recorder)
+    verify(inputs, model, compiled_model, forge_property_handler=forge_property_recorder)
