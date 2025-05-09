bbf98b33a7438854201643d30f1d2a2d15b1ce0d
Vladimir Brkic <vbrkic@tenstorrent.com>
2025-05-09 12:27:04 +0000

    Skip matmul high memory tests [skip ci]

--- forge/test/operators/utils/failing_reasons.py
+++ forge/test/operators/utils/failing_reasons.py
@@ -953,6 +953,10 @@ class FailingReasons(Enum):
         description="Fatal error occured",
     )
 
+    HIGH_MEMORY = FailingReason(
+        description="High memory usage",
+    )
+
     UNSUPPORTED_INPUT_SOURCE = FailingReason(
         description="Unsupported input source",
     )
