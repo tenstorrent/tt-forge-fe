60a8b761059bb084fe59502e932df95b87fc42e5
Vladimir Brkic <vbrkic@tenstorrent.com>
2025-05-09 12:27:04 +0000

    Skip matmul high memory tests [skip ci]

--- forge/test/operators/pytorch/ids/failing_reasons_register.py
+++ forge/test/operators/pytorch/ids/failing_reasons_register.py
@@ -80,4 +80,6 @@ class FailingReasonsRegister:
         ("transpose", FailingReasons.MLIR_RUNTIME),
     ]
 
-    skip = []
+    skip = [
+        ("matmul", FailingReasons.HIGH_MEMORY, None),
+    ]
