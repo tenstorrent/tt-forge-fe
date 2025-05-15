bdee2e4047e2c08915c200c7181a53faf88e2f02
svuckovic <svuckovic@tenstorrent.com>
2025-05-15 09:50:00 +0000

    pytest -svv forge/test/models/pytorch/vision/deit/test_deit.py::test_customer_team_tiny_deit

--- forge/test/models/pytorch/vision/deit/test_deit.py
+++ forge/test/models/pytorch/vision/deit/test_deit.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 import pytest
 from datasets import load_dataset
-from transformers import AutoFeatureExtractor, ViTForImageClassification
+from transformers import AutoFeatureExtractor, ViTForImageClassification, DeiTConfig
 
 import forge
 from forge.forge_property_utils import Framework, Source, Task
@@ -61,3 +61,27 @@ def test_deit_imgcls_hf_pytorch(forge_property_recorder, variant):
 
     # Model Verification
     verify(inputs, framework_model, compiled_model, forge_property_handler=forge_property_recorder)
+
+
+def test_customer_team_tiny_deit(forge_property_recorder):
+    sequence_size = 224
+    image_channels = 3
+    image_size = 224
+
+    model_name = "facebook/deit-tiny-patch16-224"
+    config = DeiTConfig.from_pretrained(model_name)
+    batch_size = 1
+    config.num_hidden_layers = 12
+    model = ViTForImageClassification.from_pretrained(model_name, config=config)
+    dataset = load_dataset("huggingface/cats-image")
+    image = dataset["test"]["image"][0]
+    image_processor = AutoFeatureExtractor.from_pretrained("facebook/deit-tiny-patch16-224")
+    torch_pixel_values = image_processor(image, return_tensors="pt").pixel_values
+    torch_pixel_values = torch_pixel_values.repeat(batch_size, 1, 1, 1)
+    compiled_model = forge.compile(model, sample_inputs=[torch_pixel_values])
+
+    # file_path = "generated_export_deit_tiny.cpp"
+    # compiled_model.export_to_cpp(file_path)
+
+    # Inference with compiled model
+    output = compiled_model(torch_pixel_values)
