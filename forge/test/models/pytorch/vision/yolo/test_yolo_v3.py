ea425bfc48e6a52cd08b9ede8cc60b201fa9b88e
Nikola Vukobrat <124874832+nvukobratTT@users.noreply.github.com>
2025-05-21 20:12:10 +0200

    Integrate Forge models repository into FFE as submodule (#2050)
    Integration with the Forge Models repo. 
    
    This repository will host future models that our compiler will run.
    Also, introducing a few models from this repository:
    - OFT (variant based on ResNet50)
    - VGG-Unet
    - Yolo v3
    - Yolo v4
    
    Fix #2049

---
+++ forge/test/models/pytorch/vision/yolo/test_yolo_v3.py
@@ -0,0 +1,54 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+
+import forge
+from forge._C import DataFormat
+from forge.config import CompilerConfig
+from forge.forge_property_utils import Framework, Source, Task
+from forge.verify.config import VerifyConfig
+from forge.verify.value_checkers import AutomaticValueChecker
+from forge.verify.verify import verify
+
+from third_party.tt_forge_models.yolov3 import ModelLoader  # isort:skip
+
+
+@pytest.mark.nightly
+def test_yolo_v3(forge_property_recorder):
+    # Record Forge Property
+    module_name = forge_property_recorder.record_model_properties(
+        framework=Framework.PYTORCH,
+        model="Yolo v3",
+        variant="default",
+        task=Task.OBJECT_DETECTION,
+        source=Source.GITHUB,
+    )
+    forge_property_recorder.record_group("red")
+    forge_property_recorder.record_priority("P1")
+
+    # Load model and input
+    framework_model = ModelLoader.load_model()
+    input_sample = ModelLoader.load_inputs()
+
+    # Configurations
+    compiler_cfg = CompilerConfig()
+    compiler_cfg.default_df_override = DataFormat.Float16_b
+
+    # Forge compile framework model
+    compiled_model = forge.compile(
+        framework_model,
+        sample_inputs=[input_sample],
+        module_name=module_name,
+        forge_property_handler=forge_property_recorder,
+        compiler_cfg=compiler_cfg,
+    )
+
+    # Model Verification
+    verify(
+        [input_sample],
+        framework_model,
+        compiled_model,
+        forge_property_handler=forge_property_recorder,
+        verify_cfg=VerifyConfig(value_checker=AutomaticValueChecker(pcc=0.95)),
+    )
---
+++ forge/test/models/pytorch/vision/yolo/test_yolo_v4.py
@@ -0,0 +1,63 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+
+import forge
+from forge._C import DataFormat
+from forge.config import CompilerConfig
+from forge.forge_property_utils import Framework, Source, Task
+from forge.verify.verify import verify
+
+from third_party.tt_forge_models.yolov4 import ModelLoader  # isort:skip
+
+
+class Wrapper(torch.nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, image: torch.Tensor):
+        x, y, z = self.model(image)
+        # Post processing inside model casts output to float32,
+        # even though raw output is aligned with image.dtype
+        # Therefore we need to cast it back to image.dtype
+        return x.to(image.dtype), y.to(image.dtype), z.to(image.dtype)
+
+
+# @pytest.mark.xfail
+@pytest.mark.nightly
+def test_yolo_v4(forge_property_recorder):
+    # Record Forge Property
+    module_name = forge_property_recorder.record_model_properties(
+        framework=Framework.PYTORCH,
+        model="Yolo v4",
+        variant="default",
+        task=Task.OBJECT_DETECTION,
+        source=Source.GITHUB,
+    )
+    forge_property_recorder.record_group("red")
+    forge_property_recorder.record_priority("P1")
+
+    # Load model and input
+    framework_model = ModelLoader.load_model()
+    framework_model = Wrapper(framework_model)
+    input_sample = ModelLoader.load_inputs()
+
+    # Configurations
+    compiler_cfg = CompilerConfig()
+    compiler_cfg.default_df_override = DataFormat.Float16_b
+
+    # Forge compile framework model
+    compiled_model = forge.compile(
+        framework_model,
+        sample_inputs=[input_sample],
+        module_name=module_name,
+        forge_property_handler=forge_property_recorder,
+        compiler_cfg=compiler_cfg,
+    )
+
+    # Model Verification
+    verify([input_sample], framework_model, compiled_model, forge_property_handler=forge_property_recorder)
