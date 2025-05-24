2dcbb10dd23c0e068b79f8d132632171880322d0
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2025-05-24 09:47:13 +0200

    fix

--- conftest.py
+++ conftest.py
@@ -17,6 +17,7 @@ from forge.forge_property_utils import (
 )
 from forge._C.verif import malloc_trim
 from sys import getsizeof
+from requests_cache import DO_NOT_CACHE, NEVER_EXPIRE
 
 
 def pytest_sessionstart(session):
