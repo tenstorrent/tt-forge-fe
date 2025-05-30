02cbc8f4a45c83dfd5c9d750ee855930d859ae08
vkovinicTT <vkovinic@tenstorrent.com>
2025-05-30 10:03:57 +0000

    enabled optimization pass for all failing push tests

--- forge/test/models/pytorch/vision/ghostnet/test_ghostnet.py
+++ forge/test/models/pytorch/vision/ghostnet/test_ghostnet.py
@@ -50,7 +50,7 @@ def test_ghostnet_timm(variant):
     inputs = [inputs[0].to(torch.bfloat16)]
 
     data_format_override = DataFormat.Float16_b
-    compiler_cfg = CompilerConfig(default_df_override=data_format_override)
+    compiler_cfg = CompilerConfig(default_df_override=data_format_override, enable_optimization_passes=True)
 
     # Forge compile framework model
     compiled_model = forge.compile(
