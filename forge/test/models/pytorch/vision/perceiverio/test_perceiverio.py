8d99468a3c4768f2bc39f67c2dc8e772e92f2842
vkovinicTT <vkovinic@tenstorrent.com>
2025-05-30 14:50:10 +0000

    set optimization flag to true for failing nightly tests

--- forge/test/models/pytorch/vision/perceiverio/test_perceiverio.py
+++ forge/test/models/pytorch/vision/perceiverio/test_perceiverio.py
@@ -92,7 +92,7 @@ def test_perceiverio_for_image_classification_pytorch(variant):
     inputs = [pixel_values.to(torch.bfloat16)]
 
     data_format_override = DataFormat.Float16_b
-    compiler_cfg = CompilerConfig(default_df_override=data_format_override)
+    compiler_cfg = CompilerConfig(default_df_override=data_format_override, enable_optimization_passes=True)
 
     # Forge compile framework model
     compiled_model = forge.compile(
