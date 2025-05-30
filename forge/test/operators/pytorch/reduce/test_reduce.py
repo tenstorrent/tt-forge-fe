a2438420119e7de03cf32b20c0522eccc596068c
Konstantin Milanovic <kmilanovic@tenstorrent.com>
2025-05-30 14:45:33 +0000

    update verification for reduce ops

--- forge/test/operators/pytorch/reduce/test_reduce.py
+++ forge/test/operators/pytorch/reduce/test_reduce.py
@@ -1,62 +1,17 @@
 # SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 
 # SPDX-License-Identifier: Apache-2.0
-#
-# Tests for testing of reduce operators
-#
-# In this test we test pytorch reduce operators
-
-# GENERAL OP SUPPORT TEST PLAN:
-# 1. Operand type - any supported type
-# 2. Operand source(s):
-# (+)  2.1 From another op
-#       - Operator -> input
-# (+)  2.2 From DRAM queue - removed from test plan
-#       - Operator is first node in network
-#       - Input_queue flag = false
-# (+)  2.3 Const Inputs (const eval pass)
-#       - Operator where all inputs are constants.
-# (+)  2.4 From host
-#       - Input tensor as input of network
-#       - Operator is first node in network
-#       - Input_queue flag = true
-# 3 Operand shapes type(s):
-# (+)  3.1 Full tensor (i.e. full expected shape)
-#       - 3-4 by default P1 (high prioriy)
-#       - 2, 5, ++ include P2 (lower prioriy)
-# (+)  3.2 Tensor reduce on one or more dims to 1
-#       - Vector
-#       - Only one dim is not equal to 1
-# (+)  3.3 Scalar P2
-#       - Create tensor of dimension equal to 0 (tensor from scalar) or just to use scalar as simple value
-# 4. Operand / output size of dimensions (few examples of each, 10 values total)
-# (+)  4.1 Divisible by 32
-# (+)  4.2 Prime numbers
-# (+)  4.3 Very large (thousands, 10s of thousands)
-#       - 100x100, 100x1000
-#       - maybe nightly only
-# (+)  4.4 Extreme ratios between height/width
-#      4.5 ...probably many more interesting combinations here
-# 5. Data format - all supported formats
-# (/)  5.1 Output DF
-# (/)  5.2 Intermediate DF
-# (/)  5.3 Accumulation DF
-# (+)  5.4 Operand DFs
-#       - Fix HiFi4 for math fidelity value
-# (+) 6. Math fidelity - LoFi, HiFi2a, Hifi2b, Hifi3, Hifi4
-#       - Fix fp16b (default) for data format value
-# (/) 7. Special attributes - if applicable.. like approx_mode for Exp, for example
-# (/) 8. Special cases - if applicable
-# 9. Variable number of operands - if applicable
-# (/) Few representative values
-# (/) Reuse inputs for selected operators
 
 
+import math
+from forge.verify.config import VerifyConfig
+from forge.verify.value_checkers import AllCloseValueChecker, AutomaticValueChecker
 import pytest
 
 from typing import List, Dict
 from loguru import logger
 
+import random
 import torch
 import forge
 import forge.op
@@ -64,29 +19,24 @@ import forge.op
 from forge.op_repo import TensorShape
 
 from test.operators.utils import VerifyUtils
-from test.operators.utils import ShapeUtils
 from test.operators.utils import InputSource
 from test.operators.utils import TestVector
 from test.operators.utils import TestPlan
 from test.operators.utils import FailingReasons
-from test.operators.utils.compat import TestDevice
 from test.operators.utils import TestCollection
 from test.operators.utils import TestCollectionCommon
 from test.operators.utils import ValueRanges
-from test.operators.utils.utils import PytorchUtils
+from test.operators.utils.compat import TestDevice
+from test.operators.utils.utils import PytorchUtils, TensorUtils
+from test.operators.utils.test_data import TestCollectionTorch
 from test.operators.pytorch.ids.loader import TestIdsDataLoader
 
 
 class ModelFromAnotherOp(torch.nn.Module):
-
-    model_name = "model_op_src_from_another_op"
-
-    def __init__(self, operator, opname, shape, kwargs):
+    def __init__(self, operator, kwargs):
         super(ModelFromAnotherOp, self).__init__()
-        self.testname = "Reduce_pytorch_operator_" + opname + "_test_op_src_from_another_op"
+        self.testname = "Reduce_pytorch_operator_test_op_src_from_another_op"
         self.operator = operator
-        self.opname = opname
-        self.shape = shape
         self.kwargs = kwargs
 
     def forward(self, x: torch.Tensor):
@@ -97,15 +47,10 @@ class ModelFromAnotherOp(torch.nn.Module):
 
 
 class ModelDirect(torch.nn.Module):
-
-    model_name = "model_op_src_from_host"
-
-    def __init__(self, operator, opname, shape, kwargs):
+    def __init__(self, operator, kwargs):
         super(ModelDirect, self).__init__()
-        self.testname = "Reduce_pytorch_operator_" + opname + "_test_op_src_from_host"
+        self.testname = "Reduce_pytorch_operator_test_op_src_from_host"
         self.operator = operator
-        self.opname = opname
-        self.shape = shape
         self.kwargs = kwargs
 
     def forward(self, x: torch.Tensor):
@@ -114,19 +59,18 @@ class ModelDirect(torch.nn.Module):
 
 
 class ModelConstEvalPass(torch.nn.Module):
-
-    model_name = "model_op_src_const_eval_pass"
-
-    def __init__(self, operator, opname, shape, kwargs):
+    def __init__(self, operator, shape, kwargs, dtype, value_range):
         super(ModelConstEvalPass, self).__init__()
-        self.testname = "Reduce_pytorch_operator_" + opname + "_test_op_src_const_eval_pass"
+        self.testname = "Reduce_pytorch_operator_test_op_src_const_eval_pass"
         self.operator = operator
-        self.opname = opname
-        self.shape = shape
-        self.constant_shape = ShapeUtils.reduce_microbatch_size(shape)
         self.kwargs = kwargs
 
-        self.c1 = torch.rand(*self.constant_shape) - 0.5
+        self.c1 = TensorUtils.create_torch_constant(
+            input_shape=shape,
+            dev_data_format=dtype,
+            value_range=value_range,
+        )
+        self.register_buffer("constant", self.c1)
 
     def forward(self, x):
         v1 = self.operator(self.c1, **self.kwargs)
@@ -149,26 +93,35 @@ class TestVerification:
         cls,
         test_device: TestDevice,
         test_vector: TestVector,
-        number_of_operands: int = 1,
         input_params: List[Dict] = [],
         warm_reset: bool = False,
     ):
         """Common verification function for all tests"""
 
         operator = PytorchUtils.get_op_class_by_name(test_vector.operator)
-
         kwargs = test_vector.kwargs if test_vector.kwargs else {}
+        value_range = ValueRanges.SMALL
 
         model_type = cls.MODEL_TYPES[test_vector.input_source]
-        pytorch_model = model_type(
-            operator=operator,
-            opname=test_vector.operator,
-            shape=test_vector.input_shape,
-            kwargs=kwargs,
+        pytorch_model = (
+            model_type(
+                operator=operator,
+                shape=test_vector.input_shape,
+                kwargs=kwargs,
+                dtype=test_vector.dev_data_format,
+                value_range=value_range,
+            )
+            if test_vector.input_source in (InputSource.CONST_EVAL_PASS,)
+            else model_type(operator, kwargs)
         )
 
-        input_shapes = tuple([test_vector.input_shape for _ in range(number_of_operands)])
+        input_shapes = tuple([test_vector.input_shape])
         logger.trace(f"***input_shapes: {input_shapes}")
+
+        # We use AllCloseValueChecker in all cases except for integer data formats:
+        verify_config = VerifyConfig(value_checker=AllCloseValueChecker(atol=1e-2, rtol=1e-8))
+        if test_vector.dev_data_format in TestCollectionTorch.int.dev_data_formats:
+            verify_config = VerifyConfig(value_checker=AutomaticValueChecker())
 
         VerifyUtils.verify(
             model=pytorch_model,
@@ -177,10 +130,10 @@ class TestVerification:
             input_params=input_params,
             dev_data_format=test_vector.dev_data_format,
             math_fidelity=test_vector.math_fidelity,
-            # Old behavior when dev_data_format was not set
-            value_range=None if test_vector.dev_data_format is not None else ValueRanges.SMALL_POSITIVE,
-            pcc=test_vector.pcc,
             warm_reset=warm_reset,
+            value_range=value_range,
+            deprecated_verification=False,
+            verify_config=verify_config,
         )
 
 
@@ -192,22 +145,10 @@ class TestParamsData:
 
     @classmethod
     def generate_kwargs(cls, test_vector: TestVector):
-        shape_with_kwargs = cls.extend_shape_with_dims_and_keepdims(test_vector.input_shape)
-        kwarg_list = []
-        for item in shape_with_kwargs:
-            kwargs = {}
-            kwargs["dim"] = item[1]
-            kwargs["keepdim"] = item[2]
-            kwarg_list.append(kwargs)
-        return kwarg_list
-
-    @classmethod
-    def extend_shape_with_dims_and_keepdims(cls, shape):
-        shape_with_dims_and_keepdims = list()
-        for dim in list(range(0, len(shape), 1)):
-            shape_with_dims_and_keepdims.append((shape, dim, True))
-            shape_with_dims_and_keepdims.append((shape, dim, False))
-        return shape_with_dims_and_keepdims
+        rng = random.Random(math.prod(test_vector.input_shape))
+        dim = rng.choice(range(0, len(test_vector.input_shape)))
+        for ch in [True, False]:
+            yield {"dim": dim, "keepdim": ch}
 
 
 class TestCollectionData:
@@ -221,14 +162,18 @@ class TestCollectionData:
         ],
         input_sources=TestCollectionCommon.all.input_sources,
         input_shapes=TestCollectionCommon.all.input_shapes,
-        dev_data_formats=TestCollectionCommon.all.dev_data_formats,
+        dev_data_formats=[
+            item
+            for item in TestCollectionTorch.all.dev_data_formats
+            if item not in TestCollectionTorch.single.dev_data_formats
+        ],
         math_fidelities=TestCollectionCommon.all.math_fidelities,
     )
 
     single = TestCollection(
         input_sources=TestCollectionCommon.single.input_sources,
         input_shapes=TestCollectionCommon.single.input_shapes,
-        dev_data_formats=TestCollectionCommon.single.dev_data_formats,
+        dev_data_formats=TestCollectionTorch.single.dev_data_formats,
         math_fidelities=TestCollectionCommon.single.math_fidelities,
     )
 
@@ -242,7 +187,11 @@ class TestCollectionData:
         operators=[
             "mean",  # 01
         ],
-        dev_data_formats=TestCollectionCommon.float.dev_data_formats,
+        dev_data_formats=[
+            item
+            for item in TestCollectionTorch.float.dev_data_formats
+            if item not in TestCollectionTorch.single.dev_data_formats
+        ],
     )
 
 
@@ -258,60 +207,59 @@ TestParamsData.test_plan = TestPlan(
     ),
     collections=[
         # Test plan:
-        # 2. Operand source(s):
-        # 3. Operand shapes type(s):
-        # 4. Operand / output size of dimensions
+        # torch.sum(input), torch.mean(input)
+        TestCollection(
+            operators=TestCollectionData.all.operators,
+            input_sources=TestCollectionCommon.all.input_sources,
+            input_shapes=TestCollectionCommon.all.input_shapes,
+        ),
+        # torch.sum(input, kwargs), torch.mean(input, kwargs)
         TestCollection(
             operators=TestCollectionData.all.operators,
             input_sources=TestCollectionData.all.input_sources,
             input_shapes=TestCollectionData.all.input_shapes,
             kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
         ),
-        # Test plan:
-        # 5. Data format - sum operator
+        # Data format - sum operator
         TestCollection(
             operators=TestCollectionData.sum.operators,
             input_sources=TestCollectionData.single.input_sources,
             input_shapes=TestCollectionData.single.input_shapes,
-            kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
-            dev_data_formats=[
-                item
-                for item in TestCollectionData.all.dev_data_formats
-                if item not in TestCollectionData.single.dev_data_formats
-            ],
+            kwargs=[{"dim": 1, "keepdim": False}],
+            dev_data_formats=TestCollectionData.all.dev_data_formats,
             math_fidelities=TestCollectionData.single.math_fidelities,
         ),
-        # 5. Data format - mean operator
+        # Data format - mean operator
         TestCollection(
             operators=TestCollectionData.mean.operators,
             input_sources=TestCollectionData.single.input_sources,
             input_shapes=TestCollectionData.single.input_shapes,
-            kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
-            dev_data_formats=[
-                item
-                for item in TestCollectionData.mean.dev_data_formats
-                if item not in TestCollectionData.single.dev_data_formats
-            ],
+            kwargs=[{"dim": 1, "keepdim": False}],
+            dev_data_formats=TestCollectionData.mean.dev_data_formats,
             math_fidelities=TestCollectionData.single.math_fidelities,
         ),
-        # Test plan:
-        # 6. Math fidelity
+        # Math fidelity
         TestCollection(
             operators=TestCollectionData.all.operators,
             input_sources=TestCollectionData.single.input_sources,
             input_shapes=TestCollectionData.single.input_shapes,
-            kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
+            kwargs=[{"dim": 1, "keepdim": False}],
             dev_data_formats=TestCollectionData.single.dev_data_formats,
             math_fidelities=TestCollectionData.all.math_fidelities,
         ),
     ],
     failing_rules=[
+        *TestIdsDataLoader.build_failing_rules(operators=TestCollectionData.all.operators),
         # Skip all tests with input shapes with 2 dimensions
         TestCollection(
             criteria=lambda test_vector: len(test_vector.input_shape) == 2,
             skip_reason=FailingReasons.NOT_IMPLEMENTED,
         ),
-        *TestIdsDataLoader.build_failing_rules(operators=TestCollectionData.all.operators),
+        TestCollection(
+            operators=TestCollectionData.all.operators,
+            skip_reason=FailingReasons.UNSUPPORTED_DATA_FORMAT,
+            dev_data_formats=[torch.float16],
+        ),
     ],
 )
 
