6331ad494e001b080ba1c163669d51175a5fe6bb
Ashok Kumar Kannan <160501980+ashokkumarkannan1@users.noreply.github.com>
2025-06-16 05:30:22 +0000

    it Parametrize the variant

--- forge/test/models/pytorch/vision/yolo/test_yolo_v10.py
+++ forge/test/models/pytorch/vision/yolo/test_yolo_v10.py
@@ -50,7 +50,7 @@ def test_yolov10(variant):
 
     # Load  model and input
     model, image_tensor = load_yolo_model_and_image(
-        "https://github.com/ultralytics/assets/releases/download/v8.2.0/yolov10x.pt"
+        f"https://github.com/ultralytics/assets/releases/download/v8.2.0/{variant}.pt"
     )
     framework_model = YoloWrapper(model).to(torch.bfloat16)
     image_tensor = image_tensor.to(torch.bfloat16)
