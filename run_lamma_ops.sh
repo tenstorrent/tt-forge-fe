9aefd2c655a9a6911e16f6416f13c647057bb03e
Nikola Drakulic <ndrakulic@tenstorrent.com>
2025-06-18 15:40:54 +0000

    WIP

--- run_lamma_ops.sh
+++ run_lamma_ops.sh
@@ -1,2 +1,3 @@
 python scripts/model_analysis/generate_models_ops_test.py \
-    --test_directory_or_file_path "forge/test/models/pytorch/dummy/test_mnist.py"
+    --test_directory_or_file_path "forge/test/mlir/llama/test_llama_training.py" \
+    --training_mode
