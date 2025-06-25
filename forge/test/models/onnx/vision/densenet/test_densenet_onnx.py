627366b3ef5433d8dc2175dba4a8a46d0d88ed70
Kamalraj Kannan <157608228+kamalrajkannan78@users.noreply.github.com>
2025-06-25 19:26:48 +0530

    Add Onnx support for generality models - set1 (#2375)
    ### Summary 
    
    This PR adds ONNX support for below generality models
    
    - AlexNet
    - Deit
    - DenseNet
    - Ghostnet
    - HRnet
    - Mobilenet3-ssd
    - Mobilenet3
    - Mobilenet1
    - Wideresnet
    - Xception
    
    ### Logs
    
    
    [jun23_alexnet_onnx.log](https://github.com/user-attachments/files/20866690/jun23_alexnet_onnx.log)
    
    [jun23_deit_onnx_1.log](https://github.com/user-attachments/files/20866697/jun23_deit_onnx_1.log)
    
    [jun23_densenet_onnx.log](https://github.com/user-attachments/files/20866704/jun23_densenet_onnx.log)
    
    [jun23_ghostnet_onnx.log](https://github.com/user-attachments/files/20866705/jun23_ghostnet_onnx.log)
    
    [jun23_hrnet_onnx_1.log](https://github.com/user-attachments/files/20866708/jun23_hrnet_onnx_1.log)
    
    [jun23_hrnet_onnx_2.log](https://github.com/user-attachments/files/20866711/jun23_hrnet_onnx_2.log)
    
    [jun23_mobilenet3_ssd_onnx.log](https://github.com/user-attachments/files/20866713/jun23_mob23_ssd.log)
    
    [jun23_mobilenetv3_onnx.log](https://github.com/user-attachments/files/20866865/jun23_mobv3_onnx.log)
    
    [jun23_mobilenet1_onnx.log](https://github.com/user-attachments/files/20866716/jun23_mobv1_onnx.log)
    
    [jun23_wideresnet_onnx.log](https://github.com/user-attachments/files/20866718/jun23_wideresnet_onnx.log)
    
    [jun23_xception_onnx.log](https://github.com/user-attachments/files/20866720/jun23_xception_onnx.log)
    
    ### Logs (after reflecting suggestions)
    
    
    [jun25_alexnet.log](https://github.com/user-attachments/files/20898654/jun25_alex_cc.log)
    
    [jun25_ghostnet.log](https://github.com/user-attachments/files/20898655/jun25_ghostnet.log)
    
    [jun25_hrnet_c1.log](https://github.com/user-attachments/files/20898656/jun25_hrnet_c1.log)
    
    [jun25_mobilenet.log](https://github.com/user-attachments/files/20898657/jun25_mobilenet.log)
    
    [jun25_wideresnet.log](https://github.com/user-attachments/files/20898658/jun25_wideres.log)
    
    [jun25_xception.log](https://github.com/user-attachments/files/20902048/jun25_xception.log)

---
+++ forge/test/models/onnx/vision/densenet/test_densenet_onnx.py
@@ -0,0 +1,68 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+from forge.forge_property_utils import (
+    Framework,
+    ModelArch,
+    Source,
+    Task,
+    record_model_properties,
+)
+from forge.verify.verify import verify
+
+from test.models.models_utils import print_cls_results
+from test.models.pytorch.vision.densenet.model_utils.densenet_utils import (
+    get_input_img,
+)
+from test.utils import download_model
+import onnx
+import forge
+
+variants = ["densenet121", "densenet161", "densenet169"]
+
+
+@pytest.mark.nightly
+@pytest.mark.parametrize("variant", variants)
+def test_densenet_onnx(variant, forge_tmp_path):
+
+    # Record Forge Property
+    module_name = record_model_properties(
+        framework=Framework.ONNX,
+        model=ModelArch.DENSENET,
+        variant=variant,
+        source=Source.TORCHVISION,
+        task=Task.IMAGE_CLASSIFICATION,
+    )
+
+    # Load model
+    torch_model = download_model(torch.hub.load, "pytorch/vision:v0.10.0", variant, pretrained=True)
+    torch_model.eval()
+
+    # Load and pre-process image
+    img_tensor = get_input_img()
+    inputs = [img_tensor]
+
+    # Export model to ONNX
+    onnx_path = f"{forge_tmp_path}/{variant}.onnx"
+    torch.onnx.export(torch_model, inputs[0], onnx_path, opset_version=17)
+
+    # Load framework model
+    onnx_model = onnx.load(onnx_path)
+    onnx.checker.check_model(onnx_model)
+    framework_model = forge.OnnxModule(module_name, onnx_model)
+
+    # Compile model
+    compiled_model = forge.compile(onnx_model, inputs, module_name=module_name)
+
+    # Model Verification and Inference
+    fw_out, co_out = verify(
+        inputs,
+        framework_model,
+        compiled_model,
+    )
+
+    # Post processing
+    print_cls_results(fw_out[0], co_out[0])
