c8422680955ea20fab69628cbd70121362e49075
Meenakshi Ramanathan <mramanathan@tenstorrent.com>
2025-06-25 10:53:51 +0530

    Replace cifar10 dataset with imagenet-1k dataset (#2376)
    Logs:
    
    [alexnet_pytorch.log](https://github.com/user-attachments/files/20869682/alexnet_pytorch.log)
    
    [dla_onnx.log](https://github.com/user-attachments/files/20869693/dla_onnx.log)
    
    [dla_pytorch1.log](https://github.com/user-attachments/files/20869699/dla_pytorch1.log)
    
    [effnet_onnx.log](https://github.com/user-attachments/files/20869701/effnet_onnx.log)
    
    [ghostnet.log](https://github.com/user-attachments/files/20869704/ghostnet.log)
    
    [mobilenetv2_onnx.log](https://github.com/user-attachments/files/20869709/mobilenetv2_onnx.log)

--- forge/test/models/onnx/vision/mobilenetv2/test_mobilenetv2.py
+++ forge/test/models/onnx/vision/mobilenetv2/test_mobilenetv2.py
@@ -12,7 +12,6 @@ from forge.verify.verify import verify
 from datasets import load_dataset
 from forge.verify.config import VerifyConfig, AutomaticValueChecker
 from test.models.onnx.vision.mobilenetv2.model_utils.utils import load_inputs
-from PIL import Image
 from test.models.models_utils import print_cls_results
 from forge.forge_property_utils import Framework, Source, Task, ModelArch, record_model_properties
 
@@ -41,8 +40,8 @@ def test_mobilenetv2_onnx(variant, forge_tmp_path):
     model = timm.create_model(variant, pretrained=True)
 
     # Load the inputs
-    dataset = load_dataset("cifar10", split="test[:1]")
-    img = dataset[0]["img"]
+    dataset = load_dataset("imagenet-1k", split="validation", streaming=True)
+    img = next(iter(dataset.skip(10)))["image"]
     inputs = load_inputs(img, model)
     onnx_path = f"{forge_tmp_path}/mobilenetv2.onnx"
     torch.onnx.export(model, inputs[0], onnx_path)
