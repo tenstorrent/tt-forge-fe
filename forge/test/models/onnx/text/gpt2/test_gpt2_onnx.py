03148c8eaa6200755d6f043dac5c58184cd6dc4d
kkannan <kkannan@tenstorrent.com>
2025-07-03 08:58:08 +0000

    Add onnx support for pytorch models -set3

---
+++ forge/test/models/onnx/text/gpt2/test_gpt2_onnx.py
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+import torch
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+)
+
+import forge
+from forge.forge_property_utils import (
+    Framework,
+    ModelArch,
+    Source,
+    Task,
+    record_model_properties,
+)
+from forge.verify.verify import verify
+
+from test.utils import download_model
+import onnx
+
+
+@pytest.mark.nightly
+@pytest.mark.parametrize(
+    "variant",
+    [
+        "mnoukhov/gpt2-imdb-sentiment-classifier",
+    ],
+)
+def test_gpt2_sequence_classification_onnx(variant, forge_tmp_path):
+
+    # Record Forge Property
+    module_name = record_model_properties(
+        framework=Framework.ONNX,
+        model=ModelArch.GPT,
+        variant=variant,
+        task=Task.SEQUENCE_CLASSIFICATION,
+        source=Source.HUGGINGFACE,
+    )
+
+    # Load tokenizer and model from HuggingFace
+    tokenizer = download_model(AutoTokenizer.from_pretrained, variant, padding_side="left")
+    torch_model = download_model(
+        AutoModelForSequenceClassification.from_pretrained, variant, return_dict=False, use_cache=False
+    )
+    torch_model.eval()
+
+    # Prepare input
+    test_input = "This is a sample text from "
+    input_tokens = tokenizer(test_input, return_tensors="pt")
+    inputs = [input_tokens["input_ids"]]
+
+    # Export model to ONNX
+    onnx_path = f"{forge_tmp_path}/" + str(variant).split("/")[-1].replace("-", "_") + ".onnx"
+    torch.onnx.export(torch_model, inputs[0], onnx_path, opset_version=17)
+
+    # Load framework model
+    onnx_model = onnx.load(onnx_path)
+    onnx.checker.check_model(onnx_model)
+    framework_model = forge.OnnxModule(module_name, onnx_model)
+
+    # Compile model
+    compiled_model = forge.compile(onnx_model, inputs, module_name=module_name)
+
+    # Model Verification and Inference
+    _, co_out = verify(
+        inputs,
+        framework_model,
+        compiled_model,
+    )
+
+    # post processing
+    predicted_value = co_out[0].argmax(-1).item()
+    print(f"Predicted Sentiment: {torch_model.config.id2label[predicted_value]}")
