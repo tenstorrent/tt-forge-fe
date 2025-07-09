460f310dc007d3206f74d8a00b6437518d4d117a
kkannan <kkannan@tenstorrent.com>
2025-07-09 08:19:49 +0000

    seg fault in bart

--- forge/test/models/pytorch/text/bart/test_bart.py
+++ forge/test/models/pytorch/text/bart/test_bart.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # BART Demo Script - SQuADv1.1 QA
 import pytest
-import torch
 from transformers import BartForSequenceClassification, BartTokenizer
 from transformers.models.bart.modeling_bart import shift_tokens_right
 
@@ -20,16 +19,6 @@ from forge.verify.value_checkers import AutomaticValueChecker
 from forge.verify.verify import verify
 
 from test.utils import download_model
-
-
-class BartWrapper(torch.nn.Module):
-    def __init__(self, model):
-        super().__init__()
-        self.model = model
-
-    def forward(self, input_ids, attention_mask, decoder_input_ids):
-        out = self.model(input_ids, attention_mask, decoder_input_ids)[0]
-        return out
 
 
 @pytest.mark.nightly
@@ -51,7 +40,9 @@ def test_pt_bart_classifier(variant):
         source=Source.HUGGINGFACE,
     )
 
-    model = download_model(BartForSequenceClassification.from_pretrained, variant, torchscript=True)
+    model = download_model(
+        BartForSequenceClassification.from_pretrained, variant, torchscript=True, use_cache=False, return_dict=False
+    )
     tokenizer = download_model(BartTokenizer.from_pretrained, variant, pad_to_max_length=True)
     hypothesis = "Most of Mrinal Sen's work can be found in European collections."
     premise = "Calcutta seems to be the only other production center having any pretensions to artistic creativity at all, but ironically you're actually more likely to see the works of Satyajit Ray or Mrinal Sen shown in Europe or North America than in India itself."
@@ -71,13 +62,25 @@ def test_pt_bart_classifier(variant):
     )
     inputs = [inputs_dict["input_ids"], inputs_dict["attention_mask"], decoder_input_ids]
 
-    # Compile & feed data
-    framework_model = BartWrapper(model.model)
-
     # Forge compile framework model
-    compiled_model = forge.compile(framework_model, sample_inputs=inputs, module_name=module_name)
+    compiled_model = forge.compile(model, sample_inputs=inputs, module_name=module_name)
 
     # Model Verification
-    verify(
-        inputs, framework_model, compiled_model, verify_cfg=VerifyConfig(value_checker=AutomaticValueChecker(pcc=0.95))
+    _, co_out = verify(
+        inputs, model, compiled_model, verify_cfg=VerifyConfig(value_checker=AutomaticValueChecker(pcc=0.95))
     )
+
+    # co_out = model(*inputs)
+
+    # logits = co_out[0]
+
+    # # Select entailment and contradiction logits
+    # entail_contradiction_logits = logits[:, [0, 2]]  # [contradiction, entailment]
+
+    # # Convert to probabilities
+    # probs = entail_contradiction_logits.softmax(dim=1)
+
+    # # Probability that the label (hypothesis) is entailed by the premise
+    # prob_label_is_true = probs[:, 1].item()
+
+    # print(f"\n Entailment probability for hypothesis: {prob_label_is_true:.4f}\n")
