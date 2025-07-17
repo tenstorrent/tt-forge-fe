955e8b51899701ea78a304cbbcaa4043f09bf1c6
acolic <acolic@tenstorrent.com>
2025-07-17 10:23:38 +0200

    Created all op files.

---
+++ forge/csrc/ops/op_log.cpp
@@ -0,0 +1,91 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <vector>
+
+#include "autograd/autograd.hpp"
+#include "graph_lib/node_types.hpp"
+#include "graph_lib/shape.hpp"
+#include "op.hpp"
+#include "op_interface.hpp"
+#include "passes/decomposing_context.hpp"
+#include "torch/extension.h"  // Needed for c++ to/from python type conversion.
+#include "torch/torch.h"
+#include "utils/assert.hpp"
+
+namespace tt
+{
+namespace ops
+{
+namespace log
+{
+using namespace graphlib;
+
+at::Tensor eval(const Op &op, const std::vector<at::Tensor> &tensors)
+{
+    TT_DBG_ASSERT(op.type() == OpType::Log, "Wrong op type.");
+
+    // Implementation.
+
+    return {};
+}
+
+std::tuple<Shape, std::vector<DimBroadcast>> shape(
+    const Op &op, const std::vector<std::vector<std::uint32_t>> &in_shapes)
+{
+    TT_DBG_ASSERT(op.type() == OpType::Log, "Wrong op type.");
+
+    // Implementation.
+
+    return {};
+}
+
+NodeContext backward(
+    const Op &op,
+    autograd::autograd_context &ac,
+    int operand,
+    const std::vector<NodeContext> &inputs,
+    const NodeContext &output,
+    const NodeContext &gradient)
+{
+    TT_DBG_ASSERT(op.type() == OpType::Log, "Wrong op type.");
+
+    // Implementation.
+
+    return {gradient};
+}
+
+void decompose_initial(const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
+{
+    TT_DBG_ASSERT(op.type() == OpType::Log, "Wrong op type.");
+
+    // Implementation.
+}
+
+void decompose_post_optimize(const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
+{
+    TT_DBG_ASSERT(op.type() == OpType::Log, "Wrong op type.");
+
+    // Implementation.
+}
+
+void decompose_post_autograd(const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
+{
+    TT_DBG_ASSERT(op.type() == OpType::Log, "Wrong op type.");
+
+    // Implementation.
+}
+
+long initial_flops_estimate(const Op &op, const std::vector<std::vector<std::uint32_t>> &inputs)
+{
+    TT_DBG_ASSERT(op.type() == OpType::Log, "Wrong op type.");
+
+    // Implementation.
+
+    return 0;
+}
+
+}  // namespace log
+}  // namespace ops
+}  // namespace tt
