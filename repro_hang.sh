efbabb85d7d29744afcddf9f18ad3cb6957f29f0
Predrag Ilkic <pilkic@tenstorrent.com>
2025-07-17 12:54:09 +0200

    adding repro script

---
+++ repro_hang.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+# This script runs the performance benchmark which causes a hang.
+# If `--workaround` is specified, it will run the benchmark with the workaround env variable `TT_METAL_FORCE_REINIT=1`, which should prevent the hang.
+
+if [ "$1" == "--workaround" ]; then
+    echo "Setting workaround environment variable TT_METAL_FORCE_REINIT=1"
+    export TT_METAL_FORCE_REINIT=1
+fi
+
+if [ $TT_METAL_HOME ]; then
+    # `tt-forge-fe` resolves appropriate `TT_METAL_HOME` value automatically. If you have it set, please make sure that it is correct and intentional.
+    # In that case remove this check.
+    echo "TT_METAL_HOME is set to $TT_METAL_HOME"
+    # raise exception since this is not expected
+    echo "Please make sure that TT_METAL_HOME is set correctly and intentionally - if not, please unset it."
+    echo "Exiting..."
+    exit 1
+fi
+
+
+python forge/test/benchmark/benchmark.py -m resnet50_hf -ts na -bs 8 -df bfloat16 -lp 32 -o forge-benchmark-e2e-resnet50_hf.json
