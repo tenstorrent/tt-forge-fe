14b1c6433b999bef4ca088c395726c9339e64cb9
acolic <acolic@tenstorrent.com>
2025-07-23 12:49:06 +0200

    Implemented all eltwise operations in cpp.
    This change is part of ops migration from python to cpp.
    For details check out: https://github.com/tenstorrent/tt-forge-fe/issues/2378

--- forge/csrc/ops/op_greater.cpp
+++ forge/csrc/ops/op_greater.cpp
@@ -8,8 +8,8 @@
 #include "graph_lib/node_types.hpp"
 #include "graph_lib/shape.hpp"
 #include "op.hpp"
+#include "op_common.hpp"
 #include "op_interface.hpp"
-#include "passes/decomposing_context.hpp"
 #include "torch/extension.h"  // Needed for c++ to/from python type conversion.
 #include "torch/torch.h"
 #include "utils/assert.hpp"
@@ -24,15 +24,18 @@ using namespace graphlib;
 
 at::Tensor eval(const graphlib::OpType &old_op_type, const Op &op, const std::vector<at::Tensor> &tensors)
 {
-    TT_DBG_ASSERT(op.type() == OpType::Greater, "Wrong op type.");
-    return op.base_eval(old_op_type, tensors);
+    TT_ASSERT(tensors.size() == 2, "Greater should have two input tensors.");
+    return torch::gt(tensors[0], tensors[1]).to(tensors[0].dtype());
 }
 
 std::tuple<Shape, std::vector<DimBroadcast>> shape(
     const graphlib::OpType &old_op_type, const Op &op, const std::vector<std::vector<std::uint32_t>> &in_shapes)
 {
     TT_DBG_ASSERT(op.type() == OpType::Greater, "Wrong op type.");
-    return op.base_shape(old_op_type, in_shapes);
+    TT_ASSERT(in_shapes.size() == 2, "Greater should have two input shapes.");
+    TT_ASSERT(op.attrs().size() == 0, "Greater should not have any attrs.");
+
+    return op_common::compute_elementwise_binary_shape(in_shapes);
 }
 
 NodeContext backward(
@@ -44,36 +47,8 @@ NodeContext backward(
     const NodeContext &output,
     const NodeContext &gradient)
 {
-    TT_DBG_ASSERT(op.type() == OpType::Greater, "Wrong op type.");
-    return op.base_backward(old_op_type, ac, operand, inputs, output, gradient);
-}
-
-void decompose_initial(
-    const graphlib::OpType &old_op_type, const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::Greater, "Wrong op type.");
-    return op.base_decompose(old_op_type, "get_f_forge_decompose", dc, inputs);
-}
-
-void decompose_post_optimize(
-    const graphlib::OpType &old_op_type, const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::Greater, "Wrong op type.");
-    return op.base_decompose(old_op_type, "get_f_forge_decompose_post_optimize", dc, inputs);
-}
-
-void decompose_post_autograd(
-    const graphlib::OpType &old_op_type, const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::Greater, "Wrong op type.");
-    return op.base_decompose(old_op_type, "get_f_forge_decompose_post_autograd", dc, inputs);
-}
-
-long initial_flops_estimate(
-    const graphlib::OpType &old_op_type, const Op &op, const std::vector<std::vector<std::uint32_t>> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::Greater, "Wrong op type.");
-    return op.base_initial_flops_estimate(old_op_type, inputs);
+    TT_ASSERT(false, "Greater does not have backward.");
+    unreachable();
 }
 
 }  // namespace greater
