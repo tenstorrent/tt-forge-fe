10d142c43e49ed6d3c6e6680273faf0b5cd78a19
Kamalraj Kannan <157608228+kamalrajkannan78@users.noreply.github.com>
2025-07-23 15:09:51 +0530

    Add onnx support for generality pytorch models -set3 (#2440)
    ## Summary 
    
    This PR adds ONNX support for below generality pytorch models 
    
    - Bart
    - Bloom
    - GPT-2
    - Perceiver
    - Roberta
    - Squeezebert
    - Beit
    - Centernet
    - Monodle
    - Resnext
    
    ## Logs
    
    [jul22_bart_onnx.log](https://github.com/user-attachments/files/21372125/jul22_bart_onnx.log)
    
    [jul22_beit.log](https://github.com/user-attachments/files/21372127/jul22_beit.log)
    
    [jul22_bloom_onnx.log](https://github.com/user-attachments/files/21372145/jul22_bloom_onnx.log)
    
    [jul22_centernet_onnx_1.log](https://github.com/user-attachments/files/21372161/jul22_centernet_onnx_1.log)
    
    [jul22_gpt2_onnx.log](https://github.com/user-attachments/files/21372164/jul22_gpt2_onnx.log)
    
    [jul22_perciever_onnx.log](https://github.com/user-attachments/files/21372171/jul22_perciever_onnx.log)
    
    [jul22_resnext_onnx.log](https://github.com/user-attachments/files/21372192/jul22_resnext_onnx.log)
    
    [jul22_roberta_onnx.log](https://github.com/user-attachments/files/21372197/jul22_roberta_onnx.log)
    
    [jul22_sqbert_onnx.log](https://github.com/user-attachments/files/21372199/jul22_sqbert_onnx.log)
    
    [jul22_monodle_onnx.log](https://github.com/user-attachments/files/21372237/jul22_monodle_onnx.log)

---
+++ forge/test/models/onnx/text/squeezebert/test_squeezebert_onnx.py
@@ -0,0 +1,60 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+from third_party.tt_forge_models.squeezebert import ModelLoader
+
+import forge
+from forge.forge_property_utils import (
+    Framework,
+    ModelArch,
+    Source,
+    Task,
+    record_model_properties,
+)
+from forge.verify.verify import verify
+import torch
+import onnx
+
+
+@pytest.mark.nightly
+@pytest.mark.parametrize("variant", ["squeezebert/squeezebert-mnli"])
+def test_squeezebert_sequence_classification_onnx(variant, forge_tmp_path):
+    # Record Forge Property
+    module_name = record_model_properties(
+        framework=Framework.ONNX,
+        model=ModelArch.SQUEEZEBERT,
+        variant=variant,
+        task=Task.SEQUENCE_CLASSIFICATION,
+        source=Source.HUGGINGFACE,
+    )
+
+    # Load model and input
+    loader = ModelLoader()
+    torch_model = loader.load_model()
+    input_tokens = loader.load_inputs()
+    inputs = [input_tokens]
+
+    # Export model to ONNX
+    onnx_path = f"{forge_tmp_path}/" + str(variant).split("/")[-1].replace("-", "_") + ".onnx"
+    torch.onnx.export(torch_model, inputs[0], onnx_path, opset_version=17)
+
+    # Load framework model
+    onnx_model = onnx.load(onnx_path)
+    onnx.checker.check_model(onnx_model)
+    framework_model = forge.OnnxModule(module_name, onnx_model)
+
+    # Compile model
+    compiled_model = forge.compile(onnx_model, inputs, module_name=module_name)
+
+    # Model Verification and Inference
+    _, co_out = verify(
+        inputs,
+        framework_model,
+        compiled_model,
+    )
+
+    # post processing
+    predicted_class_id = co_out[0].argmax().item()
+    predicted_category = torch_model.config.id2label[predicted_class_id]
+    print(f"predicted category: {predicted_category}")
