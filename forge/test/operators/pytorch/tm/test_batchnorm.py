2e4bb327c994a450ae1aaacef37f08b4cac2c189
Vladica Obojevic <vobojevic@tenstorrent.com>
2025-07-28 13:05:02 +0000

    Add BatchNorm1d and BatchNorm3d tests

--- forge/test/operators/pytorch/tm/test_batchnorm.py
+++ forge/test/operators/pytorch/tm/test_batchnorm.py
@@ -2,9 +2,9 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-import forge
 import torch
-import pytest
+import random
+import math
 
 from typing import List, Dict
 from loguru import logger
@@ -13,8 +13,7 @@ from forge._C import DataFormat
 
 from forge.config import CompilerConfig
 from forge.verify.config import VerifyConfig
-from forge.verify.value_checkers import AllCloseValueChecker, AutomaticValueChecker
-from forge.verify.verify import verify as forge_verify
+from forge.verify.value_checkers import AllCloseValueChecker
 
 from test.operators.utils import VerifyUtils
 from test.operators.utils import InputSource
@@ -30,10 +29,11 @@ from test.operators.utils.test_data import TestCollectionTorch
 
 
 class ModelFromAnotherOp(torch.nn.Module):
-    def __init__(self, operator, kwargs):
+    def __init__(self, operator, opname, kwargs):
         super().__init__()
-        self.testname = "BatchNorm2d_operator_test_op_src_from_another_op"
+        self.testname = f"{opname}_operator_test_op_src_from_another_op"
         self.operator = operator
+        self.opname = opname
         self.batchnorm2d = self.operator(**kwargs)
 
     def forward(self, x):
@@ -42,10 +42,11 @@ class ModelFromAnotherOp(torch.nn.Module):
 
 
 class ModelDirect(torch.nn.Module):
-    def __init__(self, operator, kwargs):
+    def __init__(self, operator, opname, kwargs):
         super().__init__()
-        self.testname = "BatchNorm2d_operator_test_op_src_direct"
+        self.testname = f"{opname}_operator_test_op_src_direct"
         self.operator = operator
+        self.opname = opname
         self.batchnorm2d = self.operator(**kwargs)
 
     def forward(self, x):
@@ -53,10 +54,11 @@ class ModelDirect(torch.nn.Module):
 
 
 class ModelConstEvalPass(torch.nn.Module):
-    def __init__(self, operator, input_shape, kwargs, value_range):
+    def __init__(self, operator, opname, input_shape, kwargs, value_range):
         super().__init__()
-        self.testname = "BatchNorm2d_operator_test_op_src_const_eval_pass"
+        self.testname = f"{opname}_operator_test_op_src_const_eval_pass"
         self.operator = operator
+        self.opname = opname
         self.batchnorm2d = self.operator(**kwargs)
         self.dtype = kwargs.get("dtype")
         self.const = TensorUtils.create_torch_constant(
@@ -97,6 +99,7 @@ class TestVerification:
         if test_vector.input_source == InputSource.CONST_EVAL_PASS:
             pytorch_model = model_type(
                 operator=operator,
+                opname=test_vector.operator,
                 input_shape=test_vector.input_shape,
                 kwargs=kwargs,
                 value_range=value_range,
@@ -104,6 +107,7 @@ class TestVerification:
         else:
             pytorch_model = model_type(
                 operator=operator,
+                opname=test_vector.operator,
                 kwargs=kwargs,
             )
 
@@ -137,7 +141,9 @@ class TestParamsData:
 
     __test__ = False
 
-    test_plan: TestPlan = None
+    test_plan_1d: TestPlan = None
+    test_plan_2d: TestPlan = None
+    test_plan_2d: TestPlan = None
 
     @classmethod
     def generate_kwargs(cls, test_vector: TestVector):
@@ -159,15 +165,31 @@ class TestParamsData:
 
 
 class TestCollectionData:
-
     __test__ = False
 
-    operators = ["batch_norm_2d"]
+    operator_1d = ["batch_norm_1d"]
+    operator_2d = ["batch_norm_2d"]
+    operator_3d = ["batch_norm_3d"]
 
-    input_shapes = [shape for shape in TestCollectionCommon.all.input_shapes if len(shape) == 4]
+    input_shapes_1d = [s for s in TestCollectionCommon.all.input_shapes if len(s) in [2, 3]]
+    input_shapes_2d = [s for s in TestCollectionCommon.all.input_shapes if len(s) == 4]
+
+    @staticmethod
+    def _input_shapes_3d(shapes):
+        ret = []
+        for s in shapes:
+            rnd = random.Random(math.prod(s))
+            ret.append(s[:2] + (rnd.randint(1, 5),) + s[2:])
+        return ret
+
+    input_shapes_3d = _input_shapes_3d(input_shapes_2d)
+
+    single_input_shape_1d = [(32, 64, 64)]
+    single_input_shape_2d = [(10, 32, 64, 64)]
+    single_input_shape_3d = [(10, 32, 5, 64, 64)]
 
 
-TestParamsData.test_plan = TestPlan(
+TestParamsData.test_plan_1d = TestPlan(
     verify=lambda test_device, test_vector: TestVerification.verify(
         test_device,
         test_vector,
@@ -175,16 +197,16 @@ TestParamsData.test_plan = TestPlan(
     collections=[
         # Test operators with all shapes and input sources collection
         TestCollection(
-            operators=TestCollectionData.operators,
+            operators=TestCollectionData.operator_1d,
             input_sources=TestCollectionCommon.all.input_sources,
-            input_shapes=TestCollectionData.input_shapes,
+            input_shapes=TestCollectionData.input_shapes_1d,
             kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
         ),
         # Test Data formats collection
         TestCollection(
-            operators=TestCollectionData.operators,
+            operators=TestCollectionData.operator_1d,
             input_sources=TestCollectionCommon.single.input_sources,
-            input_shapes=TestCollectionCommon.single.input_shapes,
+            input_shapes=TestCollectionData.single_input_shape_1d,
             dev_data_formats=[
                 item
                 for item in TestCollectionTorch.float.dev_data_formats
@@ -195,25 +217,25 @@ TestParamsData.test_plan = TestPlan(
         ),
         # Test Math fidelities collection
         TestCollection(
-            operators=TestCollectionData.operators,
+            operators=TestCollectionData.operator_1d,
             input_sources=TestCollectionCommon.single.input_sources,
-            input_shapes=TestCollectionCommon.single.input_shapes,
+            input_shapes=TestCollectionData.single_input_shape_1d,
             dev_data_formats=TestCollectionTorch.single.dev_data_formats,
             math_fidelities=TestCollectionCommon.all.math_fidelities,
             kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
         ),
         # Test eps and momentum collection
         TestCollection(
-            operators=TestCollectionData.operators,
+            operators=TestCollectionData.operator_1d,
             input_sources=TestCollectionCommon.all.input_sources,
-            input_shapes=[(10, 32, 64, 64)],
+            input_shapes=TestCollectionData.single_input_shape_1d,
             kwargs=lambda test_vector: TestParamsData.generate_kwargs_eps_momentum(test_vector),
         ),
         # Test affine = False collection
         TestCollection(
-            operators=TestCollectionData.operators,
+            operators=TestCollectionData.operator_1d,
             input_sources=TestCollectionCommon.single.input_sources,
-            input_shapes=TestCollectionCommon.single.input_shapes,
+            input_shapes=TestCollectionData.single_input_shape_1d,
             kwargs=lambda test_vector: (
                 kwargs
                 for kwargs in [
@@ -226,9 +248,9 @@ TestParamsData.test_plan = TestPlan(
         ),
         # Test track_running_stats = False collection
         TestCollection(
-            operators=TestCollectionData.operators,
+            operators=TestCollectionData.operator_1d,
             input_sources=TestCollectionCommon.single.input_sources,
-            input_shapes=TestCollectionCommon.single.input_shapes,
+            input_shapes=TestCollectionData.single_input_shape_1d,
             kwargs=lambda test_vector: (
                 kwargs
                 for kwargs in [
@@ -241,12 +263,172 @@ TestParamsData.test_plan = TestPlan(
         ),
     ],
     failing_rules=[
-        *TestIdsDataLoader.build_failing_rules(operators=TestCollectionData.operators),
+        *TestIdsDataLoader.build_failing_rules(operators=TestCollectionData.operator_1d),
+    ],
+)
+
+
+TestParamsData.test_plan_2d = TestPlan(
+    verify=lambda test_device, test_vector: TestVerification.verify(
+        test_device,
+        test_vector,
+    ),
+    collections=[
+        # Test operators with all shapes and input sources collection
+        TestCollection(
+            operators=TestCollectionData.operator_2d,
+            input_sources=TestCollectionCommon.all.input_sources,
+            input_shapes=TestCollectionData.input_shapes_2d,
+            kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
+        ),
+        # Test Data formats collection
+        TestCollection(
+            operators=TestCollectionData.operator_2d,
+            input_sources=TestCollectionCommon.single.input_sources,
+            input_shapes=TestCollectionData.single_input_shape_2d,
+            dev_data_formats=[
+                item
+                for item in TestCollectionTorch.float.dev_data_formats
+                if item not in TestCollectionTorch.single.dev_data_formats
+            ],
+            math_fidelities=TestCollectionCommon.single.math_fidelities,
+            kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
+        ),
+        # Test Math fidelities collection
+        TestCollection(
+            operators=TestCollectionData.operator_2d,
+            input_sources=TestCollectionCommon.single.input_sources,
+            input_shapes=TestCollectionData.single_input_shape_2d,
+            dev_data_formats=TestCollectionTorch.single.dev_data_formats,
+            math_fidelities=TestCollectionCommon.all.math_fidelities,
+            kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
+        ),
+        # Test eps and momentum collection
+        TestCollection(
+            operators=TestCollectionData.operator_2d,
+            input_sources=TestCollectionCommon.all.input_sources,
+            input_shapes=TestCollectionData.single_input_shape_2d,
+            kwargs=lambda test_vector: TestParamsData.generate_kwargs_eps_momentum(test_vector),
+        ),
+        # Test affine = False collection
+        TestCollection(
+            operators=TestCollectionData.operator_2d,
+            input_sources=TestCollectionCommon.single.input_sources,
+            input_shapes=TestCollectionData.single_input_shape_2d,
+            kwargs=lambda test_vector: (
+                kwargs
+                for kwargs in [
+                    {
+                        "num_features": test_vector.input_shape[1],
+                        "affine": False,
+                    }
+                ]
+            ),
+        ),
+        # Test track_running_stats = False collection
+        TestCollection(
+            operators=TestCollectionData.operator_2d,
+            input_sources=TestCollectionCommon.single.input_sources,
+            input_shapes=TestCollectionData.single_input_shape_2d,
+            kwargs=lambda test_vector: (
+                kwargs
+                for kwargs in [
+                    {
+                        "num_features": test_vector.input_shape[1],
+                        "track_running_stats": False,
+                    }
+                ]
+            ),
+        ),
+    ],
+    failing_rules=[
+        *TestIdsDataLoader.build_failing_rules(operators=TestCollectionData.operator_2d),
+    ],
+)
+
+
+TestParamsData.test_plan_3d = TestPlan(
+    verify=lambda test_device, test_vector: TestVerification.verify(
+        test_device,
+        test_vector,
+    ),
+    collections=[
+        # Test operators with all shapes and input sources collection
+        TestCollection(
+            operators=TestCollectionData.operator_3d,
+            input_sources=TestCollectionCommon.all.input_sources,
+            input_shapes=TestCollectionData.input_shapes_3d,
+            kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
+        ),
+        # Test Data formats collection
+        TestCollection(
+            operators=TestCollectionData.operator_3d,
+            input_sources=TestCollectionCommon.single.input_sources,
+            input_shapes=TestCollectionData.single_input_shape_3d,
+            dev_data_formats=[
+                item
+                for item in TestCollectionTorch.float.dev_data_formats
+                if item not in TestCollectionTorch.single.dev_data_formats
+            ],
+            math_fidelities=TestCollectionCommon.single.math_fidelities,
+            kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
+        ),
+        # Test Math fidelities collection
+        TestCollection(
+            operators=TestCollectionData.operator_3d,
+            input_sources=TestCollectionCommon.single.input_sources,
+            input_shapes=TestCollectionData.single_input_shape_3d,
+            dev_data_formats=TestCollectionTorch.single.dev_data_formats,
+            math_fidelities=TestCollectionCommon.all.math_fidelities,
+            kwargs=lambda test_vector: TestParamsData.generate_kwargs(test_vector),
+        ),
+        # Test eps and momentum collection
+        TestCollection(
+            operators=TestCollectionData.operator_3d,
+            input_sources=TestCollectionCommon.all.input_sources,
+            input_shapes=TestCollectionData.single_input_shape_3d,
+            kwargs=lambda test_vector: TestParamsData.generate_kwargs_eps_momentum(test_vector),
+        ),
+        # Test affine = False collection
+        TestCollection(
+            operators=TestCollectionData.operator_3d,
+            input_sources=TestCollectionCommon.single.input_sources,
+            input_shapes=TestCollectionData.single_input_shape_3d,
+            kwargs=lambda test_vector: (
+                kwargs
+                for kwargs in [
+                    {
+                        "num_features": test_vector.input_shape[1],
+                        "affine": False,
+                    }
+                ]
+            ),
+        ),
+        # Test track_running_stats = False collection
+        TestCollection(
+            operators=TestCollectionData.operator_3d,
+            input_sources=TestCollectionCommon.single.input_sources,
+            input_shapes=TestCollectionData.single_input_shape_3d,
+            kwargs=lambda test_vector: (
+                kwargs
+                for kwargs in [
+                    {
+                        "num_features": test_vector.input_shape[1],
+                        "track_running_stats": False,
+                    }
+                ]
+            ),
+        ),
+    ],
+    failing_rules=[
+        *TestIdsDataLoader.build_failing_rules(operators=TestCollectionData.operator_3d),
     ],
 )
 
 
 def get_test_plans() -> List[TestPlan]:
     return [
-        TestParamsData.test_plan,
+        TestParamsData.test_plan_1d,
+        TestParamsData.test_plan_2d,
+        TestParamsData.test_plan_3d,
     ]
