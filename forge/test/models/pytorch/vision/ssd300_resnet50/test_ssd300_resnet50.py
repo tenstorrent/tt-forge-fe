6b44de03eb9690c106a5dcbdb9cd10345c29dc60
kkannan <kkannan@tenstorrent.com>
2025-07-28 08:35:53 +0000

    Track changes of Bringup tt-forge-fe models & link FFE tests - set3

--- forge/test/models/pytorch/vision/ssd300_resnet50/test_ssd300_resnet50.py
+++ forge/test/models/pytorch/vision/ssd300_resnet50/test_ssd300_resnet50.py
@@ -1,11 +1,12 @@
 # SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 
 # SPDX-License-Identifier: Apache-2.0
-import numpy as np
 import pytest
-import requests
 import torch
-from third_party.tt_forge_models.tools.utils import get_file
+from third_party.tt_forge_models.ssd300_resnet50.pytorch import (
+    ModelLoader,
+    ModelVariant,
+)
 
 import forge
 from forge._C import DataFormat
@@ -19,10 +20,6 @@ from forge.forge_property_utils import (
 )
 from forge.verify.verify import verify
 
-from test.models.pytorch.vision.ssd300_resnet50.model_utils.image_utils import (
-    prepare_input,
-)
-
 
 @pytest.mark.nightly
 @pytest.mark.xfail
@@ -35,27 +32,11 @@ def test_pytorch_ssd300_resnet50():
         task=Task.IMAGE_CLASSIFICATION,
     )
 
-    # STEP 2 : prepare model
-    framework_model = torch.hub.load("NVIDIA/DeepLearningExamples:torchhub", "nvidia_ssd", pretrained=False)
-    url = "https://api.ngc.nvidia.com/v2/models/nvidia/ssd_pyt_ckpt_amp/versions/19.09.0/files/nvidia_ssdpyt_fp16_190826.pt"
-    checkpoint_path = "nvidia_ssdpyt_fp16_190826.pt"
-
-    response = requests.get(url)
-    with open(checkpoint_path, "wb") as f:
-        f.write(response.content)
-
-    checkpoint = torch.load(checkpoint_path, map_location=torch.device("cpu"))
-    framework_model.load_state_dict(checkpoint["model"])
-    framework_model.to(torch.bfloat16)
-    framework_model.eval()
-
-    # STEP 3 : prepare input
-    input_image = get_file("http://images.cocodataset.org/val2017/000000397133.jpg")
-    HWC = prepare_input(input_image)
-    CHW = np.swapaxes(np.swapaxes(HWC, 0, 2), 1, 2)
-    batch = np.expand_dims(CHW, axis=0)
-    input_batch = torch.from_numpy(batch).float()
-    inputs = [input_batch.to(torch.bfloat16)]
+    # Load model and inputs
+    loader = ModelLoader(variant=ModelVariant.BASE)
+    framework_model = loader.load_model(dtype_override=torch.bfloat16)
+    input_tensor = loader.load_inputs(dtype_override=torch.bfloat16)
+    inputs = [input_tensor]
 
     data_format_override = DataFormat.Float16_b
     compiler_cfg = CompilerConfig(default_df_override=data_format_override)
