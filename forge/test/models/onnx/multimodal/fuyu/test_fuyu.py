2d64c333e1df5447d1cf4e892757dbdb490ec128
Ashok Kumar Kannan <160501980+ashokkumarkannan1@users.noreply.github.com>
2025-07-29 12:39:48 +0530

    Update test_fuyu.py

--- forge/test/models/onnx/multimodal/fuyu/test_fuyu.py
+++ forge/test/models/onnx/multimodal/fuyu/test_fuyu.py
@@ -1,17 +1,15 @@
 # SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
+
 import pytest
-from transformers import FuyuProcessor, FuyuForCausalLM, FuyuConfig
 import torch
 import onnx
-import forge
 
-from test.models.models_utils import pad_inputs
-from test.models.onnx.text.fuyu.model_utils.model_utils import generate_no_cache
 from forge.verify.verify import verify
 from forge.forge_property_utils import Framework, Source, Task, ModelArch, record_model_properties
-from datasets import load_dataset
+from third_party.tt_forge_models.fuyu.pytorch.loader import ModelLoader, ModelVariant
+import forge
 
 
 @pytest.mark.out_of_memory
@@ -19,9 +17,7 @@ from datasets import load_dataset
 @pytest.mark.parametrize(
     "variant",
     [
-        pytest.param(
-            "adept/fuyu-8b",
-        ),
+        pytest.param(ModelVariant.FUYU_8B),
     ],
 )
 def test_fuyu_onnx(variant, forge_tmp_path):
@@ -29,67 +25,33 @@ def test_fuyu_onnx(variant, forge_tmp_path):
     module_name = record_model_properties(
         framework=Framework.ONNX,
         model=ModelArch.FUYU,
-        variant=variant,
+        variant=variant.value,
         task=Task.CAUSAL_LM,
         source=Source.HUGGINGFACE,
     )
 
     pytest.xfail(reason="Requires multi-chip support")
 
-    # Load model and tokenizer
-    config = FuyuConfig.from_pretrained(variant)
-    config_dict = config.to_dict()
-    config_dict["return_dict"] = False
-    config_dict["use_cache"] = False
-    config = FuyuConfig(**config_dict)
-
-    framework_model = FuyuForCausalLM.from_pretrained(variant, device_map="cpu", config=config)
-    framework_model.eval()
-    processor = FuyuProcessor.from_pretrained(variant, use_cache=False)
-
-    # Prepare input
-    prompt = "Generate a coco-style caption.\n"
-    dataset = load_dataset("imagenet-1k", split="validation", streaming=True)
-    image = next(iter(dataset.skip(10)))["image"]
-
-    model_inputs = processor(images=image, text=prompt, return_tensors="pt")
-    input_ids = model_inputs["input_ids"]
-    attention_mask = model_inputs["attention_mask"]
-    image_patches = model_inputs["image_patches"]
-    image_patches_indices = model_inputs["image_patches_indices"]
-
-    # Pad input_ids and attention_mask
-    padded_input_ids, seq_len = pad_inputs(input_ids, 512)
-    padded_attention_mask, _ = pad_inputs(attention_mask, 512)
-
-    # Updated model inputs
-    model_inputs["input_ids"] = padded_input_ids
-    model_inputs["attention_mask"] = padded_attention_mask
-
-    inputs = [input_ids, image_patches, image_patches_indices, attention_mask]
+    # Load model and inputs using ModelLoader
+    loader = ModelLoader(variant)
+    framework_model = loader.load_model()
+    model_inputs = loader.load_inputs()
 
     # Export model to ONNX
     onnx_path = f"{forge_tmp_path}/model.onnx"
     torch.onnx.export(
         framework_model,
-        (inputs[0], inputs[1], inputs[2], inputs[3]),
+        (model_inputs),
         onnx_path,
         opset_version=17,
-        input_names=["input_ids", "image_patches", "image_patches_indices", "attention_mask"],
+        input_names=["input_embeds"],
         output_names=["output"],
     )
 
-    # Load framework model
+    # Load and compile ONNX model
     onnx_model = onnx.load(onnx_path)
     framework_model = forge.OnnxModule(module_name, onnx_model, onnx_path)
+    compiled_model = forge.compile(framework_model, [model_inputs], module_name=module_name)
 
-    # Compile model
-    compiled_model = forge.compile(framework_model, inputs, module_name=module_name)
-
-    # Model Verification
-    verify(inputs, framework_model, compiled_model)
-
-    generated_text = generate_no_cache(
-        max_new_tokens=512, model=compiled_model, inputs=model_inputs, seq_len=seq_len, tokenizer=processor.tokenizer
-    )
-    print("Generated:", generated_text)
+    # Model verification
+    verify([model_inputs], framework_model, compiled_model)
