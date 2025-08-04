c57d3eb9f89a2b0e719cb39e44f2f1e4ff2b12e7
mstojkovicTT <mstojkovic@tenstorrent.com>
2025-08-04 14:14:36 +0000

    initial

--- forge/csrc/ops/op_embedding.cpp
+++ forge/csrc/ops/op_embedding.cpp
@@ -25,14 +25,21 @@ using namespace graphlib;
 at::Tensor eval(const graphlib::OpType &old_op_type, const Op &op, const std::vector<at::Tensor> &tensors)
 {
     TT_DBG_ASSERT(op.type() == OpType::Embedding, "Wrong op type.");
-    return op.base_eval(old_op_type, tensors);
+    TT_ASSERT(tensors.size() == 2, "Embedding should have exactly 2 input tensors.");
+
+    return torch::embedding(tensors[1], tensors[0].to(torch::kInt32));
 }
 
 std::tuple<Shape, std::vector<DimBroadcast>> shape(
     const graphlib::OpType &old_op_type, const Op &op, const std::vector<std::vector<std::uint32_t>> &in_shapes)
 {
     TT_DBG_ASSERT(op.type() == OpType::Embedding, "Wrong op type.");
-    return op.base_shape(old_op_type, in_shapes);
+    TT_ASSERT(in_shapes.size() == 2, "Embedding should have exactly 2 input shapes.");
+
+    std::vector<std::uint32_t> output_shape = in_shapes[0];
+    output_shape.push_back(in_shapes[1].back());
+
+    return std::make_tuple(Shape::create(output_shape), std::vector<DimBroadcast>{});
 }
 
 NodeContext backward(
@@ -45,35 +52,8 @@ NodeContext backward(
     const NodeContext &gradient)
 {
     TT_DBG_ASSERT(op.type() == OpType::Embedding, "Wrong op type.");
-    return op.base_backward(old_op_type, ac, operand, inputs, output, gradient);
-}
 
-void decompose_initial(
-    const graphlib::OpType &old_op_type, const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::Embedding, "Wrong op type.");
-    return op.base_decompose(old_op_type, "get_f_forge_decompose", dc, inputs);
-}
-
-void decompose_post_optimize(
-    const graphlib::OpType &old_op_type, const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::Embedding, "Wrong op type.");
-    return op.base_decompose(old_op_type, "get_f_forge_decompose_post_optimize", dc, inputs);
-}
-
-void decompose_post_autograd(
-    const graphlib::OpType &old_op_type, const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::Embedding, "Wrong op type.");
-    return op.base_decompose(old_op_type, "get_f_forge_decompose_post_autograd", dc, inputs);
-}
-
-long initial_flops_estimate(
-    const graphlib::OpType &old_op_type, const Op &op, const std::vector<std::vector<std::uint32_t>> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::Embedding, "Wrong op type.");
-    return op.base_initial_flops_estimate(old_op_type, inputs);
+    return ac.autograd->create_op(ac, graphlib::OpType("embedding_bw", {}, {}), {inputs[0], inputs[1], gradient});
 }
 
 }  // namespace embedding
--- forge/csrc/ops/op_embedding_bw.cpp
+++ forge/csrc/ops/op_embedding_bw.cpp
@@ -25,14 +25,28 @@ using namespace graphlib;
 at::Tensor eval(const graphlib::OpType &old_op_type, const Op &op, const std::vector<at::Tensor> &tensors)
 {
     TT_DBG_ASSERT(op.type() == OpType::EmbeddingBw, "Wrong op type.");
-    return op.base_eval(old_op_type, tensors);
+    TT_ASSERT(tensors.size() == 3, "EmbeddingBw should have exactly 3 input tensors.");
+
+    at::Tensor input = tensors[0];
+    at::Tensor weight = tensors[1];
+    at::Tensor grad = tensors[2];
+
+    at::Tensor result = torch::zeros(weight.sizes(), weight.options());
+    for (int64_t i = 0; i < input.numel(); ++i)
+    {
+        int64_t idx = input.flatten()[i].item<int64_t>();
+        result[idx] = grad.flatten()[i];
+    }
+    return result;
 }
 
 std::tuple<Shape, std::vector<DimBroadcast>> shape(
     const graphlib::OpType &old_op_type, const Op &op, const std::vector<std::vector<std::uint32_t>> &in_shapes)
 {
     TT_DBG_ASSERT(op.type() == OpType::EmbeddingBw, "Wrong op type.");
-    return op.base_shape(old_op_type, in_shapes);
+    TT_ASSERT(in_shapes.size() == 3, "EmbeddingBw should have exactly 3 input shapes.");
+
+    return std::make_tuple(Shape::create(in_shapes[1]), std::vector<DimBroadcast>{});
 }
 
 NodeContext backward(
@@ -45,35 +59,8 @@ NodeContext backward(
     const NodeContext &gradient)
 {
     TT_DBG_ASSERT(op.type() == OpType::EmbeddingBw, "Wrong op type.");
-    return op.base_backward(old_op_type, ac, operand, inputs, output, gradient);
-}
-
-void decompose_initial(
-    const graphlib::OpType &old_op_type, const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::EmbeddingBw, "Wrong op type.");
-    return op.base_decompose(old_op_type, "get_f_forge_decompose", dc, inputs);
-}
-
-void decompose_post_optimize(
-    const graphlib::OpType &old_op_type, const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::EmbeddingBw, "Wrong op type.");
-    return op.base_decompose(old_op_type, "get_f_forge_decompose_post_optimize", dc, inputs);
-}
-
-void decompose_post_autograd(
-    const graphlib::OpType &old_op_type, const Op &op, DecomposingContext &dc, const std::vector<NodeContext> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::EmbeddingBw, "Wrong op type.");
-    return op.base_decompose(old_op_type, "get_f_forge_decompose_post_autograd", dc, inputs);
-}
-
-long initial_flops_estimate(
-    const graphlib::OpType &old_op_type, const Op &op, const std::vector<std::vector<std::uint32_t>> &inputs)
-{
-    TT_DBG_ASSERT(op.type() == OpType::EmbeddingBw, "Wrong op type.");
-    return op.base_initial_flops_estimate(old_op_type, inputs);
+    TT_ASSERT(false, "embedding_bw should not be backwarded");
+    return gradient;
 }
 
 }  // namespace embedding_bw
