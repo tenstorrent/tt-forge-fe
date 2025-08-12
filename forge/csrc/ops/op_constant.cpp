9d90227412e8a77a9e4a5d2edab38ccf64214dc2
Pavle Glusac <pglusac@tenstorrent.com>
2025-08-12 14:24:46 +0000

    Extend const with dtype

--- forge/csrc/ops/op_constant.cpp
+++ forge/csrc/ops/op_constant.cpp
@@ -6,6 +6,7 @@
 
 #include "autograd/autograd.hpp"
 #include "graph_lib/shape.hpp"
+#include "lower_to_forge/common.hpp"
 #include "op.hpp"
 #include "op_interface.hpp"
 #include "torch/extension.h"
@@ -23,9 +24,17 @@ at::Tensor eval(const graphlib::OpType &old_op_type, const Op &op, const std::ve
 {
     TT_DBG_ASSERT(op.type() == OpType::Constant, "Wrong op type.");
     TT_ASSERT(tensors.size() == 0, "Constant eval should not have any operands");
-    TT_ASSERT(op.attrs().size() == 1, "Constant eval should contain 1 attr.");
+    TT_ASSERT(op.attrs().size() == 1 || op.attrs().size() == 2, "Constant eval should contain 1 or 2 attrs.");
+    // Convert from forge dtype to torch dtype
+    auto forge_dtype = DataFormat::Float32;
+    if (op.has_attr("dtype"))
+    {
+        forge_dtype = static_cast<DataFormat>(op.attr_as<int>("dtype"));
+    }
 
-    return torch::tensor({op.attr_as<float>("c")});
+    auto torch_dtype = graphlib::data_format_to_scalar_type(forge_dtype);
+
+    return torch::tensor({op.attr_as<float>("c")}, torch::TensorOptions().dtype(torch_dtype));
 }
 
 std::tuple<graphlib::Shape, std::vector<graphlib::DimBroadcast>> shape(
