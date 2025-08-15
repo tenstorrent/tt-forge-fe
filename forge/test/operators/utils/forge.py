33873743e689b5b003a241d9e6eae00b2cf5506e
Vladimir Brkic <vbrkic@tenstorrent.com>
2025-08-15 19:41:41 +0000

    SweepsFrontend
    (cherry picked from commit 399c2eb8fea9b0270ee09cd82f4490295cbc1194)

--- forge/test/operators/utils/forge.py
+++ forge/test/operators/utils/forge.py
@@ -4,31 +4,41 @@
 
 # Organize all forge imports
 
-from forge import MathFidelity, DataFormat
-from forge import Tensor
-from forge import Tensor as ForgeTensor
 
-from forge.tensor import to_pt_tensors
-from forge.tensor import to_forge_tensors
+from .frontend import XLA_MODE
 
-from forge import ForgeModule, Module
-from forge import compile
 
-from forge.op_repo import TensorShape
-from forge.op_repo.pytorch_operators import pytorch_operator_repository
+if XLA_MODE:
+    from forge.op_repo import *
+    from forge.compat import *
+    from forge import Tensor as ForgeTensor
 
-from forge.verify import TestKind  # , verify_module
-from forge.config import CompilerConfig
-from forge.verify.config import VerifyConfig
-from forge.verify.compare import compare_with_golden
-from forge.verify.verify import verify
-from forge.verify.value_checkers import ValueChecker
-from forge.verify.value_checkers import AutomaticValueChecker
-from forge.verify.value_checkers import AllCloseValueChecker
+else:
+    from forge import MathFidelity, DataFormat
+    from forge import Tensor
+    from forge import Tensor as ForgeTensor
 
-from forge.forge_property_utils import forge_property_handler_var
-from forge.forge_property_utils import record_sweeps_test_tags, record_sweeps_expected_failing_reason
-from forge.forge_property_utils import record_sweeps_detected_failing_reason
+    from forge.tensor import to_pt_tensors
+    from forge.tensor import to_forge_tensors
+
+    from forge import ForgeModule, Module
+    from forge import compile
+
+    from forge.op_repo import TensorShape
+    from forge.op_repo.pytorch_operators import pytorch_operator_repository
+
+    from forge.verify import TestKind  # , verify_module
+    from forge.config import CompilerConfig
+    from forge.verify.config import VerifyConfig
+    from forge.verify.compare import compare_with_golden
+    from forge.verify.verify import verify
+    from forge.verify.value_checkers import ValueChecker
+    from forge.verify.value_checkers import AutomaticValueChecker
+    from forge.verify.value_checkers import AllCloseValueChecker
+
+    from forge.forge_property_utils import forge_property_handler_var
+    from forge.forge_property_utils import record_sweeps_test_tags, record_sweeps_expected_failing_reason
+    from forge.forge_property_utils import record_sweeps_detected_failing_reason
 
 
 __all__ = [
---
+++ forge/test/operators/utils/frontend/xla.py
@@ -0,0 +1,52 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+# Forge frontend
+
+
+from typing import Any, Dict, Sequence
+
+import torch
+from infra import ComparisonConfig, Model, RunMode, TorchModelTester
+
+
+class SweepsTester(TorchModelTester):
+    """Tester for Sweeps model."""
+
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        inputs: Dict[str, torch.Tensor],
+        comparison_config: ComparisonConfig = ComparisonConfig(),
+        run_mode: RunMode = RunMode.INFERENCE,
+    ) -> None:
+        self.model = model
+        self.inputs = inputs
+        super().__init__(comparison_config, run_mode)
+
+    # @override
+    def _get_model(self) -> Model:
+        return self.model
+
+    def _get_forward_method_args(self) -> Sequence[Any]:
+        return self.inputs
+
+    # @override
+    def _get_input_activations(self) -> Dict | Sequence[Any]:
+        # return {
+        #     "x": torch.randn(1, 3, 24, 22),
+        #     "y": torch.randn(1, 3, 24, 22),
+        # }
+        inputs = {}
+        # names = ["x", "y", "z", "a1", "a2", "a3", "a4", "a5", "a6"]
+        # for index, input in enumerate(self.inputs):
+        #     inputs[names[index]] = input
+        return inputs
+
+
+class XlaFrontend:
+    @classmethod
+    def verify(cls, model, inputs, compiler_cfg, verify_config):
+        sweeps_tester = SweepsTester(model, inputs)
+        sweeps_tester.test()
