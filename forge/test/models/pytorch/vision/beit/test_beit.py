02f5864f28f0530ce3b14a06d355d62c7f95fe86
Ashok Kumar Kannan <160501980+ashokkumarkannan1@users.noreply.github.com>
2025-08-16 13:39:06 +0530

    Link FFE Scripts for Deepseek math and Beit to tt_forge_models (#2732)
    ### Problem description
    This PR Includes the Linkage of the deepseek math modal and Beit modal
    to FFE Scripts.
    
    ### What's changed
    Test script of the deepsee_math and Beit has been modified
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes
    
    ### Model Log
    
    [deepseek_math.log](https://github.com/user-attachments/files/21583271/deepseek_math.log)
    [beit.log](https://github.com/user-attachments/files/21583277/beit.log)

--- forge/test/models/pytorch/vision/beit/test_beit.py
+++ forge/test/models/pytorch/vision/beit/test_beit.py
@@ -1,63 +1,55 @@
-# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
-
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
 # SPDX-License-Identifier: Apache-2.0
+
 import pytest
 import torch
+from third_party.tt_forge_models.beit.pytorch import ModelLoader as BeitLoader
+from third_party.tt_forge_models.beit.pytorch import ModelVariant as BeitVariant
 
 import forge
-from forge._C import DataFormat
-from forge.config import CompilerConfig
 from forge.forge_property_utils import (
     Framework,
     ModelArch,
+    ModelGroup,
+    ModelPriority,
     Source,
     Task,
     record_model_properties,
 )
 from forge.verify.verify import verify
 
-from test.models.pytorch.vision.beit.model_utils.utils import load_input, load_model
-
-variants = [
-    pytest.param(
-        "microsoft/beit-base-patch16-224",
-        marks=[pytest.mark.xfail],
-    ),
-    pytest.param(
-        "microsoft/beit-large-patch16-224",
-        marks=[pytest.mark.xfail],
-    ),
+BEIT_VARIANTS = [
+    pytest.param(BeitVariant.BASE, marks=[pytest.mark.xfail]),
+    pytest.param(BeitVariant.LARGE, marks=[pytest.mark.xfail]),
 ]
 
 
 @pytest.mark.nightly
-@pytest.mark.parametrize("variant", variants)
-def test_beit_image_classification(variant):
-
+@pytest.mark.parametrize("variant", BEIT_VARIANTS)
+def test_beit_image_classification_pytorch(variant):
     # Record Forge Property
     module_name = record_model_properties(
         framework=Framework.PYTORCH,
         model=ModelArch.BEIT,
         variant=variant,
-        source=Source.HUGGINGFACE,
         task=Task.IMAGE_CLASSIFICATION,
+        source=Source.HUGGINGFACE,
+        group=ModelGroup.GENERALITY,
+        priority=ModelPriority.P2,
     )
 
     # Load model and input
-    framework_model = load_model(variant).to(torch.bfloat16)
-    inputs = load_input(variant)
-    inputs = [inputs[0].to(torch.bfloat16)]
+    loader = BeitLoader(variant)
+    model = loader.load_model(dtype_override=torch.bfloat16)
+    input_dict = loader.load_inputs(dtype_override=torch.bfloat16)
 
-    data_format_override = DataFormat.Float16_b
-    compiler_cfg = CompilerConfig(default_df_override=data_format_override)
+    # prepare inputs
+    inputs = [input_dict["pixel_values"]]
+    model.eval()
 
     # Forge compile framework model
-    compiled_model = forge.compile(
-        framework_model,
-        sample_inputs=inputs,
-        module_name=module_name,
-        compiler_cfg=compiler_cfg,
-    )
+    compiled_model = forge.compile(model, inputs, module_name)
 
     # Model Verification
-    verify(inputs, framework_model, compiled_model)
+    verify(inputs, model, compiled_model)
