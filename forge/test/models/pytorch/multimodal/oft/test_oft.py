e5e3635ea521da988e492c6b8bfe315abbea1d42
kkannan <kkannan@tenstorrent.com>
2025-08-19 12:04:12 +0000

    Track changes of tt-forge-fe model Bringup & link FFE tests - set12

--- forge/test/models/pytorch/multimodal/oft/test_oft.py
+++ forge/test/models/pytorch/multimodal/oft/test_oft.py
@@ -2,6 +2,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
+from third_party.tt_forge_models.oft_stable_diffusion.pytorch import (
+    ModelLoader,
+    ModelVariant,
+)
 
 import forge
 from forge.forge_property_utils import (
@@ -17,19 +21,18 @@ from forge.verify.verify import verify
 
 from test.models.pytorch.multimodal.oft.model_utils.oft_utils import (
     StableDiffusionWrapper,
-    get_inputs,
 )
 
 
 @pytest.mark.xfail
-@pytest.mark.parametrize("variant", ["runwayml/stable-diffusion-v1-5"])
+@pytest.mark.parametrize("variant", [ModelVariant.OFT_STABLE_DIFFUSION_V1_5])
 @pytest.mark.nightly
 def test_oft(variant):
     # Record Forge Property
     module_name = record_model_properties(
         framework=Framework.PYTORCH,
         model=ModelArch.OFT,
-        variant=variant.split("/")[-1],
+        variant=variant,
         task=Task.CONDITIONAL_GENERATION,
         source=Source.HUGGINGFACE,
         group=ModelGroup.RED,
@@ -37,7 +40,9 @@ def test_oft(variant):
     )
 
     # Load model and inputs
-    pipe, inputs = get_inputs(model=variant)
+    loader = ModelLoader(variant=variant)
+    pipe = loader.load_model()
+    inputs = loader.load_inputs()
 
     # Forge compile framework model
     framework_model = StableDiffusionWrapper(pipe)
